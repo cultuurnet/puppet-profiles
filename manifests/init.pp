749a0827010a4d2b2e09d9195e1551914d724eae
Kristof Willaert <kristof.willaert@cultuurnet.be>
2018-11-20 17:08:44 +0100

    Add Ubuntu 16.04 as supported operating system

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@ class profiles {
   case $::operatingsystem {
     'Ubuntu': {
       case $::operatingsystemrelease {
-        '14.04': {
+        '14.04','16.04': {
         }
         default: {
           fail("Ubuntu ${::operatingsystemrelease} not supported")
