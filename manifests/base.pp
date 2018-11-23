6ffb26cc29f56859aeb378028be7c6504d507b07
Kristof Willaert <kristof.willaert@cultuurnet.be>
2018-11-23 17:12:52 +0100

    Add PATH and RUBYLIB variables to base class

--- manifests/base.pp
+++ manifests/base.pp
@@ -2,4 +2,15 @@ class profiles::base {
 
   include ::profiles
 
+  shellvar { 'PATH':
+    ensure => 'present',
+    target => '/etc/environment',
+    value  => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/puppetlabs/bin'
+  }
+
+  shellvar { 'RUBYLIB':
+    ensure => 'present',
+    target => '/etc/environment',
+    value  => '/opt/puppetlabs/puppet/lib/ruby/vendor_ruby'
+  }
 }
--- spec/classes/base_spec.rb
+++ spec/classes/base_spec.rb
@@ -2,4 +2,29 @@ require 'spec_helper'
 
 describe 'profiles::base' do
   include_examples 'operating system support', 'profiles::base'
+
+  context "on Ubuntu 14.04" do
+    let(:facts) { {
+        :osfamily               => 'Debian',
+        :operatingsystem        => 'Ubuntu',
+        :operatingsystemrelease => '14.04'
+      }
+    }
+
+    it { is_expected.to compile.with_all_deps }
+
+    it { is_expected.to contain_shellvar('PATH').with(
+      'ensure' => 'present',
+      'target' => '/etc/environment',
+      'value'  => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/puppetlabs/bin'
+      )
+    }
+
+    it { is_expected.to contain_shellvar('RUBYLIB').with(
+      'ensure' => 'present',
+      'target' => '/etc/environment',
+      'value'  => '/opt/puppetlabs/puppet/lib/ruby/vendor_ruby'
+      )
+    }
+  end
 end
