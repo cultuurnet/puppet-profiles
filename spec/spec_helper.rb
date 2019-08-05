b91110900f017d47c488d70f27ca870f9d926eef
Kristof Willaert <kristof.willaert@cultuurnet.be>
2019-08-05 23:55:39 +0200

    Make sure we use rspec-mocks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,8 @@ RSpec.configure do |c|
   c.mock_with :rspec
 end
 
-require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
+require 'puppetlabs_spec_helper/module_spec_helper'
 
 include RspecPuppetFacts
 
@@ -11,6 +11,7 @@ Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
 
 RSpec.configure do |c|
   c.default_facts = { 'staging_http_get' =>  'curl' }
+  c.mock_with :rspec
   c.after(:suite) do
     RSpec::Puppet::Coverage.report!
   end
--- spec/unit/facter/resolv_spec.rb
+++ spec/unit/facter/resolv_spec.rb
@@ -1,19 +1,19 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe "Facter::Util::Fact" do
-  before {
-    Facter.clear
-    allow(Resolv::DNS::Config).to receive(:default_config_hash).and_return({:nameserver=>["8.8.4.4", "8.8.8.8"], :search=>["example.com"], :ndots=>1})
-  }
+  before(:each) { Facter.clear }
+  after(:each) { Facter.clear }
 
   describe "nameservers" do
     it do
+      allow(Resolv::DNS::Config).to receive(:default_config_hash).and_return({:nameserver=>["8.8.4.4", "8.8.8.8"], :search=>["example.com"], :ndots=>1})
       expect(Facter.fact(:nameservers).value).to eql([ "8.8.4.4", "8.8.8.8"])
     end
   end
 
   describe "searchdomains" do
     it do
+      allow(Resolv::DNS::Config).to receive(:default_config_hash).and_return({:nameserver=>["8.8.4.4", "8.8.8.8"], :search=>["example.com"], :ndots=>1})
       expect(Facter.fact(:searchdomains).value).to eql([ "example.com"])
     end
   end
