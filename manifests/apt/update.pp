d22effcfc1f3692a3544f0894aae6b2c3f3b9e18
Kristof Willaert <kristof.willaert@cultuurnet.be>
2019-09-15 13:42:07 +0200

    Use include for apt update to get rid of dependency cycles with packages

--- manifests/apt/update.pp
+++ manifests/apt/update.pp
@@ -1,6 +1,6 @@
 define profiles::apt::update {
 
-  contain ::profiles
+  include ::profiles
 
   exec { "apt-get update ${title}":
     command => "apt-get update -o Dir::Etc::sourcelist='sources.list.d/${title}.list' -o Dir::Etc::sourceparts='-' -o APT::Get::List-Cleanup='0'",
