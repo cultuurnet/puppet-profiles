29f4bf72188ef7de77a0376c2060ec375bf87a2a
Kristof Willaert <kristof.willaert@cultuurnet.be>
2019-09-16 12:06:33 +0200

    Add deployment::curator::api class with tests

---
+++ manifests/deployment/curator/api.pp
@@ -0,0 +1,58 @@
+class profiles::deployment::curator::api (
+  String           $config_source,
+  Optional[String] $puppetdb_url   = undef
+) {
+
+  $basedir = '/var/www/curator-api'
+
+  contain profiles
+  contain profiles::deployment::curator
+
+  realize Apt::Source['publiq-curator']
+  realize Profiles::Apt::Update['publiq-curator']
+
+  #TODO: package notify Apache::Service ?
+  #TODO: config file notify Apache::Service ?
+
+  package { 'curator-api':
+    ensure  => 'latest',
+    notify  => Profiles::Deployment::Versions[$title],
+    require => Profiles::Apt::Update['publiq-curator']
+  }
+
+  file { 'curator-api-config':
+    ensure  => 'file',
+    path    => "${basedir}/.env",
+    source  => $config_source,
+    owner   => 'www-data',
+    group   => 'www-data',
+    require => Package['curator-api']
+  }
+
+  exec { 'curator-api_db_schema_update':
+    command     => 'php bin/console doctrine:migrations:migrate --no-interaction',
+    cwd         => $basedir,
+    user        => 'www-data',
+    group       => 'www-data',
+    path        => [ '/usr/local/bin', '/usr/bin', '/bin', '/var/www/curator-api'],
+    subscribe   => Package['curator-api'],
+    refreshonly => true
+  }
+
+  exec { 'curator-api_cache_clear':
+    command     => 'php bin/console cache:clear',
+    cwd         => $basedir,
+    user        => 'www-data',
+    group       => 'www-data',
+    path        => [ '/usr/local/bin', '/usr/bin', '/bin', '/var/www/curator-api'],
+    subscribe   => Package['curator-api'],
+    require     => Exec['curator-api_db_schema_update'],
+    refreshonly => true
+  }
+
+  profiles::deployment::versions { $title:
+    project      => 'curator',
+    packages     => 'curator-api',
+    puppetdb_url => $puppetdb_url
+  }
+}
---
+++ spec/classes/deployment/curator/api_spec.rb
@@ -0,0 +1,100 @@
+require 'spec_helper'
+
+describe 'profiles::deployment::curator::api' do
+  let (:pre_condition) { 'include ::profiles' }
+
+  context "with config_source => /foo" do
+    let (:params) { {
+      'config_source' => '/foo'
+    } }
+
+    include_examples 'operating system support', 'profiles::deployment::curator::api'
+
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let (:facts) { facts }
+
+        it { is_expected.to compile.with_all_deps }
+
+        it { is_expected.to contain_apt__source('publiq-curator') }
+        it { is_expected.to contain_profiles__apt__update('publiq-curator') }
+
+        it { is_expected.to contain_package('curator-api').with( 'ensure' => 'latest') }
+        it { is_expected.to contain_package('curator-api').that_notifies('Profiles::Deployment::Versions[profiles::deployment::curator::api]') }
+        it { is_expected.to contain_package('curator-api').that_requires('Profiles::Apt::Update[publiq-curator]') }
+
+        it { is_expected.to contain_file('curator-api-config').with(
+          'ensure' => 'file',
+          'path'   => '/var/www/curator-api/.env',
+          'source' => '/foo',
+          'owner'  => 'www-data',
+          'group'  => 'www-data'
+        ) }
+
+        it { is_expected.to contain_file('curator-api-config').that_requires('Package[curator-api]') }
+
+        it { is_expected.to contain_exec('curator-api_db_schema_update').with(
+          'command'     => 'php bin/console doctrine:migrations:migrate --no-interaction',
+          'cwd'         => '/var/www/curator-api',
+          'user'        => 'www-data',
+          'group'       => 'www-data',
+          'path'        => [ '/usr/local/bin', '/usr/bin', '/bin', '/var/www/curator-api'],
+          'refreshonly' => true
+        ) }
+
+        it { is_expected.to contain_exec('curator-api_db_schema_update').that_subscribes_to('Package[curator-api]') }
+
+        it { is_expected.to contain_exec('curator-api_cache_clear').with(
+          'command'     => 'php bin/console cache:clear',
+          'cwd'         => '/var/www/curator-api',
+          'user'        => 'www-data',
+          'group'       => 'www-data',
+          'path'        => [ '/usr/local/bin', '/usr/bin', '/bin', '/var/www/curator-api'],
+          'refreshonly' => true
+        ) }
+
+        it { is_expected.to contain_exec('curator-api_cache_clear').that_subscribes_to('Package[curator-api]') }
+        it { is_expected.to contain_exec('curator-api_cache_clear').that_requires('Exec[curator-api_db_schema_update]') }
+
+        it { is_expected.to contain_profiles__deployment__versions('profiles::deployment::curator::api').with(
+          'project'      => 'curator',
+          'packages'     => 'curator-api',
+          'puppetdb_url' => nil
+        ) }
+      end
+    end
+  end
+
+  context "with config_source => /bar and puppetdb_url => http://example.com:8000" do
+    let (:params) { {
+      'config_source' => '/bar',
+      'puppetdb_url'  => 'http://example.com:8000'
+    } }
+
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let (:facts) { facts }
+
+        it { is_expected.to contain_file('curator-api-config').with(
+          'source' => '/bar',
+        ) }
+
+        it { is_expected.to contain_profiles__deployment__versions('profiles::deployment::curator::api').with(
+          'puppetdb_url' => 'http://example.com:8000'
+        ) }
+      end
+    end
+  end
+
+  context "without parameters" do
+    let (:params) { {} }
+
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let (:facts) { facts }
+
+        it { expect { catalogue }.to raise_error(Puppet::ParseError, /expects a value for parameter 'config_source'/) }
+      end
+    end
+  end
+end
