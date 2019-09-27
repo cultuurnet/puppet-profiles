112dd85e0835378c6a9dc5699edee87a45107bad
Kristof Willaert <kristof.willaert@cultuurnet.be>
2019-09-27 11:36:40 +0200

    Add sysctl var and ES datadir to elasticsearch profile

--- manifests/elasticsearch.pp
+++ manifests/elasticsearch.pp
@@ -1,10 +1,18 @@
 class profiles::elasticsearch {
 
   contain ::profiles
+  contain ::profiles::java8
 
   include ::profiles::repositories
 
   realize Apt::Source['elasticsearch']
   realize Profiles::Apt::Update['elasticsearch']
 
+  file { '/data/elasticsearch':
+    ensure => 'directory'
+  }
+
+  sysctl { 'vm.max_map_count':
+    value => '262144'
+  }
 }
--- spec/classes/elasticsearch_spec.rb
+++ spec/classes/elasticsearch_spec.rb
@@ -12,8 +12,18 @@ describe 'profiles::elasticsearch' do
 
         it { is_expected.to compile.with_all_deps }
 
+        it { is_expected.to contain_class('profiles::java8') }
+
         it { is_expected.to contain_apt__source('elasticsearch') }
         it { is_expected.to contain_profiles__apt__update('elasticsearch') }
+
+        it { is_expected.to contain_file('/data/elasticsearch').with(
+          'ensure' => 'directory'
+        ) }
+
+        it { is_expected.to contain_sysctl('vm.max_map_count').with(
+          'value' => '262144'
+        ) }
       end
     end
   end
