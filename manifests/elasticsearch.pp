69269f4a562dfa9bc1bef720d3ee0b04381587e4
Kristof Willaert <kristof.willaert@cultuurnet.be>
2019-09-27 17:59:29 +0200

    Add elasticsearch class to profile

--- manifests/elasticsearch.pp
+++ manifests/elasticsearch.pp
@@ -15,4 +15,32 @@ class profiles::elasticsearch {
   sysctl { 'vm.max_map_count':
     value => '262144'
   }
+
+  if $facts['ec2_metadata'] {
+    $http_hosts = [ $facts['ipaddress_eth0'], '127.0.0.1']
+  } else {
+    $http_hosts = [ $facts['ipaddress_eth1'], '127.0.0.1']
+  }
+
+  class { 'elasticsearch':
+    version              => '5.2.2',
+    manage_repo          => false,
+    api_timeout          => 30,
+    restart_on_change    => true,
+    status               => 'running',
+    jvm_options          => [ '-Xms768m', '-Xmx768m'],
+    instances            => {
+      'es01' => {
+        'config'  => {
+          'http.host'    => $http_hosts,
+          'network.host' => [ '127.0.0.1']
+        },
+        'datadir' => '/data/elasticsearch/es01'
+      }
+    }
+  }
+
+  Profiles::Apt::Update['elasticsearch'] -> Class['elasticsearch']
+  File['/data/elasticsearch'] -> Class['elasticsearch']
+  Sysctl['vm.max_map_count'] -> Class['elasticsearch']
 }
