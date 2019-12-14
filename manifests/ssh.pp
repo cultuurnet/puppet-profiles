cf38f41f374d4ad51c9995826261367ca70832f1
Kristof Willaert <kristof.willaert@cultuurnet.be>
2019-12-14 09:19:12 +0100

    Manage ssh authorized keys in ssh class

--- manifests/ssh.pp
+++ manifests/ssh.pp
@@ -1,8 +1,11 @@
-class profiles::ssh {
+class profiles::ssh(
+  Variant[String, Array[String]] $ssh_authorized_keys_tags = []
+) {
 
   contain ::profiles
 
   include ::profiles::firewall
+  include ::profiles::ssh_authorized_keys
 
   Sshd_config {
     notify => Service['ssh']
@@ -20,6 +23,10 @@ class profiles::ssh {
 
   resources { 'ssh_authorized_key':
     purge => true
+  }
+
+  $ssh_authorized_keys_tags.each |$tag| {
+    Ssh_authorized_key <| tag == $tag |>
   }
 
   realize Firewall['100 accept ssh traffic']
--- spec/classes/ssh_spec.rb
+++ spec/classes/ssh_spec.rb
@@ -7,31 +7,54 @@ describe 'profiles::ssh' do
     context "on #{os}" do
       let(:facts) { facts }
 
-      it { is_expected.to compile.with_all_deps }
+      context "without parameters" do
+        let(:params) { { } }
 
-      it { is_expected.to contain_sshd_config('PermitRootLogin').with(
-        'ensure' => 'present',
-        'value'  => 'no'
-        )
-      }
+        it { is_expected.to compile.with_all_deps }
 
-      it { is_expected.to contain_sshd_config('PermitRootLogin').that_notifies('Service[ssh]') }
+        it { is_expected.to contain_sshd_config('PermitRootLogin').with(
+          'ensure' => 'present',
+          'value'  => 'no'
+          )
+        }
 
-      it { is_expected.to contain_service('ssh').with(
-        'ensure' => 'running',
-        'enable' => true
-        )
-      }
+        it { is_expected.to contain_sshd_config('PermitRootLogin').that_notifies('Service[ssh]') }
 
-      it { is_expected.to contain_resources('ssh_authorized_key').with(
-        'purge' => true
-      ) }
+        it { is_expected.to contain_service('ssh').with(
+          'ensure' => 'running',
+          'enable' => true
+          )
+        }
 
-      it { is_expected.to contain_firewall('100 accept ssh traffic').with(
-        'proto'  => 'tcp',
-        'dport'  => '22',
-        'action' => 'accept'
-      ) }
+        it { is_expected.to contain_resources('ssh_authorized_key').with(
+          'purge' => true
+        ) }
+
+        it { is_expected.to contain_firewall('100 accept ssh traffic').with(
+          'proto'  => 'tcp',
+          'dport'  => '22',
+          'action' => 'accept'
+        ) }
+
+        it { is_expected.to have_ssh_authorized_key_resource_count(0) }
+      end
+
+      context "with ssh_authorized_keys_tags => [ publiq, acme]" do
+        let(:params) { { 'ssh_authorized_keys_tags' => [ 'publiq', 'acme'] } }
+
+        let(:pre_condition) { [
+          '@ssh_authorized_key { "publiq key 1": tag => "publiq" }',
+          '@ssh_authorized_key { "publiq key 2": tag => "publiq" }',
+          '@ssh_authorized_key { "foobar key 1": tag => "foobar" }',
+          '@ssh_authorized_key { "acme key 1": tag => "acme" }'
+        ] }
+
+        it { is_expected.to contain_ssh_authorized_key('publiq key 1') }
+        it { is_expected.to contain_ssh_authorized_key('publiq key 2') }
+        it { is_expected.to contain_ssh_authorized_key('acme key 1') }
+
+        it { is_expected.to have_ssh_authorized_key_resource_count(3) }
+      end
     end
   end
 end
