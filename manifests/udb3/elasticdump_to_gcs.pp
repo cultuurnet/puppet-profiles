b419bfc00a3a6d6554b41036ff0b7fc8e9dec411
Kristof Willaert <kristof.willaert@cultuurnet.be>
2019-12-24 14:16:40 +0100

    Add elasticdump_to_gcs class

---
+++ manifests/udb3/elasticdump_to_gcs.pp
@@ -0,0 +1,43 @@
+class profiles::udb3::elasticdump_to_gcs (
+  String      $gcs_bucket_name,
+  String      $gcs_key_file_source,
+  String      $bucket_mountpoint,
+  String      $index_name,
+  String      $local_timezone = 'UTC',
+  Integer[-1] $size           = '-1'
+) {
+
+  contain ::profiles
+
+  include ::profiles::repositories
+  include ::profiles::packages
+
+  realize Apt::Source['nodejs_10.x']
+  realize Apt::Source['tools']
+  realize Profiles::Apt::Update['nodejs_10.x']
+  realize Profiles::Apt::Update['tools']
+
+  realize Package['elasticdump']
+  realize Package['gcsfuse']
+
+  file { 'gcs_credentials.json':
+    path   => '/etc/gcs_credentials.json',
+    mode   => '0644',
+    source => $gcs_key_file_source
+  }
+
+  file { 'elasticdump_to_gcs':
+    path    => '/usr/local/bin/elasticdump_to_gcs',
+    content => template('profiles/udb3/elasticdump_to_gcs.erb'),
+    mode    => '0755',
+    require => [ Package['elasticdump'], Package['gcsfuse'], File['gcs_credentials.json']],
+  }
+
+  cron { 'elasticdump_to_gcs':
+    command => '/usr/local/bin/elasticdump_to_gcs',
+    require => File['elasticdump_to_gcs'],
+    user    => 'ubuntu',
+    hour    => '*',
+    minute  => '59'
+  }
+}
