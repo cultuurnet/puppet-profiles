02b8e623135cf164dcf04aedbefdf50fa1385f4e
sethlinnpubliq <seth.linn@publiq.be>
2020-03-16 10:23:17 +0100

    SYS-1369 Create aptly AMI Added parameters to puppet profile.

--- manifests/aptly.pp
+++ manifests/aptly.pp
@@ -1,3 +1,15 @@
 class profiles::aptly {
 
+	class { 'aptly':                                                                    
+		s3_publish_endpoints =>                                                           
+		{ 
+			'apt.publiq.be' =>                                                           
+			{ 
+				'region' => 'eu-west-1',                                                                                                                                                                      
+				'bucket' => 'apt.publiq.be' 
+				'awsAccessKeyID' => 'AKIAIVJBGNGHTNGMYNEA'
+				'awsSecretAccessKey' => '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'
+			}                                                                              
+		}                                                                                
+	}
 }
