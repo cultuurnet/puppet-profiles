4e96e25ca9290a36ffcd26df16de78ec0fd75a7a
sethlinnpubliq <seth.linn@publiq.be>
2020-03-17 11:24:46 +0100

    SYS-1369 Create aptly AMI Fixed sysntax problems

--- manifests/aptly.pp
+++ manifests/aptly.pp
@@ -1,15 +1,14 @@
+## This profile/module installs, configures, and maintains aptly.
 class profiles::aptly {
 
-	class { 'aptly':                                                                    
-		s3_publish_endpoints =>                                                           
-		{ 
-			'apt.publiq.be' =>                                                           
-			{ 
-				'region' => 'eu-west-1',                                                                                                                                                                      
-				'bucket' => 'apt.publiq.be' 
-				'awsAccessKeyID' => 'AKIAIVJBGNGHTNGMYNEA'
-				'awsSecretAccessKey' => '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'
-			}                                                                              
-		}                                                                                
-	}
+  class { 'aptly':
+    s3_publish_endpoints =>
+    {
+      'apt.publiq.be' =>
+      {
+        'region' => 'eu-west-1',
+        'bucket' => 'apt.publiq.be'
+      }
+    }
+  }
 }
