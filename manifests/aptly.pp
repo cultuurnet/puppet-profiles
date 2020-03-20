0e78cef6decc454f3c3a53e0944086de819c5f1e
sethlinnpubliq <seth.linn@publiq.be>
2020-03-20 18:09:58 +0100

    SYS-1369 Create aptly AMI Fixed 'realize' statement

--- manifests/aptly.pp
+++ manifests/aptly.pp
@@ -3,6 +3,10 @@ class profiles::aptly {
 
   contain ::profiles
 
+  # we need to do an apt-get update now that we have the aptly key.
+  realize Apt::Source['aptly']
+  realize Profiles::Apt::Update['aptly']
+
   #Gets the aptly install key unless it is already there.
   exec { 'Get Install Key':
     command   => 'sudo apt-key adv --keyserver pool.sks-keyservers.net --recv-keys ED75B5A4483DA07C',
@@ -10,9 +14,6 @@ class profiles::aptly {
     path      => [ '/usr/local/bin', '/usr/bin'],
     logoutput => 'true',
   }
-
-  # we need to do an apt-get update now that we have the aptly key.
-  realize Profiles::Apt::Update['aptly']
 
   #This will install aptly and set the s3_publish_endpoints parameter.
   class { 'aptly':
@@ -25,4 +26,7 @@ class profiles::aptly {
       }
     }
   }
+
+  #Exec['Get Install Key']
+  Exec['Get Install Key'] -> Profiles::Apt::Update['elasticsearch']
 }
