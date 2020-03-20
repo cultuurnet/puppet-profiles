1329eb7e79a38e21b8ac83953146dc11f69f2e28
sethlinnpubliq <seth.linn@publiq.be>
2020-03-20 16:24:17 +0100

    OPS-519 Operations Create aptly AMI

--- manifests/apt/update.pp
+++ manifests/apt/update.pp
@@ -3,7 +3,8 @@ define profiles::apt::update {
   include ::profiles
 
   exec { "apt-get update ${title}":
-    command => "apt-get update -o Dir::Etc::sourcelist='sources.list.d/${title}.list' -o Dir::Etc::sourceparts='-' -o APT::Get::List-Cleanup='0'",
-    path    => [ '/usr/local/bin', '/usr/bin']
+    command   => "apt-get update -o Dir::Etc::sourcelist='sources.list.d/${title}.list' -o Dir::Etc::sourceparts='-' -o APT::Get::List-Cleanup='0'",
+    path      => [ '/usr/local/bin', '/usr/bin'],
+    logoutput => 'on_fail'
   }
 }
