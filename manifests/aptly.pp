85d86e16abf00a342a9e8648153db22e1ad39bda
sethlinnpubliq <seth.linn@publiq.be>
2020-03-20 15:17:51 +0100

    OPS-519 Operations Create aptly AMI Added aptly install key and apt-get update

--- manifests/aptly.pp
+++ manifests/aptly.pp
@@ -3,6 +3,18 @@ class profiles::aptly {
 
   contain ::profiles
 
+  #Gets the aptly install key unless it is already there.
+  exec { 'Get Install Key':
+    command   => 'sudo apt-key adv --keyserver pool.sks-keyservers.net --recv-keys ED75B5A4483DA07C',
+    unless    => '[[ $(apt-key list | grep -w 4096R/483DA07C) ]] && true',
+    path      => [ '/usr/local/bin', '/usr/bin'],
+    logoutput => 'true',
+  }
+
+  # we need to do an apt-get update now that we have the aptly key.
+  realize Profiles::Apt::Update['aptly']
+
+  #This will install aptly and set the s3_publish_endpoints parameter.
   class { 'aptly':
     s3_publish_endpoints =>
     {
