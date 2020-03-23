f69fbaa3931b48ef4c2690655b0a4dccbdfd8717
sethlinnpubliq <seth.linn@publiq.be>
2020-03-23 15:51:57 +0100

    SYS-1369 Create aptly AMI

--- manifests/aptly.pp
+++ manifests/aptly.pp
@@ -6,36 +6,24 @@ class profiles::aptly (
 
   contain ::profiles
 
-  # Gets the aptly install key unless it is already there.
-  exec { 'Get Install Key':
-    command   => 'sudo apt-key adv --keyserver pool.sks-keyservers.net --recv-keys ED75B5A4483DA07C',
-    unless    => 'apt-key list | /bin/grep -w Andrey',
-    path      => [ '/usr/local/bin', '/usr/bin'],
-    logoutput => 'true',
-  }
-
   # This will install aptly and set the s3_publish_endpoints parameter.
   class { 'aptly':
+    install_repo         => true,
+    repo_location        => 'http://aptly.publiq.be',
+    repo_keyserver       => 'hkps.pool.sks-keyservers.net',
+    repo_keys            => 'ED75B5A4483DA07C',
+    api_port             => '8080',
+    api_nolock           => true,
+    enable_api           => true,
     s3_publish_endpoints =>
     {
       'apt.publiq.be' =>
       {
         'region'             => 'eu-west-1',
         'bucket'             => 'apt.publiq.be',
-        'awsAccessKeyID'     => lookup('profiles::aptly::awsaccesskeyid'),
-        'awsSecretAccessKey' => lookup('profiles::aptly::awssecretaccesskey')
+        'awsAccessKeyID'     => $awsaccesskeyid,
+        'awsSecretAccessKey' => $awssecretaccesskey
       }
     }
   }
-
-  # Start the apply service.
-  # Quote from 
-  exec { 'Start Aptly':
-    command   => 'aptly api serve',
-    unless    => 'sudo service aptly started',
-    path      => [ '/usr/local/bin', '/usr/bin'],
-    logoutput => 'true',
-  }
-
-  Exec['Get Install Key'] -> Exec['apt_update']
 }
