d8923d9b2ad932f4b89448654d511a273e926762
sethlinnpubliq <seth.linn@publiq.be>
2020-03-24 14:11:21 +0100

    OPS-519 Create aptly AMI

--- manifests/aptly.pp
+++ manifests/aptly.pp
@@ -9,15 +9,12 @@ class profiles::aptly (
   # This will install aptly and set the s3_publish_endpoints parameter.
   class { 'aptly':
     install_repo         => true,
-    repo_location        => 'http://aptly.publiq.be',
+    repo_location        => 'http://repo.aptly.info/',
     repo_keyserver       => 'hkps.pool.sks-keyservers.net',
     repo_key             => 'ED75B5A4483DA07C',
     api_port             => '8080',
     api_nolock           => true,
     enable_api           => true,
-
-    #repo_release        =>,
-    #repos    => $aptly::repo_repos,
 
     s3_publish_endpoints =>
     {
