67d4f1f59d264b2b433c37a6f0188e8d5b8204f4
sethlinnpubliq <seth.linn@publiq.be>
2020-03-30 16:18:24 +0200

    OPS-519 Create aptly AMI Added Apache puppet

--- manifests/aptly.pp
+++ manifests/aptly.pp
@@ -8,10 +8,11 @@ class profiles::aptly (
 
   # This will install aptly and set the s3_publish_endpoints parameter.
   class { 'aptly':
-    install_repo         => true, # Tell aptly to install the 
+    install_repo         => true, # Tell aptly to install from a repo
     repo_location        => 'http://repo.aptly.info/', # Where to get the deb file
     repo_keyserver       => 'hkps.pool.sks-keyservers.net', # Where to get the install key
     repo_key             => 'ED75B5A4483DA07C',
+    port                 => '80',
     api_nolock           => true,
     enable_api           => true,
 
@@ -27,9 +28,16 @@ class profiles::aptly (
     }
   }
 
-  # According to the aptly module documenttion, aptly API service can not start without at least one repo. 
-  #aptly::repo {'initial_repo':
-  #  ensure => present,
-  #  uid    => '0',
-  #}
+  class{ 'apache':
+    default_vhost => false,
+  }
+
+  apache::vhost { 'apt-private_80':
+    docroot         => '/var/aptly/public',
+    port            => '80',
+    servername      => 'aptly.publiq.be',
+    redirect_source => '/',
+    redirect_dest   => 'http://localhost:80',
+    redirect_status => 'permanent',
+  }
 }
