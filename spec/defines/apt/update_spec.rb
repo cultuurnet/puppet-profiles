0e41761818600fe08a4f193cd7615e10fffd8723
Kristof Willaert <kristof.willaert@cultuurnet.be>
2020-04-16 15:14:03 +0200

    Add spec for apt::update logoutput

--- spec/defines/apt/update_spec.rb
+++ spec/defines/apt/update_spec.rb
@@ -10,8 +10,9 @@ describe 'profiles::apt::update' do
       it { is_expected.to compile.with_all_deps }
 
       it { is_expected.to contain_exec('apt-get update foobar').with(
-        'command' => "apt-get update -o Dir::Etc::sourcelist='sources.list.d/foobar.list' -o Dir::Etc::sourceparts='-' -o APT::Get::List-Cleanup='0'",
-        'path'    => [ '/usr/local/bin', '/usr/bin']
+        'command'   => "apt-get update -o Dir::Etc::sourcelist='sources.list.d/foobar.list' -o Dir::Etc::sourceparts='-' -o APT::Get::List-Cleanup='0'",
+        'path'      => [ '/usr/local/bin', '/usr/bin'],
+        'logoutput' => 'on_failure'
         )
       }
 
@@ -19,8 +20,9 @@ describe 'profiles::apt::update' do
         let(:title) { 'guineapig' }
 
         it { is_expected.to contain_exec('apt-get update guineapig').with(
-          'command' => "apt-get update -o Dir::Etc::sourcelist='sources.list.d/guineapig.list' -o Dir::Etc::sourceparts='-' -o APT::Get::List-Cleanup='0'",
-          'path'    => [ '/usr/local/bin', '/usr/bin']
+          'command'   => "apt-get update -o Dir::Etc::sourcelist='sources.list.d/guineapig.list' -o Dir::Etc::sourceparts='-' -o APT::Get::List-Cleanup='0'",
+          'path'      => [ '/usr/local/bin', '/usr/bin'],
+          'logoutput' => 'on_failure'
           )
         }
       end
