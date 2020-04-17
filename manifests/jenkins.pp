ca503fe6399363bd43465ea60bde845d921c94cb
sethlinnpubliq <seth.linn@publiq.be>
2020-04-17 20:27:17 +0200

    OPS-571-create-puppet-profile-for-jenkin

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -37,17 +37,17 @@ class profiles::jenkins ()
   # We have to use the .jar manually because the name of the file was changed in jenkins itslef but the puppet plugin has not been updated yet,  https://github.com/voxpupuli/puppet-jenkins/pull/945
 
   #Installs the jenkins plugin delivery-pipeline-plugin. The cli will detect if the plugin is already present and do nothing if it is. 
-  exec { 'check-jenkins-cli-version':
+  exec { 'delivery-pipeline-plugin':
     command => "java -jar ${jar} -s http://localhost:8080/ install-plugin delivery-pipeline-plugin -restart",
   }
 
   #Installs the jenkins plugin templating engine. The cli will detect if the plugin is already present and do nothing if it is.
-  exec { 'check-jenkins-cli-version':
+  exec { 'templating-engine':
     command => "java -jar ${jar} -s http://localhost:8080/ install-plugin templating-engine -restart",
   }
 
   #Installs the jenkins plugin templating engine. The cli will detect if the plugin is already present and do nothing if it is.
-  exec { 'check-jenkins-cli-version':
+  exec { 'bitbucket':
     command => "java -jar ${jar} -s http://localhost:8080/ install-plugin bitbucket -restart",
   }
 }
