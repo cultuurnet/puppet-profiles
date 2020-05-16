970ff81d0e44077adcd5550b187f78ee1ecd3491
sethlinnpubliq <seth.linn@publiq.be>
2020-05-16 22:25:31 +0200

    OPS-519 Create aptly AMI Added aptly path

--- manifests/aptly.pp
+++ manifests/aptly.pp
@@ -14,6 +14,7 @@ class profiles::aptly (
 
   #Install the gpg key for the aptly user to sign the published packages.
   exec { 'import-gpg-key':
+    path    => '/home/aptly',
     command => "gpg --import ${gpgkey}",
     require => File[$gpgkey],
   }
