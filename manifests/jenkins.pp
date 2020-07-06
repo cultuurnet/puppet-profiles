c2bb129100a5a5e0fc287ff9ccd9018f823957ae
sethlinnpubliq <seth.linn@publiq.be>
2020-07-06 20:09:01 +0200

    Removed unused line

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -148,7 +148,6 @@ instance.save()' | ${clitool} -auth ${adminuser}:${adminpassword} groovy =",
   Package['dpkg'] -> Class['::profiles::java8'] -> Class['jenkins'] -> File[$sshdir] -> File['jenkins.model.JenkinsLocationConfiguration.xml'] -> Package['jenkins-cli'] -> File[$helper_groovy] -> Exec['mailer'] -> Exec['create-jenkins-user-admin'] -> Exec["jenkins-security-${security_model}"]
 
   realize Package['git']  #defined in packages.pp, installs git
-  profiles::nodejs
 
   # ----------- Install Jenkins Plugins and Credentials-----------
   # The puppet-jenkins module has functionality for adding plugins but you must install the dependencies manually(not done automatically). 
