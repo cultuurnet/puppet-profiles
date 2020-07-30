4c36408cbd6ef5acc66ec2ab0071cae4f255aeda
Kristof Willaert <kristof@publiq.be>
2020-07-30 19:13:07 +0200

    Add UiTID v2 deployment

---
+++ manifests/deployment/uitidv2/frontend.pp
@@ -0,0 +1,61 @@
+class profiles::deployment::uitidv2::frontend (
+  String           $config_source,
+  String           $package_version     = 'latest',
+  Optional[String] $env_defaults_source = undef,
+  Boolean          $service_manage      = true,
+  String           $service_ensure      = 'running',
+  Boolean          $service_enable      = true,
+  Optional[String] $puppetdb_url        = undef
+) {
+
+  $basedir = '/var/www/uitid-frontend'
+
+  contain ::profiles
+
+  include ::profiles::deployment::uitidv2
+
+  realize Apt::Source['publiq-uitidv2']
+  realize Profiles::Apt::Update['publiq-uitidv2']
+
+  package { 'uitid-frontend':
+    ensure  => $package_version,
+    notify  => Profiles::Deployment::Versions[$title],
+    require => Profiles::Apt::Update['publiq-uitidv2']
+  }
+
+  file { 'uitid-frontend-config':
+    ensure  => 'file',
+    path    => "${basedir}/.env",
+    owner   => 'www-data',
+    group   => 'www-data',
+    source  => $config_source,
+    require => Package['uitid-frontend']
+  }
+
+  if $service_manage {
+    if $env_defaults_source {
+      file { '/etc/default/uitid-frontend':
+        ensure => 'file',
+        owner  => 'root',
+        group  => 'root',
+        source => $env_defaults_source,
+        notify => Service['uitid-frontend']
+      }
+    }
+
+    service { 'uitid-frontend':
+      ensure    => $service_ensure,
+      enable    => $service_enable,
+      require   => Package['uitid-frontend'],
+      hasstatus => true
+    }
+
+    File['uitid-frontend-config'] ~> Service['uitid-frontend']
+  }
+
+  profiles::deployment::versions { $title:
+    project      => 'uitid',
+    packages     => 'uitid-frontend',
+    puppetdb_url => $puppetdb_url
+  }
+}
