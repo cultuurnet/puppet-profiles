fd6f5bf65f19cc5d9037b97316c836ab84e9a9f0
Kristof Willaert <kristof@publiq.be>
2020-08-02 12:32:45 +0200

    Cleanup unit tests for resolv facts

--- spec/unit/facter/resolv_spec.rb
+++ spec/unit/facter/resolv_spec.rb
@@ -1,20 +1,25 @@
 require 'spec_helper'
 
-describe "Facter::Util::Fact" do
+describe 'Facter::Util::Fact' do
   before(:each) { Facter.clear }
-  after(:each) { Facter.clear }
 
-  describe "nameservers" do
-    it do
-      allow(Resolv::DNS::Config).to receive(:default_config_hash).and_return({:nameserver=>["8.8.4.4", "8.8.8.8"], :search=>["example.com"], :ndots=>1})
-      expect(Facter.fact(:nameservers).value).to eql([ "8.8.4.4", "8.8.8.8"])
+  describe 'nameservers' do
+    context 'with nameservers set to 8.8.4.4 and 8.8.8.8' do
+      before(:each) do
+        allow(Resolv::DNS::Config).to receive(:default_config_hash).and_return({:nameserver => [ '8.8.4.4', '8.8.8.8'], :search => [ 'example.com'], :ndots => 1})
+      end
+
+      it { expect(Facter.fact(:nameservers).value).to eql([ '8.8.4.4', '8.8.8.8']) }
     end
   end
 
-  describe "searchdomains" do
-    it do
-      allow(Resolv::DNS::Config).to receive(:default_config_hash).and_return({:nameserver=>["8.8.4.4", "8.8.8.8"], :search=>["example.com"], :ndots=>1})
-      expect(Facter.fact(:searchdomains).value).to eql([ "example.com"])
+  describe 'searchdomains' do
+    context 'with searchdomains set to example.com' do
+      before(:each) do
+        allow(Resolv::DNS::Config).to receive(:default_config_hash).and_return({:nameserver => [ '8.8.4.4', '8.8.8.8'], :search => [ 'example.com'], :ndots => 1})
+      end
+
+      it { expect(Facter.fact(:searchdomains).value).to eql([ 'example.com']) }
     end
   end
 end
