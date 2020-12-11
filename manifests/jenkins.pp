f1823448bf715786df050c50157faee65ef73117
Kristof Willaert <kristof@publiq.be>
2020-12-11 18:57:43 +0100

    Add common jenkins class

---
+++ manifests/jenkins.pp
@@ -0,0 +1,19 @@
+class profiles::jenkins {
+
+  contain ::profiles
+
+  include ::profiles::apt::keys
+
+  @apt::source { 'publiq-jenkins':
+    location => "http://apt.uitdatabank.be/jenkins-${environment}",
+    release  => $facts['lsbdistcodename'],
+    repos    => 'main',
+    require  => Class['profiles::apt::keys'],
+    include  => {
+      'deb' => true,
+      'src' => false
+    }
+  }
+
+  @profiles::apt::update { 'publiq-jenkins': }
+}
