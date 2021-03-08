9da19dd6b269636419ec508fd6264af8f6267f62
Kristof Willaert <kristof@publiq.be>
2021-03-08 10:42:54 +0100

    Add deployment for prototypes

---
+++ spec/classes/deployment/prototypes_spec.rb
@@ -0,0 +1,72 @@
+require 'spec_helper'
+
+describe 'profiles::deployment::prototypes' do
+  include_examples 'operating system support', 'profiles::deployment::prototypes'
+
+  on_supported_os.each do |os, facts|
+   context "on #{os}" do
+      let (:facts) { facts }
+
+      it { is_expected.to compile.with_all_deps }
+
+      it { is_expected.to contain_class('profiles::apt::keys') }
+
+      it { is_expected.to contain_apt__source('publiq-prototypes').with(
+        'location' => 'https://apt.publiq.be/prototypes-production',
+        'ensure'   => 'present',
+        'repos'    => 'main',
+        'include'  => {
+           'deb' => 'true',
+           'src' => 'false'
+        }
+      ) }
+
+      it { is_expected.to contain_apt__source('publiq-prototypes').that_requires('Class[profiles::apt::keys]') }
+      it { is_expected.to contain_profiles__apt__update('publiq-prototypes') }
+
+      it { is_expected.to contain_package('prototypes-publiq').that_requires('Profiles::Apt::Update[publiq-prototypes]') }
+
+      case facts[:os]['release']['major']
+      when '14.04'
+        let (:facts) { facts }
+
+        it { is_expected.to contain_apt__source('publiq-prototypes').with(
+          'release' => 'trusty'
+        ) }
+
+      when '16.04'
+        let (:facts) { facts }
+
+        it { is_expected.to contain_apt__source('publiq-prototypes').with(
+          'release' => 'xenial'
+        ) }
+      end
+
+      context "without parameters" do
+        let (:params) { {} }
+
+        it { is_expected.to contain_package('prototypes-publiq').with(
+          'ensure' => 'latest'
+        ) }
+      end
+
+      context "with package_version => 1.2.3 and puppetdb_url => http://example.com:8000" do
+        let (:params) { {
+          'package_version' => '1.2.3',
+          'puppetdb_url'    => 'http://example.com:8000'
+        } }
+
+        it { is_expected.to contain_package('prototypes-publiq').with(
+          'ensure' => '1.2.3'
+        ) }
+
+        it { is_expected.to contain_profiles__deployment__versions('profiles::deployment::prototypes').with(
+          'project'         => 'prototypes',
+          'packages'        => 'prototypes-publiq',
+          'destination_dir' => '/var/run',
+          'puppetdb_url'    => 'http://example.com:8000'
+        ) }
+      end
+    end
+  end
+end
