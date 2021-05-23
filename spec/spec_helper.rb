18a9d58f3b0848c9d999eb9060793bf264bd8634
Kristof Willaert <kristof@publiq.be>
2021-05-24 00:43:53 +0200

    Reorder spec_helper.rb to get rid of deprecation warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
+RSpec.configure do |c|
+  c.mock_with :rspec
+  c.after(:suite) do
+    RSpec::Puppet::Coverage.report!
+  end
+end
+
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
 
@@ -6,11 +13,3 @@ include RspecPuppetFacts
 add_custom_fact :staging_http_get, 'curl'
 
 Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
-
-RSpec.configure do |c|
-  c.mock_with :rspec
-  c.after(:suite) do
-    RSpec::Puppet::Coverage.report!
-  end
-end
-
