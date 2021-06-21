082abda9ae7b16ea5861b73decab00b66216fcdf
Kristof Willaert <kristof@publiq.be>
2021-06-21 15:05:11 +0200

    Refactor aptly profile and include unit tests

--- manifests/aptly.pp
+++ manifests/aptly.pp
@@ -1,14 +1,13 @@
 class profiles::aptly (
-  $hostname,
-  $awsaccesskeyid = '',
-  $awssecretaccesskey = '',
-  $sslchain = '',
-  $sslcert = '',
-  $sslkey = '',
-  $gpgkey_source = '',
-  $gpgkey_fingerprint = '',
-  $data_dir = '/var/aptly',
-  $repositories = []
+  String                         $api_hostname,
+  String                         $certificate,
+  Hash                           $signing_keys      = {},
+  String                         $version           = 'latest',
+  String                         $data_dir          = '/var/aptly',
+  Stdlib::Ipv4                   $api_bind          = '127.0.0.1',
+  Stdlib::Port::Unprivileged     $api_port          = 8081,
+  Hash                           $publish_endpoints = {},
+  Variant[String, Array[String]] $repositories      = []
 ) {
 
   contain ::profiles
@@ -16,10 +15,7 @@ class profiles::aptly (
   include ::profiles::users
   include ::profiles::groups
   include ::profiles::packages
-  include ::profiles::firewall
-
-  $api_bind = '127.0.0.1'
-  $api_port = 8081
+  include ::profiles::apt::updates
 
   realize Group['aptly']
   realize User['aptly']
@@ -28,7 +24,19 @@ class profiles::aptly (
 
   realize Profiles::Apt::Update['aptly']
 
-  class { 'aptly':
+  $signing_keys.each |$name, $attributes| {
+    gnupg_key { $name:
+      ensure     => 'present',
+      key_id     => $attributes['id'],
+      user       => 'aptly',
+      key_source => $attributes['source'],
+      key_type   => 'private',
+      require    => User['aptly']
+    }
+  }
+
+  class { '::aptly':
+    version              => $version,
     install_repo         => false,
     manage_user          => false,
     root_dir             => $data_dir,
@@ -38,87 +46,30 @@ class profiles::aptly (
     api_port             => $api_port,
     api_nolock           => true,
     require              => [ Profiles::Apt::Update['aptly'], User['aptly']],
-    s3_publish_endpoints =>
-    {
-      'apt.publiq.be' =>
-      {
-        'region'             => 'eu-west-1',
-        'bucket'             => 'apt.publiq.be',
-        'awsAccessKeyID'     => $awsaccesskeyid,
-        'awsSecretAccessKey' => $awssecretaccesskey
-      }
-    }
+    s3_publish_endpoints => $publish_endpoints
   }
 
   if versioncmp( $facts['os']['release']['major'], '16.04') >= 0 {
     systemd::unit_file { 'aptly-api.service':
       content => template('profiles/aptly/aptly-api.service.erb'),
       enable  => true,
-      active  => true
+      active  => true,
+      require => Class['aptly']
     }
   }
 
-  realize Firewall['300 accept HTTP traffic']
-  realize Firewall['300 accept HTTPS traffic']
-
-  class { 'apache':
-    default_vhost => false
+  profiles::apache::vhost::redirect { $api_hostname:
+    destination => "https://${api_hostname}"
   }
 
-  apache::vhost { "${hostname}_80":
-    docroot         => '/var/www/html',
-    manage_docroot  => false,
-    port            => '80',
-    servername      => $hostname,
-    redirect_source => '/',
-    redirect_dest   => "https://${hostname}",
-    redirect_status => 'permanent'
+  profiles::apache::vhost::reverse_proxy { $api_hostname:
+    https       => true,
+    certificate => $certificate,
+    destination => "http://${api_bind}:${api_port}/"
   }
 
-  apache::vhost { "${hostname}_443":
-    docroot             => '/var/www/html',
-    manage_docroot      => false,
-    proxy_preserve_host => true,
-    port                => '443',
-    servername          => $hostname,
-    ssl                 => true,
-    ssl_cert            => $sslcert,
-    ssl_chain           => $sslchain,
-    ssl_key             => $sslkey,
-    proxy_pass          =>
-    {
-      path =>  '/',
-      url  => "http://${api_bind}:${api_port}/"
-    },
-    require             => [
-      File[$sslchain],
-      File[$sslcert],
-      File[$sslkey],
-    ]
-  }
-
-  file { '/home/aptly/private.key':
-    ensure  => 'file',
-    owner   => 'aptly',
-    group   => 'aptly',
-    mode    => '0644',
-    require => User['aptly'],
-    source  => $gpgkey_source
-  }
-
-  #Install the gpg key for the aptly user to sign the published packages.
-  exec { 'import_gpg_secret_key':
-    command => 'gpg --import /home/aptly/private.key',
-    path    => [ '/usr/local/bin', '/usr/bin', '/bin' ],
-    unless  => "test ${gpgkey_fingerprint} = $(gpg --list-secret-keys --with-colons --fingerprint | egrep '^fpr' | cut -d : -f 10)",
-    user    => 'aptly',
-    group   => 'aptly',
-    require => File['/home/aptly/private.key']
-  }
-
-  any2array($repositories).each |$repo| {
+  [$repositories].flatten.each |$repo| {
     aptly::repo { $repo:
-      ensure            => 'present',
       default_component => 'main'
     }
   }
---
+++ spec/classes/aptly_spec.rb
@@ -0,0 +1,207 @@
+require 'spec_helper'
+
+describe 'profiles::aptly' do
+  context "with api_hostname => aptly.example.com and certificate => wildcard.example.com" do
+    let(:params) { {
+      'api_hostname' => 'aptly.example.com',
+      'certificate'  => 'wildcard.example.com'
+    } }
+
+    let(:pre_condition) {
+      '@profiles::certificate { "wildcard.example.com": certificate_source => "/tmp/cert/foo", key_source => "/tmp/cert/key"}'
+    }
+
+    context "without optional parameters" do
+      include_examples 'operating system support', 'profiles::aptly'
+
+      on_supported_os.each do |os, facts|
+        context "on #{os}" do
+          let(:facts) { facts }
+
+          it { is_expected.to compile.with_all_deps }
+
+          it { is_expected.to contain_group('aptly') }
+          it { is_expected.to contain_user('aptly') }
+          it { is_expected.to contain_package('graphviz') }
+          it { is_expected.to contain_profiles__apt__update('aptly') }
+
+          it { is_expected.to have_gnupg_key_resource_count(0) }
+          it { is_expected.to have_aptly__repo_resource_count(0) }
+
+          it { is_expected.to contain_class('aptly').with(
+            'version'              => 'latest',
+            'install_repo'         => false,
+            'manage_user'          => false,
+            'user'                 => 'aptly',
+            'group'                => 'aptly',
+            'root_dir'             => '/var/aptly',
+            'enable_service'       => false,
+            'enable_api'           => true,
+            'api_bind'             => '127.0.0.1',
+            'api_port'             => '8081',
+            'api_nolock'           => true,
+            's3_publish_endpoints' => {}
+          )}
+
+          it { is_expected.to contain_profiles__apache__vhost__redirect('aptly.example.com').with(
+            'destination' => 'https://aptly.example.com'
+          )}
+
+          it { is_expected.to contain_profiles__apache__vhost__reverse_proxy('aptly.example.com').with(
+            'https'       => true,
+            'certificate' => 'wildcard.example.com',
+            'destination' => 'http://127.0.0.1:8081/'
+          )}
+
+          case facts[:os]['release']['major']
+          when '14.04'
+            let (:facts) { facts }
+
+            it { is_expected.to have_systemd__unit_file_resource_count(0) }
+          when '16.04'
+            let (:facts) { facts }
+
+            it { is_expected.to contain_systemd__unit_file('aptly-api.service').with(
+              'enable' => true,
+              'active' => true
+            ) }
+
+            it { is_expected.to contain_systemd__unit_file('aptly-api.service').with_content(/WorkingDirectory=\/var\/aptly/) }
+            it { is_expected.to contain_systemd__unit_file('aptly-api.service').with_content(/ExecStart=\/usr\/bin\/aptly api serve -listen=127.0.0.1:8081 -no-lock/) }
+
+            it { is_expected.to contain_systemd__unit_file('aptly-api.service').that_requires('Class[aptly]') }
+          end
+
+          it { is_expected.to contain_class('aptly').that_requires('User[aptly]') }
+          it { is_expected.to contain_class('aptly').that_requires('Profiles::Apt::Update[aptly]') }
+        end
+      end
+    end
+
+    context "with api_hostname => foo.example.com and certificate => foo.example.com" do
+      let(:params) { {
+        'api_hostname' => 'foo.example.com',
+        'certificate'  => 'foo.example.com'
+      } }
+
+      let(:pre_condition) {
+        '@profiles::certificate { "foo.example.com": certificate_source => "/tmp/cert/foo", key_source => "/tmp/cert/key"}'
+      }
+
+      on_supported_os.each do |os, facts|
+        context "on #{os}" do
+          let(:facts) { facts }
+
+          context "with signing_keys => { 'test' => { 'id' => '1234ABCD', 'source' => '/tmp/test.key' }}, version => 1.2.3, data_dir => '/data/aptly', api_bind => 1.2.3.4, api_port => 8080 and repositories => [ 'foo', 'bar']" do
+            let(:params) { super().merge(
+              {
+                'signing_keys' => { 'test' => { 'id' => '1234ABCD', 'source' => '/tmp/test.key' }},
+                'version'      => '1.2.3',
+                'data_dir'     => '/data/aptly',
+                'api_bind'     => '1.2.3.4',
+                'api_port'     => 8080,
+                'repositories' => [ 'foo', 'bar']
+              }
+            )}
+
+            it { is_expected.to contain_gnupg_key('test').with(
+              'ensure'     => 'present',
+              'key_id'     => '1234ABCD',
+              'user'       => 'aptly',
+              'key_source' => '/tmp/test.key',
+              'key_type'   => 'private'
+            ) }
+
+            it { is_expected.to contain_class('aptly').with(
+              'version'  => '1.2.3',
+              'root_dir' => '/data/aptly',
+              'api_bind' => '1.2.3.4',
+              'api_port' => 8080
+            ) }
+
+            it { is_expected.to contain_profiles__apache__vhost__redirect('foo.example.com').with(
+              'destination' => 'https://foo.example.com'
+            )}
+
+            it { is_expected.to contain_profiles__apache__vhost__reverse_proxy('foo.example.com').with(
+              'https'       => true,
+              'certificate' => 'foo.example.com',
+              'destination' => 'http://1.2.3.4:8080/'
+            )}
+
+            it { is_expected.to contain_aptly__repo('foo').with(
+              'default_component' => 'main'
+            ) }
+
+            case facts[:os]['release']['major']
+            when '16.04'
+              let (:facts) { facts }
+
+              it { is_expected.to contain_systemd__unit_file('aptly-api.service').with_content(/WorkingDirectory=\/data\/aptly/) }
+              it { is_expected.to contain_systemd__unit_file('aptly-api.service').with_content(/ExecStart=\/usr\/bin\/aptly api serve -listen=1.2.3.4:8080 -no-lock/) }
+            end
+
+            it { is_expected.to contain_gnupg_key('test').that_requires('User[aptly]') }
+          end
+
+          context "with signing_keys => { 'test1' => { 'id' => '6789DEFG', 'source' => '/tmp/test1.key' }, 'test2' => { 'id' => '1234ABCD', 'source' => '/tmp/test2.key' }}, publish_endpoints => { 'apt1' => { 'region' => 'eu-west-1', bucket => 'apt1', awsAccessKeyID => '123', awsSecretAccessKey => 'abc' }} and repositories => 'baz'" do
+            let(:params) { super().merge(
+              {
+                'signing_keys'      => {
+                   'test1' => { 'id' => '6789DEFG', 'source' => '/tmp/test1.key' },
+                   'test2' => { 'id' => '1234ABCD', 'source' => '/tmp/test2.key' }
+                 },
+                'publish_endpoints' => {
+                   'apt1' => {
+                     'region' => 'eu-west-1',
+                     'bucket' => 'apt1',
+                     'awsAccessKeyID' => '123',
+                     'awsSecretAccessKey' => 'abc'
+                   }
+                 },
+                'repositories'      => 'baz'
+              }
+            ) }
+
+            it { is_expected.to contain_gnupg_key('test1').with(
+              'ensure'     => 'present',
+              'key_id'     => '6789DEFG',
+              'user'       => 'aptly',
+              'key_source' => '/tmp/test1.key',
+              'key_type'   => 'private'
+            ) }
+
+            it { is_expected.to contain_gnupg_key('test2').with(
+              'ensure'     => 'present',
+              'key_id'     => '1234ABCD',
+              'user'       => 'aptly',
+              'key_source' => '/tmp/test2.key',
+              'key_type'   => 'private'
+            ) }
+
+            it { is_expected.to contain_class('aptly').with(
+              's3_publish_endpoints' => { 'apt1' => {
+                                            'region' => 'eu-west-1',
+                                            'bucket' => 'apt1',
+                                            'awsAccessKeyID' => '123',
+                                            'awsSecretAccessKey' => 'abc'
+                                          }
+                                        }
+            ) }
+
+            it { is_expected.to contain_aptly__repo('baz').with(
+              'default_component' => 'main'
+            ) }
+          end
+        end
+      end
+    end
+  end
+
+  context "without parameters" do
+    let(:params) { { } }
+
+    it { expect { catalogue }.to raise_error(Puppet::ParseError, /expects a value for parameter 'api_hostname'/) }
+    it { expect { catalogue }.to raise_error(Puppet::ParseError, /expects a value for parameter 'certificate'/) }
+  end
+end
