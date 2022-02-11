a1e5737320e4c3d8539be70b6c79b7b772ab4e13
Kristof Willaert <kristof@publiq.be>
2022-02-11 19:02:40 +0100

    Check for OS compatibility on users and groups

--- spec/classes/groups_spec.rb
+++ spec/classes/groups_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe 'profiles::groups' do
+  include_examples 'operating system support'
+
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) { facts }
--- spec/classes/users_spec.rb
+++ spec/classes/users_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe 'profiles::users' do
+  include_examples 'operating system support'
+
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) { facts }
