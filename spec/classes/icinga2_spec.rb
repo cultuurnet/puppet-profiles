a75a14e23c6796c7c8a164f05ca04d463a1d7566
Kristof Willaert <kristof@publiq.be>
2022-04-22 17:04:21 +0200

    Add required repository for icinga2-plugins-systemd-service

--- spec/classes/icinga2_spec.rb
+++ spec/classes/icinga2_spec.rb
@@ -15,9 +15,12 @@ describe 'profiles::icinga2' do
         it { is_expected.to_not contain_package('icinga2-plugins-systemd-service') }
       when '16.04'
 
+        it { is_expected.to contain_apt__source('cultuurnet-tools') }
         it { is_expected.to contain_package('icinga2-plugins-systemd-service').with(
           'ensure' => 'present'
         ) }
+
+        it { is_expected.to contain_package('icinga2-plugins-systemd-service').that_requires('Apt::Source[cultuurnet-tools]') }
       end
     end
   end
