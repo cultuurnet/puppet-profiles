7087a8c0742fcd5fd86fbe716b5d9b24e0d4252e
Paul Herbosch <paul.herbosch@publiq.be>
2022-05-30 14:40:34 +0200

    trying to fix syntax errors. escaping double quote issue?

--- manifests/newrelic-php5.pp
+++ manifests/newrelic-php5.pp
@@ -11,15 +11,13 @@ class profiles::newrelic-php5 (
   # https://docs.newrelic.com/docs/apm/agents/php-agent/installation/php-agent-installation-ubuntu-debian/
   #
   exec { 'update_newrelic_app_name':
-    command     => "echo ${package_name} ${package_name}/application-name string \"${newrelic_app_name}\" | debconf-set-selections",
+    command     => "echo ${package_name} ${package_name}/application-name string ${newrelic_app_name} | debconf-set-selections",
     path        => ['/usr/bin'],
-    onlyif      => "test 0 -eq $(debconf-show ${package_name} | grep ${package_name/application-name | wc -l)",
-    refreshonly => true
+    onlyif      => "test 0 -eq $(debconf-show ${package_name} | grep ${package_name/application-name | wc -l)"
   }
   exec { 'update_newrelic_license_key':
-    command     => "echo ${package_name} ${package_name}/license-key string \"${newrelic_license_key}\" | debconf-set-selections",
+    command     => "echo ${package_name} ${package_name}/license-key string ${newrelic_license_key} | debconf-set-selections",
     path        => ['/usr/bin'],
-    onlyif      => "test 0 -eq $(debconf-show ${package_name} | grep ${package_name/license-key | wc -l)",
-    refreshonly => true
+    onlyif      => "test 0 -eq $(debconf-show ${package_name} | grep ${package_name/license-key | wc -l)"
   }
 }
