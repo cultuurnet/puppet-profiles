f7f88046de1f178d63072749799c297b3eff6f8e
Paul Herbosch <paul.herbosch@publiq.be>
2022-05-30 14:51:51 +0200

    renamed class, fixed syntax error: missing curly brace

---
+++ manifests/newrelic_php5.pp
@@ -0,0 +1,23 @@
+class profiles::newrelic_php5 (
+  String $package_name = 'newrelic-php5',
+  String $newrelic_app_name,
+  String $newrelic_license_key
+) inherits ::profiles {
+
+  realize Apt::Source["newrelic"]
+  realize Package[$package_name]
+
+  # installation instruction:
+  # https://docs.newrelic.com/docs/apm/agents/php-agent/installation/php-agent-installation-ubuntu-debian/
+  #
+  exec { 'update_newrelic_app_name':
+    command     => "echo ${package_name} ${package_name}/application-name string \"${newrelic_app_name}\" | debconf-set-selections",
+    path        => ['/usr/bin'],
+    onlyif      => "test 0 -eq $(debconf-show ${package_name} | grep ${package_name}/application-name | wc -l)",
+  }
+  exec { 'update_newrelic_license_key':
+    command     => "echo ${package_name} ${package_name}/license-key string \"${newrelic_license_key}\" | debconf-set-selections",
+    path        => ['/usr/bin'],
+    onlyif      => "test 0 -eq $(debconf-show ${package_name} | grep ${package_name}/license-key | wc -l)",
+  }
+}
