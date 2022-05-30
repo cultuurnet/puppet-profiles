6fa5d6cf854a054c4cf09078efea9966d80b9ffe
Paul Herbosch <paul.herbosch@publiq.be>
2022-05-30 14:29:20 +0200

    fix syntax error

--- manifests/newrelic-php5.pp
+++ manifests/newrelic-php5.pp
@@ -5,17 +5,17 @@ class profiles::newrelic-php5 (
   realize Apt::Source["newrelic"]
   realize Package[$package_name]
 
-  # do something with debconf-set-selections to set appname and license key
-  # see: https://docs.newrelic.com/docs/apm/agents/php-agent/installation/php-agent-installation-ubuntu-debian/
+  # installation instruction:
+  # https://docs.newrelic.com/docs/apm/agents/php-agent/installation/php-agent-installation-ubuntu-debian/
   #
   exec { 'update_newrelic_app_name':
     command     => "echo ${package_name} ${package_name}/application-name string \"${newrelic_app_name} \" | debconf-set-selections",
     path        => ['/usr/bin'],
     onlyif      => "test 0 -eq $(debconf-show ${package_name} | grep ${package_name/application-name | wc -l)",
-    refreshonly => true,
+    refreshonly => true
   }
   exec { 'update_newrelic_license_key':
-    command     => "echo ${package_name} ${package_name}/license-key string \"${newrelic_license_key} \" | debconf-set-selections",
+    command     => "echo ${package_name} ${package_name}/license-key string \"${newrelic_license_key}\" | debconf-set-selections",
     path        => ['/usr/bin'],
     onlyif      => "test 0 -eq $(debconf-show ${package_name} | grep ${package_name/license-key | wc -l)",
     refreshonly => true
