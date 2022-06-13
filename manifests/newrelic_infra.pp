961c56993e874f7d4003d7789f3faeb0b842b43c
Paul Herbosch <paul.herbosch@publiq.be>
2022-06-13 16:01:24 +0200

    new profile newrelic_infra: first draft

---
+++ manifests/newrelic_infra.pp
@@ -0,0 +1,40 @@
+class profiles::newrelic_infra (
+  String $license_key,
+  Optional[Variant[Hash]] $integrations = undef
+) {
+
+  class { 'newrelic_infra::agent':
+    ensure      => 'latest',
+    license_key => $license_key
+  }
+
+  if $integrations {
+    class { 'newrelic_infra::integrations':
+      integrations => $integrations
+    }
+
+    if $integrations['newrelic-infra-integretions'] {
+      notice("TODO: deploy infra config files")
+    }
+
+    if $integrations['nri-mysql'] {
+      notice("TODO: deploy mysql config files")
+    }
+
+    if $integrations['nri-redis'] {
+      notice("TODO: deploy redis config files")
+    }
+
+    if $integrations['nri-cassandra'] {
+      notice("TODO: deploy cassandra config files")
+    }
+
+    if $integrations['nri-apache'] {
+      notice("TODO: deploy apache config files")
+    }
+
+    if $integrations['nri-nginx'] {
+      notice("TODO: deploy nginx config files")
+    }
+  }
+}
