cf39bf5b8a2ff97a79e971b755caf0060c970c39
Kristof Willaert <kristof@publiq.be>
2022-07-18 19:47:30 +0200

    Don't manage user and group in component module, but make sure they are declared

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -1,6 +1,11 @@
 class profiles::apache inherits ::profiles {
 
-  class { '::apache':
+  realize Group['www-data']
+  realize User['www-data']
 
+  class { '::apache':
+    manage_group => false,
+    manage_user  => false,
+    require      => [Group['www-data'], User['www-data']]
   }
 }
