fc4c4d8a33c335113a70613bb9aacbe365412010
Kristof Willaert <kristof@publiq.be>
2022-08-11 15:16:02 +0200

    Update unit tests for rename balie -> uitpas-balie project

---
+++ spec/unit/facter/uitpas-balie_deployment_version_spec.rb
@@ -0,0 +1,71 @@
+require 'spec_helper'
+
+describe Facter::Util::Fact do
+  before(:each) { Facter.clear }
+
+  describe 'uitpas-balie_deployment_version' do
+    context 'without packages' do
+      before :each do
+        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package}:${Version}:${Pipeline-Version}:${Git-Ref}\\n' -W 'uitpas-balie-*' 2> /dev/null") { '' }
+      end
+
+      it do
+        expect(Facter.fact('uitpas-balie_deployment_version').value).to eq(nil)
+      end
+    end
+
+    context 'with package uitpas-balie-api:2022.07.12.124306+sha.28ee098:: and no custom metadata' do
+      before :each do
+        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package}:${Version}:${Pipeline-Version}:${Git-Ref}\\n' -W 'uitpas-balie-*' 2> /dev/null") { 'uitpas-balie-api:2022.07.12.124306+sha.28ee098::' }
+      end
+
+      it do
+        expect(Facter.fact('uitpas-balie_deployment_version').value).to eq({"uitpas-balie-api" => { "commit" => "28ee098", "pipeline" => "2022.07.12.124306", "version" => "2022.07.12.124306+sha.28ee098" }})
+      end
+    end
+
+    context 'with package uitpas-balie-api:2022.07.12.124306+sha.28ee098:2022.07.12.124306:28ee098 and custom metadata' do
+      before :each do
+        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package}:${Version}:${Pipeline-Version}:${Git-Ref}\\n' -W 'uitpas-balie-*' 2> /dev/null") { 'uitpas-balie-api:2022.07.12.124306+sha.28ee098:2022.07.12.124306:28ee098' }
+      end
+
+      it do
+        expect(Facter.fact('uitpas-balie_deployment_version').value).to eq({"uitpas-balie-api" => { "commit" => "28ee098", "pipeline" => "2022.07.12.124306", "version" => "2022.07.12.124306+sha.28ee098" }})
+      end
+    end
+
+    context 'with package uitpas-balie-api:2022.07.12.124306+sha.28ee098:: and uitpas-balie-frontend:2022.08.10.132311+sha.fbf71d6:: and no custom metadata' do
+      before :each do
+        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package}:${Version}:${Pipeline-Version}:${Git-Ref}\\n' -W 'uitpas-balie-*' 2> /dev/null") {
+          "uitpas-balie-api:2022.07.12.124306+sha.28ee098::\nuitpas-balie-frontend:2022.08.10.132311+sha.fbf71d6::"
+        }
+      end
+
+      it do
+        expect(Facter.fact('uitpas-balie_deployment_version').value).to eq(
+          {
+            "uitpas-balie-api" => { "commit" => "28ee098", "pipeline" => "2022.07.12.124306", "version" => "2022.07.12.124306+sha.28ee098" },
+            "uitpas-balie-frontend" => { "commit" => "fbf71d6", "pipeline" => "2022.08.10.132311", "version" => "2022.08.10.132311+sha.fbf71d6" }
+          }
+        )
+      end
+    end
+
+    context 'with package uitpas-balie-api:2022.07.12.124306+sha.28ee098:2022.07.12.124306:28ee098 and uitpas-balie-frontend:2022.08.10.132311+sha.fbf71d6:2022.08.10.132311:fbf71d6 and custom metadata' do
+      before :each do
+        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package}:${Version}:${Pipeline-Version}:${Git-Ref}\\n' -W 'uitpas-balie-*' 2> /dev/null") {
+          "uitpas-balie-api:2022.07.12.124306+sha.28ee098:2022.07.12.124306:28ee098\nuitpas-balie-frontend:2022.08.10.132311+sha.fbf71d6:2022.08.10.132311:fbf71d6"
+        }
+      end
+
+      it do
+        expect(Facter.fact('uitpas-balie_deployment_version').value).to eq(
+          {
+            "uitpas-balie-api" => { "commit" => "28ee098", "pipeline" => "2022.07.12.124306", "version" => "2022.07.12.124306+sha.28ee098" },
+            "uitpas-balie-frontend" => { "commit" => "fbf71d6", "pipeline" => "2022.08.10.132311", "version" => "2022.08.10.132311+sha.fbf71d6" }
+          }
+        )
+      end
+    end
+  end
+end
