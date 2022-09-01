c24264e0a30341b9f5939528088befd33d5257e8
Paul Herbosch <paul.herbosch@publiq.be>
2022-09-01 12:03:30 +0200

    remove publiq-uitidv2 apt source from spectest

--- spec/classes/deployment/uitidv2/backend_spec.rb
+++ spec/classes/deployment/uitidv2/backend_spec.rb
@@ -14,7 +14,6 @@ describe 'profiles::deployment::uitidv2::backend' do
 
         it { is_expected.to compile.with_all_deps }
 
-        it { is_expected.to contain_apt__source('publiq-uitidv2') }
         it { is_expected.to contain_apt__source('uitid-api') }
 
         it { is_expected.to contain_package('uitid-api').with( 'ensure' => 'latest') }
