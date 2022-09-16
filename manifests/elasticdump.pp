fd6344d29e6b17b8eaaa3f9ec15d55e501b31f26
Kristof Willaert <kristof@publiq.be>
2022-09-16 17:38:01 +0200

    Get elasticdump from temporary publiq-tools-xenial repository

--- manifests/elasticdump.pp
+++ manifests/elasticdump.pp
@@ -3,8 +3,17 @@ class profiles::elasticdump inherits ::profiles {
   include ::profiles::nodejs
 
   realize Apt::Source['cultuurnet-tools']
+  realize Apt::Source['publiq-tools-xenial']
 
   package { 'elasticdump':
-    require => [ Apt::Source['cultuurnet-tools'], Class['profiles::nodejs']]
+    require => [Apt::Source['cultuurnet-tools'], Apt::Source['publiq-tools-xenial'], Class['profiles::nodejs']]
+  }
+
+  alternative_entry { '/opt/elasticdump/node_modules/elasticdump/bin/elasticdump':
+    ensure   => 'present',
+    altname  => 'elasticdump',
+    priority => 10,
+    altlink  => '/usr/bin/elasticdump',
+    require  => Package['elasticdump']
   }
 }
