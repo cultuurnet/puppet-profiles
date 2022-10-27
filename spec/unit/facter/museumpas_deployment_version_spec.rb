5a4e99a6d64792d4fc44d721083a20db2d9ddc23
Paul Herbosch <paul.herbosch@publiq.be>
2022-10-27 14:06:53 +0200

    add extra facts for packages

--- spec/unit/facter/museumpas_deployment_version_spec.rb
+++ spec/unit/facter/museumpas_deployment_version_spec.rb
@@ -6,7 +6,7 @@ describe Facter::Util::Fact do
   describe 'museumpas_deployment_version' do
     context 'without packages' do
       before :each do
-        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package}:${Version}:${Pipeline-Version}:${Git-Ref}\\n' -W 'museumpas-*' 2> /dev/null") { '' }
+        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package};${Version};${Pipeline-Version};${Git-Ref};${Build-Url};${Homepage}\\n' -W 'museumpas-*' 2> /dev/null") { '' }
       end
 
       it do
@@ -16,37 +16,37 @@ describe Facter::Util::Fact do
 
     context 'with package museumpas-website:20200609124800+sha.692e9e0 and no custom metadata' do
       before :each do
-        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package}:${Version}:${Pipeline-Version}:${Git-Ref}\\n' -W 'museumpas-*' 2> /dev/null") { 'museumpas-website:20200609124800+sha.692e9e0::' }
+        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package};${Version};${Pipeline-Version};${Git-Ref};${Build-Url};${Homepage}\\n' -W 'museumpas-*' 2> /dev/null") { 'museumpas-website;20200609124800+sha.692e9e0;;;;' }
       end
 
       it do
-        expect(Facter.fact(:museumpas_deployment_version).value).to eq({"museumpas-website" => { "commit" => "692e9e0", "pipeline" => "20200609124800", "version" => "20200609124800+sha.692e9e0" }})
+        expect(Facter.fact(:museumpas_deployment_version).value).to eq({"museumpas-website" => { "commit" => "692e9e0", "pipeline" => "20200609124800", "version" => "20200609124800+sha.692e9e0", "build_url" => "", "homepage" => "" }})
       end
     end
 
     context 'with package museumpas-website:20200609124800+sha.692e9e0 and custom metadata' do
       before :each do
-        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package}:${Version}:${Pipeline-Version}:${Git-Ref}\\n' -W 'museumpas-*' 2> /dev/null") { 'museumpas-website:20200609124800+sha.692e9e0:20200609124800:692e9e0' }
+        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package};${Version};${Pipeline-Version};${Git-Ref};${Build-Url};${Homepage}\\n' -W 'museumpas-*' 2> /dev/null") { 'museumpas-website;20200609124800+sha.692e9e0;20200609124800;692e9e0;https://jenkins.com/test/job;;' }
       end
 
       it do
-        expect(Facter.fact(:museumpas_deployment_version).value).to eq({"museumpas-website" => { "commit" => "692e9e0", "pipeline" => "20200609124800", "version" => "20200609124800+sha.692e9e0" }})
+        expect(Facter.fact(:museumpas_deployment_version).value).to eq({"museumpas-website" => { "commit" => "692e9e0", "pipeline" => "20200609124800", "version" => "20200609124800+sha.692e9e0", "build_url" => "https://jenkins.com/test/job", "homepage" => "" }})
       end
     end
 
     context 'with package museumpas-database:20180810152730, museumpas-files:20180816150116 and museumpas-website:20200609124800+sha.692e9e0 and no custom metadata' do
       before :each do
-        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package}:${Version}:${Pipeline-Version}:${Git-Ref}\\n' -W 'museumpas-*' 2> /dev/null") {
-          "museumpas-database:20180810152730::\nmuseumpas-files:20180816150116::\nmuseumpas-website:20200609124800+sha.692e9e0::"
+        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package};${Version};${Pipeline-Version};${Git-Ref};${Build-Url};${Homepage}\\n' -W 'museumpas-*' 2> /dev/null") {
+          "museumpas-database;20180810152730;;;;;\nmuseumpas-files;20180816150116;;;;;\nmuseumpas-website;20200609124800+sha.692e9e0;;;;"
         }
       end
 
       it do
         expect(Facter.fact(:museumpas_deployment_version).value).to eq(
           {
-            "museumpas-database" => { "version" => "20180810152730", "pipeline" => "20180810152730" },
-            "museumpas-files" => { "version" => "20180816150116", "pipeline" => "20180816150116" },
-            "museumpas-website" => { "commit" => "692e9e0", "pipeline" => "20200609124800", "version" => "20200609124800+sha.692e9e0" }
+            "museumpas-database" => { "version" => "20180810152730", "pipeline" => "20180810152730", "build_url" => "", "homepage" => "" },
+            "museumpas-files" => { "version" => "20180816150116", "pipeline" => "20180816150116", "build_url" => "", "homepage" => "" },
+            "museumpas-website" => { "commit" => "692e9e0", "pipeline" => "20200609124800", "version" => "20200609124800+sha.692e9e0", "build_url" => "", "homepage" => "" }
           }
         )
       end
@@ -54,17 +54,17 @@ describe Facter::Util::Fact do
 
     context 'with package museumpas-database:20180810152730, museumpas-files:20180816150116 and museumpas-website:20200609124800+sha.692e9e0 and custom metadata' do
       before :each do
-        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package}:${Version}:${Pipeline-Version}:${Git-Ref}\\n' -W 'museumpas-*' 2> /dev/null") {
-          "museumpas-database:20180810152730:20180810152730:\nmuseumpas-files:20180816150116:20180816150116:\nmuseumpas-website:20200609124800+sha.692e9e0:20200609124800:692e9e0"
+        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package};${Version};${Pipeline-Version};${Git-Ref};${Build-Url};${Homepage}\\n' -W 'museumpas-*' 2> /dev/null") {
+          "museumpas-database;20180810152730;20180810152730;;;;\nmuseumpas-files;20180816150116;20180816150116;;;;\nmuseumpas-website;20200609124800+sha.692e9e0;20200609124800;692e9e0;;https://github.com/cultuurnet/myrepo"
         }
       end
 
       it do
         expect(Facter.fact(:museumpas_deployment_version).value).to eq(
           {
-            "museumpas-database" => { "version" => "20180810152730", "pipeline" => "20180810152730" },
-            "museumpas-files" => { "version" => "20180816150116", "pipeline" => "20180816150116" },
-            "museumpas-website" => { "commit" => "692e9e0", "pipeline" => "20200609124800", "version" => "20200609124800+sha.692e9e0" }
+            "museumpas-database" => { "version" => "20180810152730", "pipeline" => "20180810152730", "build_url" => "", "homepage" => "" },
+            "museumpas-files" => { "version" => "20180816150116", "pipeline" => "20180816150116", "build_url" => "", "homepage" => "" },
+            "museumpas-website" => { "commit" => "692e9e0", "pipeline" => "20200609124800", "version" => "20200609124800+sha.692e9e0", "build_url" => "", "homepage" => "https://github.com/cultuurnet/myrepo" }
           }
         )
       end
--- spec/unit/facter/uitpas-balie_deployment_version_spec.rb
+++ spec/unit/facter/uitpas-balie_deployment_version_spec.rb
@@ -6,7 +6,7 @@ describe Facter::Util::Fact do
   describe 'uitpas-balie_deployment_version' do
     context 'without packages' do
       before :each do
-        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package}:${Version}:${Pipeline-Version}:${Git-Ref}\\n' -W 'uitpas-balie-*' 2> /dev/null") { '' }
+        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package};${Version};${Pipeline-Version};${Git-Ref};${Build-Url};${Homepage}\\n' -W 'uitpas-balie-*' 2> /dev/null") { '' }
       end
 
       it do
@@ -16,36 +16,36 @@ describe Facter::Util::Fact do
 
     context 'with package uitpas-balie-api:2022.07.12.124306+sha.28ee098:: and no custom metadata' do
       before :each do
-        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package}:${Version}:${Pipeline-Version}:${Git-Ref}\\n' -W 'uitpas-balie-*' 2> /dev/null") { 'uitpas-balie-api:2022.07.12.124306+sha.28ee098::' }
+        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package};${Version};${Pipeline-Version};${Git-Ref};${Build-Url};${Homepage}\\n' -W 'uitpas-balie-*' 2> /dev/null") { 'uitpas-balie-api;2022.07.12.124306+sha.28ee098;;;;;' }
       end
 
       it do
-        expect(Facter.fact('uitpas-balie_deployment_version').value).to eq({"uitpas-balie-api" => { "commit" => "28ee098", "pipeline" => "2022.07.12.124306", "version" => "2022.07.12.124306+sha.28ee098" }})
+        expect(Facter.fact('uitpas-balie_deployment_version').value).to eq({"uitpas-balie-api" => { "commit" => "28ee098", "pipeline" => "2022.07.12.124306", "version" => "2022.07.12.124306+sha.28ee098", "build_url" => "", "homepage" => "" }})
       end
     end
 
     context 'with package uitpas-balie-api:2022.07.12.124306+sha.28ee098:2022.07.12.124306:28ee098 and custom metadata' do
       before :each do
-        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package}:${Version}:${Pipeline-Version}:${Git-Ref}\\n' -W 'uitpas-balie-*' 2> /dev/null") { 'uitpas-balie-api:2022.07.12.124306+sha.28ee098:2022.07.12.124306:28ee098' }
+        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package};${Version};${Pipeline-Version};${Git-Ref};${Build-Url};${Homepage}\\n' -W 'uitpas-balie-*' 2> /dev/null") { 'uitpas-balie-api;2022.07.12.124306+sha.28ee098;2022.07.12.124306;28ee098;https://jenkins.com/test/job;https://github.com/cultuurnet/test' }
       end
 
       it do
-        expect(Facter.fact('uitpas-balie_deployment_version').value).to eq({"uitpas-balie-api" => { "commit" => "28ee098", "pipeline" => "2022.07.12.124306", "version" => "2022.07.12.124306+sha.28ee098" }})
+        expect(Facter.fact('uitpas-balie_deployment_version').value).to eq({"uitpas-balie-api" => { "commit" => "28ee098", "pipeline" => "2022.07.12.124306", "version" => "2022.07.12.124306+sha.28ee098", "build_url" => "https://jenkins.com/test/job", "homepage" => "https://github.com/cultuurnet/test" }})
       end
     end
 
     context 'with package uitpas-balie-api:2022.07.12.124306+sha.28ee098:: and uitpas-balie-frontend:2022.08.10.132311+sha.fbf71d6:: and no custom metadata' do
       before :each do
-        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package}:${Version}:${Pipeline-Version}:${Git-Ref}\\n' -W 'uitpas-balie-*' 2> /dev/null") {
-          "uitpas-balie-api:2022.07.12.124306+sha.28ee098::\nuitpas-balie-frontend:2022.08.10.132311+sha.fbf71d6::"
+        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package};${Version};${Pipeline-Version};${Git-Ref};${Build-Url};${Homepage}\\n' -W 'uitpas-balie-*' 2> /dev/null") {
+          "uitpas-balie-api;2022.07.12.124306+sha.28ee098;;;;\nuitpas-balie-frontend;2022.08.10.132311+sha.fbf71d6;;;;"
         }
       end
 
       it do
         expect(Facter.fact('uitpas-balie_deployment_version').value).to eq(
           {
-            "uitpas-balie-api" => { "commit" => "28ee098", "pipeline" => "2022.07.12.124306", "version" => "2022.07.12.124306+sha.28ee098" },
-            "uitpas-balie-frontend" => { "commit" => "fbf71d6", "pipeline" => "2022.08.10.132311", "version" => "2022.08.10.132311+sha.fbf71d6" }
+            "uitpas-balie-api" => { "commit" => "28ee098", "pipeline" => "2022.07.12.124306", "version" => "2022.07.12.124306+sha.28ee098", "build_url" => "", "homepage" => "" },
+            "uitpas-balie-frontend" => { "commit" => "fbf71d6", "pipeline" => "2022.08.10.132311", "version" => "2022.08.10.132311+sha.fbf71d6", "build_url" => "", "homepage" => "" }
           }
         )
       end
@@ -53,16 +53,16 @@ describe Facter::Util::Fact do
 
     context 'with package uitpas-balie-api:2022.07.12.124306+sha.28ee098:2022.07.12.124306:28ee098 and uitpas-balie-frontend:2022.08.10.132311+sha.fbf71d6:2022.08.10.132311:fbf71d6 and custom metadata' do
       before :each do
-        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package}:${Version}:${Pipeline-Version}:${Git-Ref}\\n' -W 'uitpas-balie-*' 2> /dev/null") {
-          "uitpas-balie-api:2022.07.12.124306+sha.28ee098:2022.07.12.124306:28ee098\nuitpas-balie-frontend:2022.08.10.132311+sha.fbf71d6:2022.08.10.132311:fbf71d6"
+        expect(Facter::Util::Resolution).to receive(:exec).with("dpkg-query -f='${binary:Package};${Version};${Pipeline-Version};${Git-Ref};${Build-Url};${Homepage}\\n' -W 'uitpas-balie-*' 2> /dev/null") {
+          "uitpas-balie-api;2022.07.12.124306+sha.28ee098;2022.07.12.124306;28ee098;;;\nuitpas-balie-frontend;2022.08.10.132311+sha.fbf71d6;2022.08.10.132311;fbf71d6;https://jenkins.com/test/job;https://github.com/cultuurnet/test"
         }
       end
 
       it do
         expect(Facter.fact('uitpas-balie_deployment_version').value).to eq(
           {
-            "uitpas-balie-api" => { "commit" => "28ee098", "pipeline" => "2022.07.12.124306", "version" => "2022.07.12.124306+sha.28ee098" },
-            "uitpas-balie-frontend" => { "commit" => "fbf71d6", "pipeline" => "2022.08.10.132311", "version" => "2022.08.10.132311+sha.fbf71d6" }
+            "uitpas-balie-api" => { "commit" => "28ee098", "pipeline" => "2022.07.12.124306", "version" => "2022.07.12.124306+sha.28ee098", "build_url" => "", "homepage" => "" },
+            "uitpas-balie-frontend" => { "commit" => "fbf71d6", "pipeline" => "2022.08.10.132311", "version" => "2022.08.10.132311+sha.fbf71d6", "build_url" => "https://jenkins.com/test/job", "homepage" => "https://github.com/cultuurnet/test" }
           }
         )
       end
