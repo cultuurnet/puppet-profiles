422d85e2dc1b72ca11a1a98bb35d9c6ed71fb1fb
Paul Herbosch <paul.herbosch@publiq.be>
2022-11-07 11:02:02 +0100

    [III-5083] aptly repo definition for udb3 angular app

--- spec/classes/apt/repositories_spec.rb
+++ spec/classes/apt/repositories_spec.rb
@@ -438,6 +438,17 @@ describe 'profiles::apt::repositories' do
               'release'      => 'xenial'
             ) }
 
+            it { is_expected.to contain_apt__source('uitdatabank-angular-app').with(
+              'location'     => 'https://apt.publiq.be/uitdatabank-angular-app-acceptance',
+              'ensure'       => 'present',
+              'repos'        => 'main',
+              'include'      => {
+                'deb' => 'true',
+                'src' => 'false'
+              },
+              'release'      => 'xenial'
+            ) }
+
             it { is_expected.to contain_apt__source('uitdatabank-newsletter-api').with(
               'location'     => 'https://apt.publiq.be/uitdatabank-newsletter-api-acceptance',
               'ensure'       => 'present',
