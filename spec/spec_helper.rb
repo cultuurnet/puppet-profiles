243e93107858d52d009fc369186410a4b1547153
Kristof Willaert <kristof@publiq.be>
2022-11-22 15:59:19 +0100

    Mock systemd service running on MacOS

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,21 @@ RSpec.configure do |rspec|
   rspec.expect_with :rspec do |c|
     c.max_formatted_output_length = nil
   end
+  # The systemd service provider is confined to platforms that are running systemd,
+  # by checking the presence and contents of the file '/proc/1/comm'.
+  # This makes sense for a real puppet agent, but breaks when running spec tests on
+  # a platform that does not use systemd (like MacOS). We mock the calls the confine
+  # statement makes to simulate running systemd.
+  #
+  # For more information, see: https://tickets.puppetlabs.com/browse/PUP-11167
+  if RUBY_PLATFORM =~ /darwin/i
+    rspec.before(:each) do
+      allow(Puppet::FileSystem).to receive(:exist?).and_call_original
+      allow(Puppet::FileSystem).to receive(:exist?).with('/proc/1/comm').and_return(true)
+      allow(Puppet::FileSystem).to receive(:read).and_call_original
+      allow(Puppet::FileSystem).to receive(:read).with('/proc/1/comm').and_return(['systemd'])
+    end
+  end
 end
 
 require 'puppetlabs_spec_helper/module_spec_helper'
