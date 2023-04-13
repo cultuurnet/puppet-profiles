e8fc2b3b4b8798b771f9e4599d13cf5bfe3bb4de
Kristof Willaert <kristof@publiq.be>
2023-04-13 10:53:16 +0200

    Remove unused apt GPG key 'Infra Cultuurnet'

--- spec/classes/apt/keys_spec.rb
+++ spec/classes/apt/keys_spec.rb
@@ -13,11 +13,6 @@ describe 'profiles::apt::keys' do
 
         it { is_expected.to compile.with_all_deps }
 
-        it { is_expected.to contain_apt__key('Infra CultuurNet').with(
-          'id'     => '2380EA3E50D3776DFC1B03359F4935C80DC9EA95',
-          'source' => 'https://apt.publiq.be/gpgkey/cultuurnet.gpg.key'
-        ) }
-
         it { is_expected.to contain_apt__key('publiq Infrastructure').with(
           'id'     => 'AD726BD2A48017B060AA43FA4A49242DE36CDCAF',
           'source' => 'https://apt.publiq.be/gpgkey/publiq.gpg.key'
