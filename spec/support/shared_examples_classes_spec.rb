579b43c1bf6fa3fa04e1c6fd343600fbc9057d12
Kristof Willaert <kristof@publiq.be>
2023-04-13 10:46:10 +0200

    Update modules that provide core types and augeasproviders

--- spec/support/shared_examples_classes_spec.rb
+++ spec/support/shared_examples_classes_spec.rb
@@ -12,15 +12,15 @@ RSpec.shared_examples "operating system support" do
   end
 
   context 'on an unsupported operating system release' do
-    describe 'without any extra parameters on Ubuntu 12.04' do
+    describe 'without any extra parameters on Ubuntu 16.04' do
       let(:facts) do
         {
           :operatingsystem        => 'Ubuntu',
-          :operatingsystemrelease => '12.04'
+          :operatingsystemrelease => '16.04'
         }
       end
 
-      it { expect { catalogue }.to raise_error(Puppet::Error, /Ubuntu 12.04 not supported/) }
+      it { expect { catalogue }.to raise_error(Puppet::Error, /Ubuntu 16.04 not supported/) }
     end
   end
 end
