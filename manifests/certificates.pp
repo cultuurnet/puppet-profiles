fc50fc3fb0161f6630a55bbd37cc2a70aa4277f6
Kristof Willaert <kristof@publiq.be>
2023-04-26 16:52:19 +0200

    Split and simplify certificates classes

--- manifests/certificates.pp
+++ manifests/certificates.pp
@@ -1,24 +1,10 @@
 class profiles::certificates (
-  Hash                           $certificates             = {},
-  Variant[String, Array[String]] $disabled_ca_certificates = []
+  Hash $certificates = {}
 ) inherits ::profiles {
-
-  include ::profiles::certificates::update
 
   $certificates.each |$certificate, $sources| {
     @profiles::certificate { $certificate:
       * => $sources
-    }
-  }
-
-  [$disabled_ca_certificates].flatten.each |$certificate| {
-    augeas { "Disable CA certificate ${certificate}":
-      lens    => 'Simplelines.lns',
-      incl    => '/etc/ca-certificates.conf',
-      context => '/files/etc/ca-certificates.conf',
-      onlyif  => "get *[.= '${certificate}'] == '${certificate}'",
-      changes => "set *[.= '${certificate}'] '!${certificate}'",
-      notify => Class['profiles::certificates::update']
     }
   }
 }
---
+++ spec/classes/ca_certificates_spec.rb
@@ -0,0 +1,76 @@
+require 'spec_helper'
+
+describe 'profiles::ca_certificates' do
+  include_examples 'operating system support'
+
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) { facts }
+
+      context "without parameters" do
+        let(:params) { {} }
+
+        it { is_expected.to compile.with_all_deps }
+
+        it { is_expected.to contain_class('profiles::ca_certificates').with(
+          'disabled_ca_certificates' => []
+        ) }
+
+        it { is_expected.to have_augeas_resource_count(0) }
+      end
+
+      context "with disabled_ca_certificates => 'foobar'" do
+        let(:params) { {
+            'disabled_ca_certificates' => 'foobar'
+        } }
+
+        it { is_expected.to contain_augeas('Disable CA certificate foobar').with(
+          'lens'    => 'Simplelines.lns',
+          'incl'    => '/etc/ca-certificates.conf',
+          'context' => '/files/etc/ca-certificates.conf',
+          'onlyif'  => 'get *[.= \'foobar\'] == \'foobar\'',
+          'changes' => 'set *[.= \'foobar\'] \'!foobar\'',
+        ) }
+
+        it { is_expected.to contain_exec('Update CA certificates').with(
+          'command'     => 'update-ca-certificates',
+          'path'        => [ '/usr/local/bin', '/usr/bin', '/usr/sbin', '/bin'],
+          'refreshonly' => true
+        ) }
+
+        it { is_expected.to contain_augeas('Disable CA certificate foobar').that_notifies('Exec[Update CA certificates]') }
+      end
+
+      context "with disabled_ca_certificates => [ 'badcert', 'expiredcert']" do
+        let(:params) { {
+          'disabled_ca_certificates' => ['badcert', 'expiredcert']
+        } }
+
+        it { is_expected.to contain_augeas('Disable CA certificate badcert').with(
+          'lens'    => 'Simplelines.lns',
+          'incl'    => '/etc/ca-certificates.conf',
+          'context' => '/files/etc/ca-certificates.conf',
+          'onlyif'  => 'get *[.= \'badcert\'] == \'badcert\'',
+          'changes' => 'set *[.= \'badcert\'] \'!badcert\'',
+        ) }
+
+        it { is_expected.to contain_augeas('Disable CA certificate expiredcert').with(
+          'lens'    => 'Simplelines.lns',
+          'incl'    => '/etc/ca-certificates.conf',
+          'context' => '/files/etc/ca-certificates.conf',
+          'onlyif'  => 'get *[.= \'expiredcert\'] == \'expiredcert\'',
+          'changes' => 'set *[.= \'expiredcert\'] \'!expiredcert\'',
+        ) }
+
+        it { is_expected.to contain_exec('Update CA certificates').with(
+          'command'     => 'update-ca-certificates',
+          'path'        => [ '/usr/local/bin', '/usr/bin', '/usr/sbin', '/bin'],
+          'refreshonly' => true
+        ) }
+
+        it { is_expected.to contain_augeas('Disable CA certificate badcert').that_notifies('Exec[Update CA certificates]') }
+        it { is_expected.to contain_augeas('Disable CA certificate expiredcert').that_notifies('Exec[Update CA certificates]') }
+      end
+    end
+  end
+end
