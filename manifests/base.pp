f4b4f078c0f21f6f1777d16f38f12d4b835ee188
Kristof Willaert <kristof@publiq.be>
2023-04-28 16:13:17 +0200

    Remove setting environment variables from base profile

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,9 +1,5 @@
 class profiles::base inherits ::profiles {
 
-  Shellvar {
-    target  => '/etc/environment'
-  }
-
   if $facts['ec2_metadata'] {
     $admin_user = 'ubuntu'
     realize Package['awscli']
@@ -16,17 +12,5 @@ class profiles::base inherits ::profiles {
 
   class { '::profiles::sudo':
     admin_user => $admin_user
-  }
-
-  shellvar { 'system PATH':
-    ensure   => 'present',
-    variable => 'PATH',
-    value    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/puppetlabs/bin'
-  }
-
-  shellvar { 'system RUBYLIB':
-    ensure   => 'present',
-    variable => 'RUBYLIB',
-    value    => '/opt/puppetlabs/puppet/lib/ruby/vendor_ruby'
   }
 }
--- spec/classes/base_spec.rb
+++ spec/classes/base_spec.rb
@@ -9,22 +9,6 @@ describe 'profiles::base' do
 
       it { is_expected.to compile.with_all_deps }
 
-      it { is_expected.to contain_shellvar('system PATH').with(
-        'ensure'   => 'present',
-        'variable' => 'PATH',
-        'target'   => '/etc/environment',
-        'value'    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/puppetlabs/bin',
-        )
-      }
-
-      it { is_expected.to contain_shellvar('system RUBYLIB').with(
-        'ensure'   => 'present',
-        'variable' => 'RUBYLIB',
-        'target'   => '/etc/environment',
-        'value'    => '/opt/puppetlabs/puppet/lib/ruby/vendor_ruby'
-        )
-      }
-
       context "on AWS EC2" do
         let(:facts) do
           super().merge({ 'ec2_metadata' => 'true' })
