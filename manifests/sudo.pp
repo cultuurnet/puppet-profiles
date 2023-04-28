06e024b436774fd3e03cd3c842e664e7ecf3e91f
Kristof Willaert <kristof@publiq.be>
2023-04-28 17:05:34 +0200

    Move remaining code from base profile to sudo profile

--- manifests/sudo.pp
+++ manifests/sudo.pp
@@ -1,13 +1,18 @@
-class profiles::sudo (
-  Optional[String] $admin_user = undef
-) inherits ::profiles {
+class profiles::sudo inherits ::profiles {
 
   class { '::sudo': }
 
-  if $admin_user {
-    sudo::conf { $admin_user:
-      priority => '10',
-      content  => "${admin_user} ALL=(ALL) NOPASSWD: ALL"
-    }
+  $admin_user = $facts['ec2_metadata'] ? {
+    true    => 'ubuntu',
+    default => 'vagrant'
+  }
+
+  realize Group[$admin_user]
+  realize User[$admin_user]
+
+  sudo::conf { $admin_user:
+    priority => '10',
+    content  => "${admin_user} ALL=(ALL) NOPASSWD: ALL",
+    require  => [Class['sudo'], User[$admin_user]]
   }
 }
