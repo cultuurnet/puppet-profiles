e1a6831e001e91e3168e8eb9e153931c47c860ab
Kristof Willaert <kristof@publiq.be>
2023-05-26 14:07:00 +0200

    Add profile for puppetdb deployment

---
+++ manifests/puppet/puppetdb.pp
@@ -0,0 +1,44 @@
+class profiles::puppet::puppetdb (
+  String                     $version           = 'installed',
+  Optional[String]           $initial_heap_size = undef,
+  Optional[String]           $maximum_heap_size = undef,
+  Enum['running', 'stopped'] $service_status    = 'running'
+
+) inherits ::profiles {
+
+  $initial_heap_size_arg = $initial_heap_size ? { undef => {}, default => { '-Xms' => $initial_heap_size } }
+  $maximum_heap_size_arg = $maximum_heap_size ? { undef => {}, default => { '-Xmx' => $maximum_heap_size } }
+
+  $java_args = {} + $initial_heap_size_arg + $maximum_heap_size_arg
+
+  include profiles::firewall::rules
+
+  realize Group['postgres']
+  realize User['postgres']
+  realize Group['puppetdb']
+  realize User['puppetdb']
+  realize Apt::Source['puppet']
+
+  realize Firewall['300 accept puppetdb HTTPS traffic']
+
+  contain profiles::java
+
+  class { 'puppetdb::globals':
+    version => $version
+  }
+
+  class { 'puppetdb::database::postgresql':
+    manage_package_repo => false,
+    postgres_version    => '12',
+    listen_addresses    => '127.0.0.1',
+    require             => [Group['postgres'], User['postgres'], Class['puppetdb::globals']]
+  }
+
+  class { 'puppetdb::server':
+    database_host           => '127.0.0.1',
+    manage_firewall         => false,
+    puppetdb_service_status => $service_status,
+    java_args               => $java_args,
+    require                 => [Group['puppetdb'], User['puppetdb'], Apt::Source['puppet'], Class['profiles::java'], Class['puppetdb::globals'], Class['puppetdb::database::postgresql']]
+  }
+}
