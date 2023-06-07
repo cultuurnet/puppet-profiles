23f8db20aaa68c095a46139a439031200a9bd950
Kristof Willaert <kristof@publiq.be>
2023-06-07 09:44:24 +0200

    Use the puppet certificate to authenticate against puppetserver when clearing the cache

--- spec/classes/puppet/puppetserver/cache_clear_spec.rb
+++ spec/classes/puppet/puppetserver/cache_clear_spec.rb
@@ -4,16 +4,54 @@ describe 'profiles::puppet::puppetserver::cache_clear' do
   include_examples 'operating system support'
 
   on_supported_os.each do |os, facts|
-   context "on #{os}" do
+    context "on #{os}" do
       let(:facts) { facts }
 
-      it { is_expected.to compile.with_all_deps }
+      context "on host foo.example.com" do
+        let(:node) { 'foo.example.com' }
 
-      it { is_expected.to contain_exec('puppetserver_environment_cache_clear').with(
-        'command'     => 'curl -i -k --fail -X DELETE https://localhost:8140/puppet-admin-api/v1/environment-cache',
-        'path'        => [ '/usr/local/bin', '/usr/bin', '/bin' ],
-        'refreshonly' => true,
-      ) }
+        context "without parameters" do
+          let(:params) { {} }
+
+          context "without hieradata" do
+            let(:hiera_config) { 'spec/support/hiera/empty.yaml' }
+
+            it { is_expected.to compile.with_all_deps }
+
+            it { is_expected.not_to contain_exec('puppetserver_environment_cache_clear') }
+          end
+
+          context "with hieradata" do
+            let(:hiera_config) { 'spec/support/hiera/common.yaml' }
+
+            it { is_expected.to compile.with_all_deps }
+
+            it { is_expected.to contain_exec('puppetserver_environment_cache_clear').with(
+              'command'     => 'curl -i --cert /etc/puppetlabs/puppet/ssl/certs/foo.example.com.pem --key /etc/puppetlabs/puppet/ssl/private_keys/foo.example.com.pem --cacert /etc/puppetlabs/puppet/ssl/certs/ca.pem --fail -X DELETE https://puppetserver.example.com:8140/puppet-admin-api/v1/environment-cache',
+              'path'        => [ '/usr/local/bin', '/usr/bin', '/bin' ],
+              'refreshonly' => true,
+            ) }
+          end
+        end
+      end
+
+      context "on host bar.example.com" do
+        let(:node) { 'bar.example.com' }
+
+        context "without parameters" do
+          let(:params) { {} }
+
+          context "with hieradata" do
+            let(:hiera_config) { 'spec/support/hiera/common.yaml' }
+
+            it { is_expected.to contain_exec('puppetserver_environment_cache_clear').with(
+              'command'     => 'curl -i --cert /etc/puppetlabs/puppet/ssl/certs/bar.example.com.pem --key /etc/puppetlabs/puppet/ssl/private_keys/bar.example.com.pem --cacert /etc/puppetlabs/puppet/ssl/certs/ca.pem --fail -X DELETE https://puppetserver.example.com:8140/puppet-admin-api/v1/environment-cache',
+              'path'        => [ '/usr/local/bin', '/usr/bin', '/bin' ],
+              'refreshonly' => true,
+            ) }
+          end
+        end
+      end
     end
   end
 end
