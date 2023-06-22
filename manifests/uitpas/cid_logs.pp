0dd2170457651115f85f9745cef8afb423db21f3
Paul Herbosch <paul.herbosch@publiq.be>
2023-06-22 13:50:52 +0200

    rename and update uitpas cid logs profile

--- manifests/uitpas/cid_logs.pp
+++ manifests/uitpas/cid_logs.pp
@@ -1,10 +1,19 @@
-class profiles::uitpas_cid_logs (
+class profiles::uitpas::cid_logs (
   Stdlib::Httpurl            $url             = 'http://cidlogs.publiq.be',
   Stdlib::Ipv4               $service_address = '127.0.0.1',
   Stdlib::Port::Unprivileged $service_port    = 8080,
+  String                     $data_dir        = '/data/cidlogs'
 ) inherits ::profiles {
 
   profiles::apache::vhost::reverse_proxy { $url:
     destination => "http://${service_address}:${service_port}/"
+  }
+
+  file { $data_dir:
+    ensure => 'directory', 
+    owner  => 'logstash',
+    group  => 'logstash',
+    mode   => '0755',
+    require => [Package['logstash']]
   }
 }
