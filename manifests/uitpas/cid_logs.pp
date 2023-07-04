926243c58c03a07a8c1ff5ef52d753c0b3f10cbc
Kristof Willaert <kristof@publiq.be>
2023-07-04 11:41:25 +0200

    Make the new name the canonical one, the old one the alias

--- manifests/uitpas/cid_logs.pp
+++ manifests/uitpas/cid_logs.pp
@@ -1,10 +1,10 @@
 class profiles::uitpas::cid_logs (
-  String                        $hostname        = 'cidmonitor.lodgon.com',
-  Variant[String,Array[String]] $aliases         = 'cidlogs.publiq.be',
+  String                        $hostname        = 'cidlogs.publiq.be',
+  Variant[String,Array[String]] $aliases         = 'cidmonitor.lodgon.com',
   Stdlib::Ipv4                  $service_address = '127.0.0.1',
   Stdlib::Port::Unprivileged    $service_port    = 8080,
   String                        $data_dir        = '/data/cidlogs',
-  String                        $gcs_credentials,
+  String                        $gcs_credentials
 ) inherits ::profiles {
 
   realize Group['logstash']
@@ -22,16 +22,16 @@ class profiles::uitpas::cid_logs (
     group   => 'logstash',
     mode    => '0640',
     source  => $gcs_credentials,
-    require => [User['logstash'],Package['logstash']],
+    require => [User['logstash'], Package['logstash']],
     notify  => Service['logstash']
   }
 
   file { $data_dir:
-    ensure => 'directory', 
+    ensure => 'directory',
     owner  => 'logstash',
     group  => 'logstash',
     mode   => '0755',
-    require => [User['logstash'],Package['logstash']]
+    require => [User['logstash'], Package['logstash']]
   }
 
   cron { 'remove-old-cidlogs':
