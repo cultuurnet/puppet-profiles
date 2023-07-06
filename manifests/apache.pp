4a15ec8b78772ec529ba9b8ad683b17770498ed0
Kristof Willaert <kristof@publiq.be>
2023-07-06 16:22:43 +0200

    Allow different apache mpm modules and configuration

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -1,13 +1,15 @@
 class profiles::apache (
-  Boolean                    $metrics        = true,
-  Enum['running', 'stopped'] $service_status = 'running'
+  Enum['event', 'itk', 'peruser', 'prefork', 'worker']  $mpm_module        = 'prefork',
+  Hash                                                  $mpm_module_config = {},
+  Boolean                                               $metrics           = true,
+  Enum['running', 'stopped']                            $service_status    = 'running'
 ) inherits ::profiles {
 
   realize Group['www-data']
   realize User['www-data']
 
   class { '::apache':
-    mpm_module     => 'prefork',
+    mpm_module     => false,
     manage_group   => false,
     manage_user    => false,
     default_vhost  => true,
@@ -18,6 +20,10 @@ class profiles::apache (
                         'stopped' => false
                       },
     require        => [Group['www-data'], User['www-data']]
+  }
+
+  class { "apache::mod::${mpm_module}":
+    * => $mpm_module_config
   }
 
   if $metrics {
--- spec/classes/apache_spec.rb
+++ spec/classes/apache_spec.rb
@@ -11,15 +11,17 @@ describe 'profiles::apache' do
         it { is_expected.to compile.with_all_deps }
 
         it { is_expected.to contain_class('profiles::apache').with(
-          'service_status' => 'running',
-          'metrics'        => true
+          'mpm_module'        => 'prefork',
+          'mpm_module_config' => {},
+          'service_status'    => 'running',
+          'metrics'           => true
         ) }
 
         it { is_expected.to contain_group('www-data') }
         it { is_expected.to contain_user('www-data') }
 
         it { is_expected.to contain_class('apache').with(
-          'mpm_module'     => 'prefork',
+          'mpm_module'     => false,
           'manage_group'   => false,
           'manage_user'    => false,
           'default_vhost'  => true,
@@ -27,6 +29,8 @@ describe 'profiles::apache' do
           'service_ensure' => 'running',
           'service_enable' => true
         ) }
+
+        it { is_expected.to contain_class('apache::mod::prefork') }
 
         it { is_expected.to contain_class('profiles::apache::metrics') }
         it { is_expected.to contain_class('profiles::apache::logging') }
@@ -37,20 +41,27 @@ describe 'profiles::apache' do
         it { is_expected.to contain_user('www-data').that_comes_before('Class[apache]') }
       end
 
-      context "with metrics => false" do
+      context "with mpm_module => worker, mpm_module_config => { startservers => 8, maxclients => 256 }, service_status => stopped and metrics => false" do
         let(:params) { {
-          'service_status' => 'stopped',
-          'metrics'        => false
+          'mpm_module'        => 'worker',
+          'mpm_module_config' => { 'startservers' => 8, 'maxclients' => 256 },
+          'service_status'    => 'stopped',
+          'metrics'           => false
         } }
 
         it { is_expected.to contain_class('apache').with(
-          'mpm_module'     => 'prefork',
+          'mpm_module'     => false,
           'manage_group'   => false,
           'manage_user'    => false,
           'default_vhost'  => true,
           'service_manage' => true,
           'service_ensure' => 'stopped',
           'service_enable' => false
+        ) }
+
+        it { is_expected.to contain_class('apache::mod::worker').with(
+          'startservers' => 8,
+          'maxclients'   => 256
         ) }
 
         it { is_expected.not_to contain_class('profiles::apache::metrics') }
