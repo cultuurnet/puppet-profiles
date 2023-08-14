2d115526099eedde6020b6e7c23bab063c50ee48
Kristof Willaert <kristof@publiq.be>
2023-08-14 17:04:30 +0200

    Remove redundant declarations from apache::vhost::generic

--- manifests/apache/vhost/generic.pp
+++ manifests/apache/vhost/generic.pp
@@ -2,18 +2,12 @@ class profiles::apache::vhost::generic (
   Hash $vhosts = undef
 ) {
 
-  include ::profiles
   include ::profiles::apache
   include ::profiles::firewall::rules
   include ::profiles::certificates
   include ::apache::mod::proxy
   include ::apache::mod::rewrite
   include ::apache::mod::headers
-  include ::apache::mod::http2
-  include ::apache::mod::event
-
-  realize Group['www-data']
-  realize User['www-data']
 
   realize Firewall['300 accept HTTP traffic']
 
