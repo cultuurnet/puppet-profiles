f189e06a9e095f2338515b761886155fbac45797
Kristof Willaert <kristof@publiq.be>
2023-08-16 16:24:57 +0200

    Simplify uit::frontend::deployment profile

--- manifests/uit/frontend/deployment.pp
+++ manifests/uit/frontend/deployment.pp
@@ -1,18 +1,16 @@
 class profiles::uit::frontend::deployment (
-  String           $config_source,
-  String           $version                 = 'latest',
-  String           $uitdatabank_api_url     = 'http://localhost',
-  String           $repository              = 'uit-frontend',
-  Boolean          $service_manage          = true,
-  String           $service_ensure          = 'running',
-  Boolean          $service_enable          = true,
-  Optional[String] $service_defaults_source = undef,
-  Optional[String] $maintenance_source      = undef,
-  Optional[String] $deployment_source       = undef,
-  Optional[String] $puppetdb_url            = lookup('data::puppet::puppetdb::url', Optional[String], 'first', undef)
+  String                     $config_source,
+  String                     $version                 = 'latest',
+  String                     $uitdatabank_api_url     = 'http://localhost',
+  String                     $repository              = 'uit-frontend',
+  Enum['running', 'stopped'] $service_status          = 'running',
+  Optional[String]           $service_defaults_source = undef,
+  Optional[String]           $maintenance_source      = undef,
+  Optional[String]           $deployment_source       = undef,
+  Optional[String]           $puppetdb_url            = lookup('data::puppet::puppetdb::url', Optional[String], 'first', undef)
 ) inherits ::profiles {
 
-  $basedir = '/var/www/uit-frontend/packages/app'
+  $basedir = '/var/www/uit-frontend'
 
   realize Apt::Source[$repository]
 
@@ -24,7 +22,7 @@ class profiles::uit::frontend::deployment (
 
   file { 'uit-frontend-config':
     ensure  => 'file',
-    path    => "${basedir}/.env",
+    path    => "${basedir}/packages/app/.env",
     owner   => 'www-data',
     group   => 'www-data',
     source  => $config_source,
@@ -33,17 +31,17 @@ class profiles::uit::frontend::deployment (
 
   file { 'uit-frontend-migration-script':
     ensure  => 'file',
-    path    => "${basedir}/../../migrate.sh",
+    path    => "${basedir}/migrate.sh",
     owner   => 'www-data',
     group   => 'www-data',
     mode    => '0755',
-    content => template('profiles/deployment/uit/frontend/migrate.sh.erb')
+    content => template('profiles/uit/frontend/migrate.sh.erb')
   }
 
   if $maintenance_source {
     file { 'uit-maintenance-pages':
       ensure  => 'directory',
-      path    => "${basedir}/../../maintenance",
+      path    => "${basedir}/maintenance",
       recurse => true,
       source  => $maintenance_source,
       owner   => 'www-data',
@@ -55,7 +53,7 @@ class profiles::uit::frontend::deployment (
   if $deployment_source {
     file { 'uit-deployment-pages':
       ensure  => 'directory',
-      path    => "${basedir}/../../deployment",
+      path    => "${basedir}/deployment",
       recurse => true,
       source  => $deployment_source,
       owner   => 'www-data',
@@ -64,25 +62,26 @@ class profiles::uit::frontend::deployment (
     }
   }
 
-  if $service_manage {
-    if $service_defaults_source {
-      file { 'uit-frontend-service-defaults':
-        ensure => 'file',
-        path   => '/etc/default/uit-frontend',
-        owner  => 'root',
-        group  => 'root',
-        source => $service_defaults_source,
-        notify => Service['uit-frontend']
-      }
+  if $service_defaults_source {
+    file { 'uit-frontend-service-defaults':
+      ensure => 'file',
+      path   => '/etc/default/uit-frontend',
+      owner  => 'root',
+      group  => 'root',
+      source => $service_defaults_source,
+      notify => Service['uit-frontend']
     }
+  }
 
-    service { 'uit-frontend':
-      ensure    => $service_ensure,
-      enable    => $service_enable,
-      require   => Package['uit-frontend'],
-      subscribe => File['uit-frontend-config'],
-      hasstatus => true
-    }
+  service { 'uit-frontend':
+    ensure    => $service_status,
+    enable    => $service_status ? {
+                   'running' => true,
+                   'stopped' => false
+                 },
+    require   => Package['uit-frontend'],
+    subscribe => File['uit-frontend-config'],
+    hasstatus => true
   }
 
   profiles::deployment::versions { $title:
--- spec/classes/uit/frontend/deployment_spec.rb
+++ spec/classes/uit/frontend/deployment_spec.rb
@@ -14,6 +14,17 @@ describe 'profiles::uit::frontend::deployment' do
 
         it { is_expected.to compile.with_all_deps }
 
+        it { is_expected.to contain_class('profiles::uit::frontend::deployment').with(
+          'config_source'           => '/foo',
+          'version'                 => 'latest',
+          'repository'              => 'uit-frontend',
+          'uitdatabank_api_url'     => 'http://localhost',
+          'service_status'          => 'running',
+          'service_defaults_source' => nil,
+          'maintenance_source'      => nil,
+          'deployment_source'       => nil
+        ) }
+
         it { is_expected.to contain_apt__source('uit-frontend') }
 
         it { is_expected.to contain_package('uit-frontend').with( 'ensure' => 'latest') }
@@ -56,27 +67,17 @@ describe 'profiles::uit::frontend::deployment' do
             'puppetdb_url' => 'http://localhost:8081'
           ) }
         end
-
-        context "with service_manage => false" do
-          let(:params) {
-            super().merge({
-              'service_manage' => false
-            } )
-          }
-
-          it { is_expected.not_to contain_service('uit-frontend') }
-        end
       end
     end
   end
 
-  context "with config_source => /bar, version => 1.2.3, service_defaults_source => /baz, service_ensure => stopped, service_enable = false and puppetdb_url => http://example.com:8000" do
+  context "with config_source => /bar, version => 1.2.3, repository => uit-frontend-exotic, service_defaults_source => /baz, service_status => stopped and puppetdb_url => http://example.com:8000" do
     let(:params) { {
       'config_source'           => '/bar',
       'version'                 => '1.2.3',
-      'service_ensure'          => 'stopped',
+      'repository'              => 'uit-frontend-exotic',
       'service_defaults_source' => '/baz',
-      'service_enable'          => false,
+      'service_status'          => 'stopped',
       'puppetdb_url'            => 'http://example.com:8000'
     } }
 
@@ -84,30 +85,36 @@ describe 'profiles::uit::frontend::deployment' do
       context "on #{os}" do
         let(:facts) { facts }
 
-        it { is_expected.to contain_file('uit-frontend-config').with(
-          'source' => '/bar',
-        ) }
+        context "with repository uit-frontend-exotic defined" do
+          let(:pre_condition) { '@apt::source { "uit-frontend-exotic": location => "http://localhost", release => "focal", repos => "main" }' }
 
-        it { is_expected.to contain_file('uit-frontend-service-defaults').with(
-          'ensure' => 'file',
-          'path'   => '/etc/default/uit-frontend',
-          'source' => '/baz',
-          'owner'  => 'root',
-          'group'  => 'root'
-        ) }
+          it { is_expected.to contain_apt__source('uit-frontend-exotic') }
 
-        it { is_expected.to contain_package('uit-frontend').with( 'ensure' => '1.2.3') }
+          it { is_expected.to contain_file('uit-frontend-config').with(
+            'source' => '/bar',
+          ) }
 
-        it { is_expected.to contain_service('uit-frontend').with(
-          'ensure'    => 'stopped',
-          'enable'    => false
-        ) }
+          it { is_expected.to contain_file('uit-frontend-service-defaults').with(
+            'ensure' => 'file',
+            'path'   => '/etc/default/uit-frontend',
+            'source' => '/baz',
+            'owner'  => 'root',
+            'group'  => 'root'
+          ) }
 
-        it { is_expected.to contain_profiles__deployment__versions('profiles::uit::frontend::deployment').with(
-          'puppetdb_url' => 'http://example.com:8000'
-        ) }
+          it { is_expected.to contain_package('uit-frontend').with( 'ensure' => '1.2.3') }
 
-        it { is_expected.to contain_file('uit-frontend-service-defaults').that_notifies('Service[uit-frontend]') }
+          it { is_expected.to contain_service('uit-frontend').with(
+            'ensure'    => 'stopped',
+            'enable'    => false
+          ) }
+
+          it { is_expected.to contain_profiles__deployment__versions('profiles::uit::frontend::deployment').with(
+            'puppetdb_url' => 'http://example.com:8000'
+          ) }
+
+          it { is_expected.to contain_file('uit-frontend-service-defaults').that_notifies('Service[uit-frontend]') }
+        end
       end
     end
   end
