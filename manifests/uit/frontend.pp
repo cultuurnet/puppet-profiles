4f1cfe63d325754c4019c34a42dd2136437c7e69
Kristof Willaert <kristof@publiq.be>
2023-08-23 15:24:42 +0200

    Move apache vhost to uit::frontend profile

--- manifests/uit/frontend.pp
+++ manifests/uit/frontend.pp
@@ -1,13 +1,173 @@
 class profiles::uit::frontend (
-  Boolean $deployment = true
-)  inherits ::profiles {
+  String                    $servername,
+  Array[String]             $serveraliases           = [],
+  Boolean                   $deployment              = true,
+  Stdlib::Ipv4              $service_address         = '127.0.0.1',
+  Integer                   $service_port            = 3000,
+  Optional[String]          $redirect_source         = undef,
+  Optional[Stdlib::HTTPUrl] $uitdatabank_api_url     = undef,
+  Optional[String]          $maintenance_page_source = undef,
+  Optional[String]          $deployment_page_source  = undef
+) inherits ::profiles {
+
+  $basedir = '/var/www/uit-frontend'
+
+  realize Group['www-data']
+  realize User['www-data']
 
   include ::profiles::nodejs
+  include ::profiles::apache
+
+  file { $basedir:
+    ensure  => 'directory',
+    owner   => 'www-data',
+    group   => 'www-data',
+    require => [Group['www-data'], User['www-data'], Class['profiles::apache']]
+  }
+
+  if $redirect_source {
+    file { 'uit-frontend-migration-script':
+      ensure  => 'file',
+      path    => "${basedir}/migrate.sh",
+      owner   => 'www-data',
+      group   => 'www-data',
+      mode    => '0755',
+      content => template('profiles/uit/frontend/migrate.sh.erb'),
+      require => [File[$basedir], Group['www-data'], User['www-data']]
+    }
+
+    file { 'uit-frontend-redirects':
+      ensure  => 'file',
+      path    => "${basedir}/.redirect",
+      owner   => 'www-data',
+      group   => 'www-data',
+      source  => $redirect_source,
+      require => [File[$basedir], Group['www-data'], User['www-data']]
+    }
+
+    $vhost_custom_fragment = "Include ${basedir}/.redirect"
+  } else {
+    $vhost_custom_fragment = undef
+  }
+
+  if $maintenance_page_source {
+    $maintenance_page_location       = '/maintenance'
+    $maintenance_error_code          = 503
+    $rewrite_maintenance_page        = {
+                                         comment      => 'Maintenance page',
+                                         rewrite_cond => [
+                                                           "%{DOCUMENT_ROOT}${maintenance_page_location}/maintenance.html -f",
+                                                           '%{DOCUMENT_ROOT}/maintenance.enabled -f',
+                                                           "%{REQUEST_URI} !^${maintenance_page_location}/"
+                                                         ],
+                                         rewrite_rule => "^ - [R=${maintenance_error_code},L]"
+                                       }
+    $error_document_maintenance_page = {
+                                         error_code => $maintenance_error_code,
+                                         document   => "${maintenance_page_location}/maintenance.html"
+                                       }
+
+    file { 'uit-maintenance-page':
+      ensure  => 'directory',
+      path    => "${basedir}${maintenance_page_location}",
+      recurse => true,
+      source  => $maintenance_source,
+      owner   => 'www-data',
+      group   => 'www-data',
+      require => [File[$basedir], Group['www-data'], User['www-data']]
+    }
+  } else {
+    $maintenance_page_location       = undef
+    $rewrite_maintenance_page        = undef
+    $error_document_maintenance_page = undef
+  }
+
+  if $deployment_page_source {
+    $deployment_page_location       = '/deployment'
+    $deployment_error_code          = 504
+    $rewrite_deployment_page        = {
+                                        comment      => 'Deployment in progress page',
+                                        rewrite_cond => [
+                                                          "%{DOCUMENT_ROOT}${deployment_page_location}/deployment.html -f",
+                                                          '%{DOCUMENT_ROOT}/api.deployment.enabled -f [OR]',
+                                                          '%{DOCUMENT_ROOT}/frontend.deployment.enabled -f [OR]',
+                                                          '%{DOCUMENT_ROOT}/cms.deployment.enabled -f [OR]',
+                                                          '%{DOCUMENT_ROOT}/deployment.enabled -f',
+                                                          "%{REQUEST_URI} !^${deployment_page_location}/"
+                                                        ],
+                                        rewrite_rule => "^ - [R=${deployment_error_code},L]"
+                                      }
+    $error_document_deployment_page = {
+                                        error_code => $deployment_error_code,
+                                        document   => "${deployment_page_location}/deployment.html"
+                                      }
+
+    file { 'uit-deployment-page':
+      ensure  => 'directory',
+      path    => "${basedir}${deployment_page_location}",
+      recurse => true,
+      source  => $deployment_source,
+      owner   => 'www-data',
+      group   => 'www-data',
+      require => [File[$basedir], Group['www-data'], User['www-data']]
+    }
+
+  } else {
+    $deployment_page_location       = undef
+    $rewrite_deployment_page        = undef
+    $error_document_deployment_page = undef
+  }
 
   if $deployment {
     include ::profiles::uit::frontend::deployment
 
     Class['profiles::nodejs'] -> Class['profiles::uit::frontend::deployment']
+    Class['profiles::uit::frontend::deployment'] -> Apache::Vhost["${servername}_80"]
+  }
+
+  apache::vhost { "${servername}_80":
+    servername         => $servername,
+    serveraliases      => $serveraliases,
+    docroot            => $basedir,
+    manage_docroot     => false,
+    request_headers    => [
+                            'unset Proxy early'
+                          ],
+    port               => 80,
+    access_log_format  => 'uiv_frontend_json',
+    access_log_env_var => '!nolog',
+    directories        => [{
+                            path           => '/',
+                            options        => ['Indexes', 'MultiViews'],
+                            allow_override => ['All'],
+                            require        => { enforce => 'all', requires => ['all granted'] }
+                          },
+                          {
+                            path           => '/(css/|img/|js/|icons/|_nuxt/|sw.js)',
+                            provider       => 'locationmatch',
+                            headers        => [
+                                                'set Cache-Control "max-age=31536000, public"',
+                                                'unset Last-Modified "expr=%{REQUEST_URI} =~ m#^/_nuxt/#"'
+                                              ]
+                          }],
+    proxy_pass         => [{
+                            path                => '/',
+                            url                 => "http://${service_address}:${service_port}",
+                            no_proxy_uris       => [$maintenance_page_location, $deployment_page_location].filter |$item| { $item },
+                            no_proxy_uris_match => ['^/(css/|img/|js/|icons/|_nuxt/|sw.js)']
+                          }],
+    aliases            => [{
+                            aliasmatch => '^/(css/|img/|js/|icons/|_nuxt/|sw.js)(.*)$',
+                            path       => "${basedir}/packages/app/.output/public/\$1\$2"
+                          }],
+    rewrites           => [ $rewrite_maintenance_page, $rewrite_deployment_page ].filter |$item| { $item },
+    error_documents    => [ $error_document_maintenance_page, $error_document_deployment_page ].filter |$item| { $item },
+    custom_fragment    => $vhost_custom_fragment,
+    setenvif           => [
+                            'X-Forwarded-Proto "https" HTTPS=on',
+                            'X-Forwarded-For "^(\d{1,3}+\.\d{1,3}+\.\d{1,3}+\.\d{1,3}+).*" CLIENT_IP=$1'
+                          ],
+    require => [Class['profiles::apache'], File['/var/www/uit-frontend']]
   }
 
   # include ::profiles::uit::frontend::monitoring
--- spec/classes/uit/frontend_spec.rb
+++ spec/classes/uit/frontend_spec.rb
@@ -1,46 +1,166 @@
 require 'spec_helper'
 
 describe 'profiles::uit::frontend' do
-  include_examples 'operating system support'
+  context "with servername => foo.example.com" do
+    let(:params) { {
+      'servername' => 'foo.example.com'
+    } }
 
-  on_supported_os.each do |os, facts|
-    context "on #{os}" do
-      let(:facts) { facts }
+    include_examples 'operating system support'
 
-      context "without parameters" do
-        let(:params) { {} }
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) { facts }
+
+        context "without extra parameters" do
+          let(:params) {
+            super().merge({})
+          }
+
+          context "with hieradata" do
+            let(:hiera_config) { 'spec/support/hiera/common.yaml' }
+
+            it { is_expected.to compile.with_all_deps }
+
+            it { is_expected.to contain_class('profiles::uit::frontend').with(
+              'servername'              => 'foo.example.com',
+              'serveraliases'           => [],
+              'deployment'              => true,
+              'service_address'         => '127.0.0.1',
+              'service_port'            => 3000,
+              'redirect_source'         => nil,
+              'uitdatabank_api_url'     => nil,
+              'maintenance_page_source' => nil,
+              'deployment_page_source'  => nil
+            ) }
+
+            it { is_expected.to contain_group('www-data') }
+            it { is_expected.to contain_user('www-data') }
+
+            it { is_expected.to contain_class('profiles::nodejs') }
+            it { is_expected.to contain_class('profiles::apache') }
+
+            it { is_expected.to contain_file('/var/www/uit-frontend').with(
+              'ensure' => 'directory',
+              'owner'  => 'www-data',
+              'group'  => 'www-data'
+            ) }
+
+            it { is_expected.not_to contain_file('uit-frontend-redirects') }
+            it { is_expected.not_to contain_file('uit-frontend-migration-script') }
+            it { is_expected.not_to contain_file('uit-maintenance-page') }
+            it { is_expected.not_to contain_file('uit-deployment-page') }
+
+            it { is_expected.to contain_class('profiles::uit::frontend::deployment').with(
+              'service_address' => '127.0.0.1',
+              'service_port'    => 3000
+            ) }
+
+            it { is_expected.to contain_apache__vhost('foo.example.com_80').with(
+              'servername'         => 'foo.example.com',
+              'serveraliases'      => [],
+              'docroot'            => '/var/www/uit-frontend',
+              'manage_docroot'     => false,
+              'port'               => 80,
+              'access_log_env_var' => '!nolog',
+              'custom_fragment'    => nil,
+              'rewrites'           => [],
+              'error_documents'    => [],
+              'request_headers'    => ['unset Proxy early'],
+              'directories'        => [{
+                                        'path'           => '/',
+                                        'options'        => ['Indexes', 'MultiViews'],
+                                        'allow_override' => ['All'],
+                                        'require'        => { 'enforce' => 'all', 'requires' => ['all granted'] }
+                                      },
+                                      {
+                                        'path'           => '/(css/|img/|js/|icons/|_nuxt/|sw.js)',
+                                        'provider'       => 'locationmatch',
+                                        'headers'        => [
+                                                              'set Cache-Control "max-age=31536000, public"',
+                                                              'unset Last-Modified "expr=%{REQUEST_URI} =~ m#^/_nuxt/#"'
+                                                            ]
+                                      }],
+              'aliases'            => [{
+                                        'aliasmatch' => '^/(css/|img/|js/|icons/|_nuxt/|sw.js)(.*)$',
+                                        'path'       => '/var/www/uit-frontend/packages/app/.output/public/$1$2'
+                                      }],
+              'proxy_pass'         => [{
+                                        'path'                => '/',
+                                        'url'                 => 'http://127.0.0.1:3000',
+                                        'no_proxy_uris'       => [],
+                                        'no_proxy_uris_match' => ['^/(css/|img/|js/|icons/|_nuxt/|sw.js)']
+                                      }],
+              'setenvif'           => [
+                                        'X-Forwarded-Proto "https" HTTPS=on',
+                                        'X-Forwarded-For "^(\d{1,3}+\.\d{1,3}+\.\d{1,3}+\.\d{1,3}+).*" CLIENT_IP=$1'
+                                      ]
+            ) }
+
+            it { is_expected.to contain_file('/var/www/uit-frontend').that_requires('Group[www-data]') }
+            it { is_expected.to contain_file('/var/www/uit-frontend').that_requires('User[www-data]') }
+            it { is_expected.to contain_file('/var/www/uit-frontend').that_requires('Class[profiles::apache]') }
+            it { is_expected.to contain_class('profiles::uit::frontend::deployment').that_requires('Class[profiles::nodejs]') }
+            it { is_expected.to contain_apache__vhost('foo.example.com_80').that_requires('Class[profiles::uit::frontend::deployment]') }
+            it { is_expected.to contain_apache__vhost('foo.example.com_80').that_requires('Class[profiles::apache]') }
+            it { is_expected.to contain_apache__vhost('foo.example.com_80').that_requires('File[/var/www/uit-frontend]') }
+          end
+
+          context "without hieradata" do
+            let(:hiera_config) { 'spec/support/hiera/empty.yaml' }
+
+            it { expect { catalogue }.to raise_error(Puppet::ParseError, /expects a value for parameter 'config_source'/) }
+          end
+        end
+
+        context "with deployment => false" do
+          let(:params) {
+            super().merge( {
+              'deployment' => false
+            } )
+          }
+
+          it { is_expected.to compile.with_all_deps }
+
+          it { is_expected.to contain_class('profiles::nodejs') }
+          it { is_expected.to_not contain_class('profiles::uit::frontend::deployment') }
+        end
+      end
+    end
+  end
+
+  context "with servername => bar.example.com" do
+    let(:params) { {
+      'servername' => 'bar.example.com'
+    } }
+
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) { facts }
 
         context "with hieradata" do
           let(:hiera_config) { 'spec/support/hiera/common.yaml' }
 
-          it { is_expected.to compile.with_all_deps }
-
-          it { is_expected.to contain_class('profiles::uit::frontend').with(
-            'deployment' => true
+          it { is_expected.to contain_apache__vhost('bar.example.com_80').with(
+            'servername' => 'bar.example.com'
           ) }
 
-          it { is_expected.to contain_class('profiles::nodejs') }
-          it { is_expected.to contain_class('profiles::uit::frontend::deployment') }
-
-          it { is_expected.to contain_class('profiles::uit::frontend::deployment').that_requires('Class[profiles::nodejs]') }
-        end
-
-        context "without hieradata" do
-          let(:hiera_config) { 'spec/support/hiera/empty.yaml' }
-
-          it { expect { catalogue }.to raise_error(Puppet::ParseError, /expects a value for parameter 'config_source'/) }
+          it { is_expected.to contain_apache__vhost('bar.example.com_80').that_requires('Class[profiles::uit::frontend::deployment]') }
+          it { is_expected.to contain_apache__vhost('bar.example.com_80').that_requires('Class[profiles::apache]') }
+          it { is_expected.to contain_apache__vhost('bar.example.com_80').that_requires('File[/var/www/uit-frontend]') }
         end
       end
+    end
+  end
 
-      context "with deployment => false" do
-        let(:params) { {
-          'deployment' => false
-        } }
+  context "without parameters" do
+    let(:params) { {} }
 
-        it { is_expected.to compile.with_all_deps }
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) { facts }
 
-        it { is_expected.to contain_class('profiles::nodejs') }
-        it { is_expected.to_not contain_class('profiles::uit::frontend::deployment') }
+        it { expect { catalogue }.to raise_error(Puppet::ParseError, /expects a value for parameter 'servername'/) }
       end
     end
   end
