a16af46b0d3906d1377292987bddd5ed556ea680
Kristof Willaert <kristof@publiq.be>
2023-09-12 12:31:32 +0200

    Add reverse proxy in front of UiTPAS balie

--- manifests/uitpas/balie.pp
+++ manifests/uitpas/balie.pp
@@ -1,10 +1,17 @@
 class profiles::uitpas::balie (
-  Boolean                       $deployment      = true,
-  Stdlib::Ipv4                  $service_address = '127.0.0.1',
-  Integer                       $service_port    = 4000,
+  String                         $servername,
+  Variant[String, Array[String]] $serveraliases   = [],
+  Boolean                        $deployment      = true,
+  Stdlib::Ipv4                   $service_address = '127.0.0.1',
+  Integer                        $service_port    = 4000,
 ) inherits ::profiles {
 
   include ::profiles::nodejs
+
+  profiles::apache::vhost::reverse_proxy { "http://${servername}":
+    destination => "http://${service_address}:${service_port}/",
+    aliases     => $serveraliases
+  }
 
   if $deployment {
     class { 'profiles::uitpas::balie::deployment':
--- spec/classes/uitpas/balie_spec.rb
+++ spec/classes/uitpas/balie_spec.rb
@@ -1,48 +1,71 @@
 require 'spec_helper'
 
 describe 'profiles::uitpas::balie' do
-  include_examples 'operating system support'
+  context "with servername => balie.example.com" do
+    let(:params) { {
+      'servername' => 'balie.example.com'
+    } }
 
-  on_supported_os.each do |os, facts|
-    context "on #{os}" do
-      let(:facts) { facts }
+    include_examples 'operating system support'
 
-      context "without parameters" do
-        let(:params) { {} }
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) { facts }
 
-        context "with hieradata" do
-          let(:hiera_config) { 'spec/support/hiera/common.yaml' }
+        context "without extra parameters" do
+          let(:params) {
+            super().merge({})
+          }
 
-          it { is_expected.to compile.with_all_deps }
+          context "with hieradata" do
+            let(:hiera_config) { 'spec/support/hiera/common.yaml' }
 
-          it { is_expected.to contain_class('profiles::uitpas::balie').with(
-            'deployment'          => true,
-            'service_address'     => '127.0.0.1',
-            'service_port'        => 4000,
-          ) }
+            it { is_expected.to compile.with_all_deps }
 
-          it { is_expected.to contain_class('profiles::nodejs') }
+            it { is_expected.to contain_class('profiles::uitpas::balie').with(
+              'servername'      => 'balie.example.com',
+              'serveraliases'   => [],
+              'deployment'      => true,
+              'service_address' => '127.0.0.1',
+              'service_port'    => 4000
+            ) }
 
-          it { is_expected.to contain_class('profiles::uitpas::balie::deployment').with(
-            'service_address' => '127.0.0.1',
-            'service_port'    => 4000
-          ) }
+            it { is_expected.to contain_class('profiles::nodejs') }
 
-          it { is_expected.to contain_class('profiles::uitpas::balie::deployment').that_requires('Class[profiles::nodejs]') }
-        end
+            it { is_expected.to contain_class('profiles::uitpas::balie::deployment').with(
+              'service_address' => '127.0.0.1',
+              'service_port'    => 4000
+            ) }
 
-        context "without hieradata" do
-          let(:hiera_config) { 'spec/support/hiera/empty.yaml' }
+            it { is_expected.to contain_profiles__apache__vhost__reverse_proxy('http://balie.example.com').with(
+              'destination' => 'http://127.0.0.1:4000/',
+              'aliases'     => []
+            ) }
 
-          it { expect { catalogue }.to raise_error(Puppet::ParseError, /expects a value for parameter 'config_source'/) }
+            it { is_expected.to contain_class('profiles::uitpas::balie::deployment').that_requires('Class[profiles::nodejs]') }
+          end
+
+          context "without hieradata" do
+            let(:hiera_config) { 'spec/support/hiera/empty.yaml' }
+
+            it { expect { catalogue }.to raise_error(Puppet::ParseError, /expects a value for parameter 'config_source'/) }
+          end
         end
       end
+    end
+  end
 
-      context "with service_address => 127.0.1.1 and service_port => 7000" do
-        let(:params) { {
-          'service_address'  => '127.0.1.1',
-          'service_port'     => 7000,
-        } }
+  context "with servername => test.example.com, serveraliases => alias.example.com, service_address => 127.0.1.1 and service_port => 7000" do
+    let(:params) { {
+      'servername'      => 'test.example.com',
+      'serveraliases'   => 'alias.example.com',
+      'service_address' => '127.0.1.1',
+      'service_port'    => 7000
+    } }
+
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) { facts }
 
         context "with hieradata" do
           let(:hiera_config) { 'spec/support/hiera/common.yaml' }
@@ -53,18 +76,25 @@ describe 'profiles::uitpas::balie' do
             'service_address' => '127.0.1.1',
             'service_port'    => 7000
           ) }
+
+          it { is_expected.to contain_profiles__apache__vhost__reverse_proxy('http://test.example.com').with(
+            'destination' => 'http://127.0.1.1:7000/',
+            'aliases'     => 'alias.example.com'
+          ) }
+
+          context "with deployment => false" do
+            let(:params) {
+              super().merge({
+                'deployment' => false
+              })
+            }
+
+            it { is_expected.to compile.with_all_deps }
+
+            it { is_expected.to contain_class('profiles::nodejs') }
+            it { is_expected.to_not contain_class('profiles::uitpas::balie::deployment') }
+          end
         end
-      end
-
-      context "with deployment => false" do
-        let(:params) { {
-          'deployment' => false
-        } }
-
-        it { is_expected.to compile.with_all_deps }
-
-        it { is_expected.to contain_class('profiles::nodejs') }
-        it { is_expected.to_not contain_class('profiles::uitpas::balie::deployment') }
       end
     end
   end
