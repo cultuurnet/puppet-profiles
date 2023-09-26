f509ae7c16b1def5a3c5eda50f9cfdcf1592704c
Kristof Willaert <kristof@publiq.be>
2023-09-26 14:31:42 +0200

    Refactor NodeJS profile to use new mirrors and allow specifying major version only

--- manifests/nodejs.pp
+++ manifests/nodejs.pp
@@ -1,10 +1,15 @@
 class profiles::nodejs (
-  String $version = '14.16.1-1nodesource1'
+  Optional[String] $version       = undef,
+  Integer          $major_version = if $version { Integer(split($version, /\./)[0]) } else { 16 }
 ) inherits ::profiles {
 
-  $major_version = split($version, /\./)[0]
+  if ($version and $major_version) {
+    if Integer(split($version, /\./)[0]) != $major_version {
+      fail("Profiles::Nodejs: incompatible combination of 'version' and 'major_version' parameters")
+    }
+  }
 
-  realize Apt::Source["publiq-nodejs-${major_version}"]
+  realize Apt::Source["nodejs-${major_version}"]
   realize Apt::Source['publiq-tools']
 
   realize Package['yarn']
@@ -14,6 +19,6 @@ class profiles::nodejs (
     nodejs_package_ensure => $version
   }
 
-  Apt::Source["publiq-nodejs-${major_version}"] -> Class['nodejs']
+  Apt::Source["nodejs-${major_version}"] -> Class['nodejs']
   Class['nodejs'] -> Package['yarn']
 }
--- spec/classes/nodejs_spec.rb
+++ spec/classes/nodejs_spec.rb
@@ -12,33 +12,60 @@ describe 'profiles::nodejs' do
       context "without parameters" do
         let(:params) { {} }
 
-        it { is_expected.to contain_apt__source('publiq-nodejs-14') }
+        it { is_expected.to contain_class('profiles::nodejs').with(
+          'major_version' => 16,
+          'version'       => nil,
+        ) }
+
+        it { is_expected.to contain_apt__source('nodejs-16') }
         it { is_expected.to contain_apt__source('publiq-tools') }
 
         it { is_expected.to contain_class('nodejs').with(
           'manage_package_repo'   => false,
-          'nodejs_package_ensure' => '14.16.1-1nodesource1'
+          'nodejs_package_ensure' => 'present'
         ) }
 
         it { is_expected.to contain_package('yarn').with(
           'ensure' => 'present'
         ) }
 
-        it { is_expected.to contain_class('nodejs').that_requires('Apt::Source[publiq-nodejs-14]') }
+        it { is_expected.to contain_class('nodejs').that_requires('Apt::Source[nodejs-16]') }
         it { is_expected.to contain_package('yarn').that_requires('Apt::Source[publiq-tools]') }
         it { is_expected.to contain_package('yarn').that_requires('Class[nodejs]') }
       end
 
-      context "with version => 16.17.0-1nodesource1" do
-        let(:params) { { 'version' => '16.17.0-1nodesource1' } }
+      context "with major_version => 18" do
+        let(:params) { { 'major_version' => 18 } }
 
-        it { is_expected.to contain_apt__source('publiq-nodejs-16') }
+        it { is_expected.to contain_apt__source('nodejs-18') }
 
         it { is_expected.to contain_class('nodejs').with(
-          'nodejs_package_ensure' => '16.17.0-1nodesource1'
+          'manage_package_repo'   => false,
+          'nodejs_package_ensure' => 'present'
         ) }
 
-        it { is_expected.to contain_class('nodejs').that_requires('Apt::Source[publiq-nodejs-16]') }
+        it { is_expected.to contain_class('nodejs').that_requires('Apt::Source[nodejs-18]') }
+      end
+
+      context "with version => 20.2.0-1nodesource1" do
+        let(:params) { { 'version' => '20.2.0-1nodesource1' } }
+
+        it { is_expected.to contain_apt__source('nodejs-20') }
+
+        it { is_expected.to contain_class('nodejs').with(
+          'nodejs_package_ensure' => '20.2.0-1nodesource1'
+        ) }
+
+        it { is_expected.to contain_class('nodejs').that_requires('Apt::Source[nodejs-20]') }
+      end
+
+      context "with version => 18.2.0-1nodesource1 and major_version => 20" do
+        let(:params) { {
+          'version'       => '18.2.0-1nodesource1',
+          'major_version' => 20
+        } }
+
+        it { expect { catalogue }.to raise_error(Puppet::ParseError, /incompatible combination of 'version' and 'major_version' parameters/) }
       end
     end
   end
