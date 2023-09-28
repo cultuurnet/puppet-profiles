19edf4c8e712ad744d96d230cc60077fae8f8e63
Paul Herbosch <paul.herbosch@publiq.be>
2023-09-28 10:56:09 +0200

    [MPM-702] make www-data owner of mountpoint

--- manifests/museumpas/website/deployment.pp
+++ manifests/museumpas/website/deployment.pp
@@ -28,7 +28,7 @@ class profiles::museumpas::website::deployment (
       ensure  => 'mounted',
       device  => "${mount_target_dns_name}:/",
       fstype  => 'nfs4',
-      options => 'nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2',
+      options => 'nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,uid=33,gid=33',
       atboot  => true,
       require => 'Package[museumpas-website]'
     }
