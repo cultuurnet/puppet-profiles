b2b7b218214fd03ac6f88e82ed96ca88301e9591
Paul Herbosch <paul.herbosch@publiq.be>
2023-10-19 10:16:46 +0200

    fix typo

--- manifests/museumpas/website/deployment.pp
+++ manifests/museumpas/website/deployment.pp
@@ -177,7 +177,7 @@ class profiles::museumpas::website::deployment (
 
   if $run_scheduler_cron {
     cron { 'museumpas-filament-scheduler':
-      command     => "cd ${basedir} && php artisan schedule:run >> /dev/null 2>&1",
+      command     => "cd ${basedir} && php artisan schedule:run > /dev/null 2>&1",
       require     => [ User['www-data'], Package['museumpas-website'] ],
       user        => 'www-data'
     }
