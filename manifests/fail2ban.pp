6508acff1baed598274a9c83efc88668b44b4e6e
Paul Herbosch <paul.herbosch@publiq.be>
2023-11-14 09:00:58 +0100

    add action parameter: allow to change action type

--- manifests/fail2ban.pp
+++ manifests/fail2ban.pp
@@ -1,3 +1,16 @@
+# The default values in this profile configure fail2ban to check /var/log/auth.log for failed ssh login attempts
+# An email with banned ip's will be sent out on a regular basis (action_mb)
+# To disable sending of emails use action = 'action_'
+# See /etc/fail2ban/jail.conf for more possible actions
+#
+# Usage example:
+#
+# ---
+# classes:
+#   - include profile::fail2ban
+#
+# profiles::fail2ban::action: 'action_'
+#
 class profiles::fail2ban inherits ::profiles {
 
   class { 'fail2ban':
@@ -7,6 +20,7 @@ class profiles::fail2ban inherits ::profiles {
     service_enable           => true,
     config_file_hash         => {},
     config_file_options_hash => {},
+    action                   => 'action_mb',
     email                    => "infra@publiq.be",
     sender                   => "fail2ban@${facts['networking']['fqdn']}",
     whitelist                => ['127.0.0.1/8'],
