f46f63f0374af0207caa7c03724a47763f96f9d5
Kristof Willaert <kristof@publiq.be>
2023-11-24 15:58:47 +0100

    Allow adding a specific CA certificate to the PuppetDB CLI configuration

--- manifests/puppet/puppetdb/cli.pp
+++ manifests/puppet/puppetdb/cli.pp
@@ -1,8 +1,9 @@
 class profiles::puppet::puppetdb::cli (
   Variant[String, Array[String]] $server_urls,
-  Variant[String, Array[String]] $users       = 'root',
-  Optional[String]               $certificate = undef,
-  Optional[String]               $private_key = undef
+  Variant[String, Array[String]] $users          = 'root',
+  Optional[String]               $certificate    = undef,
+  Optional[String]               $private_key    = undef,
+  Optional[String]               $ca_certificate = undef
 ) inherits ::profiles {
 
   realize Apt::Source['publiq-tools']
@@ -13,9 +14,10 @@ class profiles::puppet::puppetdb::cli (
 
   [$users].flatten.each |$user| {
     profiles::puppet::puppetdb::cli::config { $user:
-      server_urls => $server_urls,
-      certificate => $certificate,
-      private_key => $private_key
+      server_urls    => $server_urls,
+      certificate    => $certificate,
+      private_key    => $private_key,
+      ca_certificate => $ca_certificate
     }
   }
 }
