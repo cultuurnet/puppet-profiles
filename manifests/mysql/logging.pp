b88ac872226b2043996eab77e1bbdca7d055ab85
Kristof Willaert <kristof@publiq.be>
2024-01-19 15:43:05 +0100

    Add MySQL logging setup

---
+++ manifests/mysql/logging.pp
@@ -0,0 +1,28 @@
+class profiles::mysql::logging inherits ::profiles {
+
+  include profiles::logrotate
+
+  realize User['mysql']
+
+  logrotate::rule { 'mysql-server-slow-query':
+    path         => '/var/log/mysql/slow-query.log',
+    rotate       => 30,
+    create_owner => 'mysql',
+    create_group => 'adm',
+    copytruncate => false,
+    postrotate   => '/usr/bin/mysql -e "select @@global.slow_query_log into @sq_log_save; set global slow_query_log=off; select sleep(5); FLUSH SLOW LOGS; select sleep(10); set global slow_query_log=@sq_log_save;"',
+    require      => User['mysql'],
+    *            => $profiles::logrotate::default_rule_attributes
+  }
+
+  logrotate::rule { 'mysql-server-error':
+    path         => '/var/log/mysql/error.log',
+    rotate       => 30,
+    create_owner => 'mysql',
+    create_group => 'adm',
+    copytruncate => false,
+    postrotate   => '/usr/bin/mysqladmin --defaults-file="/root/.my.cnf" flush-logs error',
+    require      => User['mysql'],
+    *            => $profiles::logrotate::default_rule_attributes
+  }
+}
