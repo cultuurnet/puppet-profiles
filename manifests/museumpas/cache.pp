74da13f46bef0adaa5c2c5629e7e4a0dddafd59e
Kristof Willaert <kristof@publiq.be>
2024-01-24 21:09:58 +0100

    Update museumpas cache profile to use redis profile

--- manifests/museumpas/cache.pp
+++ manifests/museumpas/cache.pp
@@ -1,8 +1,6 @@
 class profiles::museumpas::cache inherits ::profiles {
 
-  realize Firewall['400 accept redis traffic']
-
-  include redis
+  include profiles::redis
   include profiles::meilisearch
 }
 
