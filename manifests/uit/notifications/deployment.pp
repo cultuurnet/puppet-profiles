5a45a0748b4312e51db3ca8244b424006cd8dd13
Kristof Willaert <kristof@publiq.be>
2024-02-10 19:37:17 +0100

    Point yarn cache for UiT notifications deployment to /tmp

--- manifests/uit/notifications/deployment.pp
+++ manifests/uit/notifications/deployment.pp
@@ -32,7 +32,7 @@ class profiles::uit::notifications::deployment (
     command     => 'yarn notifications deploy',
     cwd         => $basedir,
     path        => ['/usr/local/bin', '/usr/bin', '/bin', $basedir],
-    environment => ["AWS_ACCESS_KEY_ID=${aws_access_key_id}", "AWS_SECRET_ACCESS_KEY=${aws_secret_access_key}"],
+    environment => ['YARN_CACHE_FOLDER=/tmp/yarn-cache', "AWS_ACCESS_KEY_ID=${aws_access_key_id}", "AWS_SECRET_ACCESS_KEY=${aws_secret_access_key}"],
     logoutput   => true,
     user        => 'www-data',
     refreshonly => true,
