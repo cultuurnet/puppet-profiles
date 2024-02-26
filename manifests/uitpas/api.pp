18f41a4ec7798bd8ea2c7b3082be7010874976ab
Kristof Willaert <kristof@publiq.be>
2024-02-26 15:30:19 +0100

    Add UiTPAS API class

---
+++ manifests/uitpas/api.pp
@@ -0,0 +1,103 @@
+class profiles::uitpas::api (
+  String  $database_password,
+  String  $database_host     = '127.0.0.1',
+  Boolean $deployment        = true
+) inherits ::profiles {
+
+  # (x) mysql server if 127.0.0.1
+  # (x) jdbc resource
+  # (x) jdbc connection pool
+  # (x) glassfish
+  # (x) domain uitpas
+  # (x) service alias
+  # jvmoptions + restart
+  # system properties
+  # service
+
+  $database_name = 'uitpas_api'
+  $database_user = 'uitpas_api'
+  $passwordfile  = '/home/glassfish/asadmin.pass'
+
+  include ::profiles::java
+  include ::profiles::glassfish
+
+  if $database_host == '127.0.0.1' {
+    include ::profiles::mysql::server
+
+    Class['profiles::mysql::server'] -> Mysql_database[$database_name]
+  }
+
+  realize Group['glassfish']
+  realize User['glassfish']
+
+  realize Apt::Source['publiq-tools']
+  realize Package['liquibase']
+  realize Package['mysql-connector-j']
+
+  mysql_database { $database_name:
+    charset => 'utf8mb4',
+    collate => 'utf8mb4_unicode_ci'
+  }
+
+  profiles::mysql::app_user { $database_user:
+    database => $database_name,
+    password => $database_password,
+    require  => Mysql_database[$database_name]
+  }
+
+  profiles::glassfish::domain { 'uitpas':
+    require => Class['profiles::glassfish']
+  }
+
+  jdbcconnectionpool { 'mysql_uitpas_api_j2eePool':
+    ensure              => 'present',
+    user                => 'glassfish',
+    passwordfile        => $passwordfile,
+    portbase            => '4800',
+    resourcetype        => 'javax.sql.DataSource',
+    dsclassname         => 'com.mysql.jdbc.jdbc2.optional.MysqlDataSource',
+    properties          => {
+                             'serverName'        => $database_host,
+                             'portNumber'        => '3306',
+                             'databaseName'      => $database_name,
+                             'User'              => $database_user,
+                             'Password'          => $database_password,
+                             'URL'               => "jdbc:mysql://${database_host}:3306/${database_name}",
+                             'driverClass'       => 'com.mysql.jdbc.Driver',
+                             'characterEncoding' => 'UTF-8',
+                             'useUnicode'        => true,
+                             'useSSL'            => false
+    },
+    require             => [Profiles::Glassfish::Domain['uitpas'], Profiles::Mysql::App_user['uitpas_api']]
+  }
+
+  jdbcresource { 'jdbc/cultuurnet_uitpas':
+    ensure         => 'present',
+    portbase       => '4800',
+    user           => 'glassfish',
+    passwordfile   => $passwordfile,
+    connectionpool => 'mysql_uitpas_api_j2eePool',
+    require        => Jdbcconnectionpool['mysql_uitpas_api_j2eePool']
+  }
+
+  profiles::glassfish::domain::service_alias { 'uitpas':
+    require => Profiles::Glassfish::Domain['uitpas']
+  }
+
+  if $deployment {
+    class { 'profiles::uitpas::api::deployment':
+      database_password => $database_password,
+      database_host     => $database_host
+    }
+
+    Class['profiles::glassfish'] -> Class['profiles::uitpas::api::deployment']
+    Package['liquibase'] -> Class['profiles::uitpas::api::deployment']
+    Package['mysql-connector-j'] -> Class['profiles::uitpas::api::deployment']
+    Profiles::Mysql::App_user[$database_user] -> Class['profiles::uitpas::api::deployment']
+  }
+
+  # include ::profiles::uitpas::api::monitoring
+  # include ::profiles::uitpas::api::metrics
+  # include ::profiles::uitpas::api::backup
+  # include ::profiles::uitpas::api::logging
+}
---
+++ spec/classes/uitpas/api_spec.rb
@@ -0,0 +1,99 @@
+describe 'profiles::uitpas::api' do
+  include_examples 'operating system support'
+
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) { facts }
+
+      context "with database_password => mypassword" do
+        let(:params) { {
+          'database_password' => 'mypassword'
+        } }
+
+        it { is_expected.to compile.with_all_deps }
+
+        it { is_expected.to contain_class('profiles::uitpas::api').with(
+          'database_password' => 'mypassword',
+          'database_host'     => '127.0.0.1',
+          'deployment'        => true
+        ) }
+
+        it { is_expected.to contain_group('glassfish') }
+        it { is_expected.to contain_user('glassfish') }
+
+        it { is_expected.to contain_apt__source('publiq-tools') }
+
+        it { is_expected.to contain_package('liquibase').with(
+          'ensure' => 'present'
+        ) }
+
+        it { is_expected.to contain_package('mysql-connector-j').with(
+          'ensure' => 'present'
+        ) }
+
+        it { is_expected.to contain_class('profiles::java') }
+        it { is_expected.to contain_class('profiles::glassfish') }
+        it { is_expected.to contain_class('profiles::mysql::server') }
+
+        it { is_expected.to contain_mysql_database('uitpas_api').with(
+          'charset' => 'utf8mb4',
+          'collate' => 'utf8mb4_unicode_ci'
+        ) }
+
+        it { is_expected.to contain_profiles__mysql__app_user('uitpas_api').with(
+          'database' => 'uitpas_api',
+          'password' => 'mypassword'
+        ) }
+
+        it { is_expected.to contain_jdbcconnectionpool('mysql_uitpas_api_j2eePool').with(
+          'ensure'              => 'present',
+          'user'                => 'glassfish',
+          'passwordfile'        => '/home/glassfish/asadmin.pass',
+          'portbase'            => '4800',
+          'resourcetype'        => 'javax.sql.DataSource',
+          'dsclassname'         => 'com.mysql.jdbc.jdbc2.optional.MysqlDataSource',
+          'properties'          => {
+                                     'serverName'        => '127.0.0.1',
+                                     'portNumber'        => '3306',
+                                     'databaseName'      => 'uitpas_api',
+                                     'User'              => 'uitpas_api',
+                                     'Password'          => 'mypassword',
+                                     'URL'               => 'jdbc:mysql://127.0.0.1:3306/uitpas_api',
+                                     'driverClass'       => 'com.mysql.jdbc.Driver',
+                                     'characterEncoding' => 'UTF-8',
+                                     'useUnicode'        => true,
+                                     'useSSL'            => false
+                                   }
+        )}
+
+        it { is_expected.to contain_jdbcresource('jdbc/cultuurnet_uitpas').with(
+          'ensure'         => 'present',
+          'portbase'       => '4800',
+          'user'           => 'glassfish',
+          'passwordfile'   => '/home/glassfish/asadmin.pass',
+          'connectionpool' => 'mysql_uitpas_api_j2eePool'
+        ) }
+
+        it { is_expected.to contain_profiles__glassfish__domain('uitpas') }
+        it { is_expected.to contain_profiles__glassfish__domain__service_alias('uitpas') }
+
+        it { is_expected.to contain_class('profiles::uitpas::api::deployment').with(
+          'database_password' => 'mypassword',
+          'database_host'     => '127.0.0.1'
+        ) }
+
+        it { is_expected.to contain_package('liquibase').that_requires('Apt::Source[publiq-tools]') }
+        it { is_expected.to contain_package('mysql-connector-j').that_requires('Apt::Source[publiq-tools]') }
+        it { is_expected.to contain_package('liquibase').that_comes_before('Class[profiles::uitpas::api::deployment]') }
+        it { is_expected.to contain_package('mysql-connector-j').that_comes_before('Class[profiles::uitpas::api::deployment]') }
+        it { is_expected.to contain_profiles__mysql__app_user('uitpas_api').that_comes_before('Class[profiles::uitpas::api::deployment]') }
+        it { is_expected.to contain_jdbcconnectionpool('mysql_uitpas_api_j2eePool').that_requires('Profiles::Glassfish::Domain[uitpas]') }
+        it { is_expected.to contain_jdbcconnectionpool('mysql_uitpas_api_j2eePool').that_requires('Profiles::Mysql::App_user[uitpas_api]') }
+        it { is_expected.to contain_jdbcresource('jdbc/cultuurnet_uitpas').that_requires('Jdbcconnectionpool[mysql_uitpas_api_j2eePool]') }
+        it { is_expected.to contain_profiles__glassfish__domain('uitpas').that_requires('Class[profiles::glassfish]') }
+        it { is_expected.to contain_profiles__glassfish__domain__service_alias('uitpas').that_requires('Profiles::Glassfish::Domain[uitpas]') }
+        it { is_expected.to contain_class('profiles::uitpas::api::deployment').that_requires('Class[profiles::glassfish]') }
+      end
+    end
+  end
+end
