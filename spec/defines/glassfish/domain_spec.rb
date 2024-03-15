a9396dad0b0820dd77c4df5347a48fd501ddeee6
Kristof Willaert <kristof@publiq.be>
2024-03-15 16:56:12 +0100

    Integrate newrelic in glassfish domain

--- spec/defines/glassfish/domain_spec.rb
+++ spec/defines/glassfish/domain_spec.rb
@@ -9,83 +9,134 @@ describe 'profiles::glassfish::domain' do
         context 'without parameters' do
           let(:params) { {} }
 
-          it { is_expected.to compile.with_all_deps }
+          context 'in the production environment' do
+            let(:environment) { 'production' }
 
-          it { is_expected.to contain_profiles__glassfish__domain('foobar-api').with(
-            'ensure'         => 'present',
-            'service_status' => 'running',
-            'initial_heap'   => nil,
-            'maximum_heap'   => nil,
-            'jmx'            => true,
-            'portbase'       => 4800
-          ) }
+            it { is_expected.to compile.with_all_deps }
 
-          it { is_expected.to contain_class('profiles::glassfish') }
+            it { is_expected.to contain_profiles__glassfish__domain('foobar-api').with(
+              'ensure'            => 'present',
+              'service_status'    => 'running',
+              'initial_heap'      => nil,
+              'maximum_heap'      => nil,
+              'jmx'               => true,
+              'newrelic'          => true,
+              'newrelic_app_name' => 'foobar-api-production',
+              'portbase'          => 4800
+            ) }
 
-          it { is_expected.to contain_group('glassfish') }
-          it { is_expected.to contain_user('glassfish') }
+            it { is_expected.to contain_class('profiles::glassfish') }
 
-          it { is_expected.to contain_domain('foobar-api').with(
-            'ensure'            => 'present',
-            'user'              => 'glassfish',
-            'asadminuser'       => 'admin',
-            'passwordfile'      => '/home/glassfish/asadmin.pass',
-            'portbase'          => '4800',
-            'startoncreate'     => false,
-            'enablesecureadmin' => false,
-            'template'          => nil
-          ) }
+            it { is_expected.to contain_group('glassfish') }
+            it { is_expected.to contain_user('glassfish') }
 
-          it { is_expected.to contain_profiles__glassfish__domain__heap('foobar-api').with(
-            'initial'  => nil,
-            'maximum'  => nil,
-            'portbase' => 4800
-          ) }
+            it { is_expected.to contain_domain('foobar-api').with(
+              'ensure'            => 'present',
+              'user'              => 'glassfish',
+              'asadminuser'       => 'admin',
+              'passwordfile'      => '/home/glassfish/asadmin.pass',
+              'portbase'          => '4800',
+              'startoncreate'     => false,
+              'enablesecureadmin' => false,
+              'template'          => nil
+            ) }
 
-          it { is_expected.to contain_profiles__glassfish__domain__jmx('foobar-api').with(
-            'ensure'   => 'present',
-            'portbase' => 4800
-          ) }
+            it { is_expected.to contain_profiles__glassfish__domain__heap('foobar-api').with(
+              'initial'  => nil,
+              'maximum'  => nil,
+              'portbase' => 4800
+            ) }
 
-          it { is_expected.to contain_firewall('400 accept glassfish domain foobar-api HTTP traffic').with(
-            'proto'  => 'tcp',
-            'dport'  => '4880',
-            'action' => 'accept'
-          ) }
+            it { is_expected.to contain_profiles__glassfish__domain__jmx('foobar-api').with(
+              'ensure'   => 'present',
+              'portbase' => 4800
+            ) }
 
-          it { is_expected.to contain_firewall('400 accept glassfish domain foobar-api HTTPS traffic').with(
-            'proto'  => 'tcp',
-            'dport'  => '4881',
-            'action' => 'accept'
-          ) }
+            it { is_expected.to contain_firewall('400 accept glassfish domain foobar-api HTTP traffic').with(
+              'proto'  => 'tcp',
+              'dport'  => '4880',
+              'action' => 'accept'
+            ) }
 
-          it { is_expected.to contain_profiles__glassfish__domain__service('foobar-api').with(
-            'status' => 'running'
-          ) }
+            it { is_expected.to contain_firewall('400 accept glassfish domain foobar-api HTTPS traffic').with(
+              'proto'  => 'tcp',
+              'dport'  => '4881',
+              'action' => 'accept'
+            ) }
 
-          it { is_expected.to contain_cron('Cleanup payara logs foobar-api').with(
-            'command'  => '/usr/bin/find /opt/payara/glassfish/domains/foobar-api/logs -type f -name "server.log_*" -mtime +7 -exec rm {} \;',
-            'user'     => 'root',
-            'hour'     => '*',
-            'minute'   => '15',
-            'weekday'  => '*',
-            'monthday' => '*',
-            'month'    => '*'
-          ) }
+            it { is_expected.to contain_profiles__glassfish__domain__service('foobar-api').with(
+              'status' => 'running'
+            ) }
 
-          it { is_expected.to contain_domain('foobar-api').that_requires('Group[glassfish]') }
-          it { is_expected.to contain_domain('foobar-api').that_requires('User[glassfish]') }
-          it { is_expected.to contain_profiles__glassfish__domain__service('foobar-api').that_requires('Domain[foobar-api]') }
-          it { is_expected.to contain_profiles__glassfish__domain__jmx('foobar-api').that_requires('Profiles::Glassfish::Domain::Service[foobar-api]') }
-          it { is_expected.to contain_profiles__glassfish__domain__heap('foobar-api').that_requires('Profiles::Glassfish::Domain::Service[foobar-api]') }
+            it { is_expected.to contain_cron('Cleanup payara logs foobar-api').with(
+              'command'  => '/usr/bin/find /opt/payara/glassfish/domains/foobar-api/logs -type f -name "server.log_*" -mtime +7 -exec rm {} \;',
+              'user'     => 'root',
+              'hour'     => '*',
+              'minute'   => '15',
+              'weekday'  => '*',
+              'monthday' => '*',
+              'month'    => '*'
+            ) }
+
+            it { is_expected.to contain_domain('foobar-api').that_requires('Group[glassfish]') }
+            it { is_expected.to contain_domain('foobar-api').that_requires('User[glassfish]') }
+            it { is_expected.to contain_profiles__glassfish__domain__service('foobar-api').that_requires('Domain[foobar-api]') }
+            it { is_expected.to contain_profiles__glassfish__domain__jmx('foobar-api').that_requires('Profiles::Glassfish::Domain::Service[foobar-api]') }
+            it { is_expected.to contain_profiles__glassfish__domain__newrelic('foobar-api').that_requires('Profiles::Glassfish::Domain::Service[foobar-api]') }
+            it { is_expected.to contain_profiles__glassfish__domain__heap('foobar-api').that_requires('Profiles::Glassfish::Domain::Service[foobar-api]') }
+
+            context "with hieradata" do
+              let(:hiera_config) { 'spec/support/hiera/common.yaml' }
+
+              it { is_expected.to contain_profiles__glassfish__domain__newrelic('foobar-api').with(
+                'ensure'      => 'present',
+                'license_key' => 'my_license_key',
+                'app_name'    => 'foobar-api-production',
+                'portbase'    => 4800
+              ) }
+            end
+
+            context "without hieradata" do
+              let(:hiera_config) { 'spec/support/hiera/empty.yaml' }
+
+              it { is_expected.to contain_profiles__glassfish__domain__newrelic('foobar-api').with(
+                'ensure'      => 'present',
+                'license_key' => nil,
+                'app_name'    => 'foobar-api-production',
+                'portbase'    => 4800
+              ) }
+            end
+          end
+
+          context 'in the testing environment' do
+            let(:environment) { 'testing' }
+
+            it { is_expected.to contain_profiles__glassfish__domain('foobar-api').with(
+              'ensure'            => 'present',
+              'service_status'    => 'running',
+              'initial_heap'      => nil,
+              'maximum_heap'      => nil,
+              'jmx'               => true,
+              'newrelic'          => true,
+              'newrelic_app_name' => 'foobar-api-testing',
+              'portbase'          => 4800
+            ) }
+
+            it { is_expected.to contain_profiles__glassfish__domain__newrelic('foobar-api').with(
+              'ensure'      => 'present',
+              'app_name'    => 'foobar-api-testing',
+              'portbase'    => 4800
+            ) }
+          end
         end
 
-        context 'with portbase => 14800, initial_heap => 512m, maximum_heap => 1024m, jmx => false and service_status => stopped' do
+        context 'with portbase => 14800, initial_heap => 512m, maximum_heap => 1024m, jmx => false, newrelic => false and service_status => stopped' do
           let(:params) { {
             'portbase'       => 14800,
             'initial_heap'   => '512m',
             'maximum_heap'   => '1024m',
             'jmx'            => false,
+            'newrelic'       => false,
             'service_status' => 'stopped'
           } }
 
@@ -111,6 +162,11 @@ describe 'profiles::glassfish::domain' do
             'portbase' => 14800
           ) }
 
+          it { is_expected.to contain_profiles__glassfish__domain__newrelic('foobar-api').with(
+            'ensure'   => 'absent',
+            'portbase' => 14800
+          ) }
+
           it { is_expected.to contain_firewall('400 accept glassfish domain foobar-api HTTP traffic').with(
             'proto'  => 'tcp',
             'dport'  => '14880',
@@ -132,8 +188,10 @@ describe 'profiles::glassfish::domain' do
       context "title baz-api" do
         let(:title) { 'baz-api' }
 
-        context 'without parameters' do
-          let(:params) { {} }
+        context 'with newrelic_app_name => my_fancy_app' do
+          let(:params) { {
+            'newrelic_app_name' => 'my_fancy_app'
+          } }
 
           it { is_expected.to contain_domain('baz-api').with(
             'ensure'            => 'present',
@@ -154,6 +212,12 @@ describe 'profiles::glassfish::domain' do
 
           it { is_expected.to contain_profiles__glassfish__domain__jmx('baz-api').with(
             'ensure'   => 'present',
+            'portbase' => 4800
+          ) }
+
+          it { is_expected.to contain_profiles__glassfish__domain__newrelic('baz-api').with(
+            'ensure'   => 'present',
+            'app_name' => 'my_fancy_app',
             'portbase' => 4800
           ) }
 
