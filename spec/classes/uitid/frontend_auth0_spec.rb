4609a350a18c2fd37ad499d06cdb3b7e896a0e0e
Kristof Willaert <kristof@publiq.be>
2024-04-15 11:50:11 +0200

    Add dummy implementation of UiTID frontend auth0 profile

---
+++ spec/classes/uitid/frontend_auth0_spec.rb
@@ -0,0 +1,25 @@
+describe 'profiles::uitid::frontend_auth0' do
+  include_examples 'operating system support'
+
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) { facts }
+
+      context "without parameters" do
+        let(:params) { { } }
+
+        context "with hieradata" do
+          let(:hiera_config) { 'spec/support/hiera/common.yaml' }
+
+          it { is_expected.to compile.with_all_deps }
+        end
+
+        context "without hieradata" do
+          let(:hiera_config) { 'spec/support/hiera/empty.yaml' }
+
+          it { expect { catalogue }.to raise_error(Puppet::ParseError, /expects a value for parameter 'config_source'/) }
+        end
+      end
+    end
+  end
+end
