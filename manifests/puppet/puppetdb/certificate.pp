8e96e3fbddb2b21f6dd175c340a2175db094e65e
Kristof Willaert <kristof@publiq.be>
2024-05-06 11:52:49 +0200

    Small refactoring

--- manifests/puppet/puppetdb/certificate.pp
+++ manifests/puppet/puppetdb/certificate.pp
@@ -2,6 +2,11 @@ class profiles::puppet::puppetdb::certificate (
   String $certname
 ) inherits ::profiles {
 
+  $default_file_attributes = {
+                               owner => 'puppetdb',
+                               group => 'puppetdb'
+                             }
+
   realize Group['puppetdb']
   realize User['puppetdb']
 
@@ -18,48 +23,43 @@ class profiles::puppet::puppetdb::certificate (
   file { 'puppetdb confdir':
     ensure  => 'directory',
     path    => '/etc/puppetlabs/puppetdb',
-    owner   => 'puppetdb',
-    group   => 'puppetdb',
     mode    => '0750',
-    require => [Group['puppetdb'], User['puppetdb']]
+    require => [Group['puppetdb'], User['puppetdb']],
+    *       => $default_file_attributes
   }
 
   file { 'puppetdb ssldir':
     ensure  => 'directory',
     path    => '/etc/puppetlabs/puppetdb/ssl',
-    owner   => 'puppetdb',
-    group   => 'puppetdb',
     mode    => '0700',
-    require => [Group['puppetdb'], User['puppetdb'], File['puppetdb confdir']]
+    require => [Group['puppetdb'], User['puppetdb'], File['puppetdb confdir']],
+    *       => $default_file_attributes
   }
 
   file { 'puppetdb cacert':
     ensure  => 'file',
     path    => '/etc/puppetlabs/puppetdb/ssl/ca.pem',
-    owner   => 'puppetdb',
-    group   => 'puppetdb',
     mode    => '0600',
     source  => 'file:///etc/puppetlabs/puppet/ssl/certs/ca.pem',
-    require => [Group['puppetdb'], User['puppetdb'], File['puppetdb ssldir']]
+    require => [Group['puppetdb'], User['puppetdb'], File['puppetdb ssldir']],
+    *       => $default_file_attributes
   }
 
   file { 'puppetdb certificate':
     ensure  => 'file',
     path    => '/etc/puppetlabs/puppetdb/ssl/public.pem',
-    owner   => 'puppetdb',
-    group   => 'puppetdb',
     mode    => '0600',
     source  => "file:///etc/puppetlabs/puppet/ssl/certs/${certname}.pem",
-    require => [Group['puppetdb'], User['puppetdb'], File['puppetdb ssldir']]
+    require => [Group['puppetdb'], User['puppetdb'], File['puppetdb ssldir']],
+    *       => $default_file_attributes
   }
 
   file { 'puppetdb private_key':
     ensure  => 'file',
     path    => '/etc/puppetlabs/puppetdb/ssl/private.pem',
-    owner   => 'puppetdb',
-    group   => 'puppetdb',
     mode    => '0600',
     source  => "file:///etc/puppetlabs/puppet/ssl/private_keys/${certname}.pem",
-    require => [Group['puppetdb'], User['puppetdb'], File['puppetdb ssldir']]
+    require => [Group['puppetdb'], User['puppetdb'], File['puppetdb ssldir']],
+    *       => $default_file_attributes
   }
 }
