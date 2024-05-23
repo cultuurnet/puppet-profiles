dba4f86455a2642f83a7785b267c574499859608
Kristof Willaert <kristof@publiq.be>
2024-05-23 16:08:22 +0200

    Add initial puppetboard profile

---
+++ manifests/puppet/puppetboard.pp
@@ -0,0 +1,64 @@
+class profiles::puppet::puppetboard (
+  String                         $servername,
+  Variant[String, Array[String]] $serveraliases   = [],
+  Stdlib::Ipv4                   $service_address = '127.0.0.1',
+  Stdlib::Port::Unprivileged     $service_port    = 3000,
+  Enum['running', 'stopped']     $service_status  = 'running'
+) inherits ::profiles {
+
+  $basedir = '/var/www/puppetboard'
+
+  include profiles::apache
+
+  realize Apt::Source['publiq-tools']
+  realize Group['www-data']
+  realize User['www-data']
+
+  class { 'profiles::puppet::puppetboard::certificate':
+    certname => $servername,
+    basedir  => $basedir,
+    notify   => Service['puppetboard'],
+  }
+
+  class { '::puppetboard':
+    install_from        => 'package',
+    package_name        => 'puppetboard',
+    manage_group        => false,
+    manage_user         => false,
+    secret_key          => fqdn_rand_string(32),
+    puppetdb_host       => '127.0.0.1',
+    puppetdb_port       => 8081,
+    puppetdb_ssl_verify => true,
+    puppetdb_key        => "${basedir}/ssl/private.pem",
+    puppetdb_cert       => "${basedir}/ssl/public.pem",
+    enable_catalog      => true,
+    enable_query        => true,
+    default_environment => 'production',
+    reports_count       => 20,
+    settings_file       => "${basedir}/settings.py",
+    extra_settings      => {},
+    require             => [Apt::Source['publiq-tools'], Group['www-data'], User['www-data'], Class['profiles::puppet::puppetboard::certificate']],
+    notify              => Service['puppetboard']
+  }
+
+  file { 'puppetboard service defaults':
+    ensure  => 'file',
+    path    => '/etc/default/puppetboard',
+    content => "HOST=${service_address}\nPORT=${service_port}",
+    notify  => Service['puppetboard']
+  }
+
+  service { 'puppetboard':
+    ensure    => $service_status,
+    hasstatus => true,
+    enable    => $service_status ? {
+                   'running' => true,
+                   'stopped' => false
+                 }
+  }
+
+  profiles::apache::vhost::reverse_proxy { "http://${servername}":
+    destination => "http://${service_address}:${service_port}/",
+    aliases     => $serveraliases
+  }
+}
---
+++ spec/classes/puppet/puppetboard/certificate_spec.rb
@@ -0,0 +1,136 @@
+describe 'profiles::puppet::puppetboard::certificate' do
+  include_examples 'operating system support'
+
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) { facts }
+
+      context "on host foo.example.com" do
+        let(:node) { 'foo.example.com' }
+
+        context "with certname => foo.example.com" do
+          let(:params) { {
+            'certname' => 'foo.example.com'
+          } }
+
+          it { is_expected.to compile.with_all_deps }
+
+          it { is_expected.to contain_class('profiles::puppet::puppetboard::certificate').with(
+            'certname' => 'foo.example.com',
+            'basedir'  => '/var/www/puppetboard'
+          ) }
+
+          it { is_expected.not_to contain_puppet_certificate('foo.example.com') }
+
+          it { is_expected.to contain_group('www-data') }
+          it { is_expected.to contain_user('www-data') }
+
+          it { is_expected.to contain_file('puppetboard basedir').with(
+            'ensure' => 'directory',
+            'path'   => '/var/www/puppetboard',
+            'owner'  => 'www-data',
+            'group'  => 'www-data',
+            'mode'   => '0700'
+          ) }
+
+          it { is_expected.to contain_file('puppetboard ssldir').with(
+            'ensure' => 'directory',
+            'path'   => '/var/www/puppetboard/ssl',
+            'owner'  => 'www-data',
+            'group'  => 'www-data',
+            'mode'   => '0700'
+          ) }
+
+          it { is_expected.to contain_file('puppetboard cacert').with(
+            'ensure' => 'file',
+            'path'   => '/var/www/puppetboard/ssl/ca.pem',
+            'owner'  => 'www-data',
+            'group'  => 'www-data',
+            'mode'   => '0600',
+            'source' => 'file:///etc/puppetlabs/puppet/ssl/certs/ca.pem'
+          ) }
+
+          it { is_expected.to contain_file('puppetboard certificate').with(
+            'ensure' => 'file',
+            'path'   => '/var/www/puppetboard/ssl/public.pem',
+            'owner'  => 'www-data',
+            'group'  => 'www-data',
+            'mode'   => '0600',
+            'source' => 'file:///etc/puppetlabs/puppet/ssl/certs/foo.example.com.pem'
+          ) }
+
+          it { is_expected.to contain_file('puppetboard private_key').with(
+            'ensure' => 'file',
+            'path'   => '/var/www/puppetboard/ssl/private.pem',
+            'owner'  => 'www-data',
+            'group'  => 'www-data',
+            'mode'   => '0600',
+            'source' => 'file:///etc/puppetlabs/puppet/ssl/private_keys/foo.example.com.pem'
+          ) }
+
+          it { is_expected.to contain_file('puppetboard basedir').that_requires('Group[www-data]') }
+          it { is_expected.to contain_file('puppetboard basedir').that_requires('User[www-data]') }
+          it { is_expected.to contain_file('puppetboard ssldir').that_requires('Group[www-data]') }
+          it { is_expected.to contain_file('puppetboard ssldir').that_requires('User[www-data]') }
+          it { is_expected.to contain_file('puppetboard ssldir').that_requires('File[puppetboard basedir]') }
+          it { is_expected.to contain_file('puppetboard cacert').that_requires('Group[www-data]') }
+          it { is_expected.to contain_file('puppetboard cacert').that_requires('User[www-data]') }
+          it { is_expected.to contain_file('puppetboard cacert').that_requires('File[puppetboard ssldir]') }
+          it { is_expected.to contain_file('puppetboard certificate').that_requires('Group[www-data]') }
+          it { is_expected.to contain_file('puppetboard certificate').that_requires('User[www-data]') }
+          it { is_expected.to contain_file('puppetboard certificate').that_requires('File[puppetboard ssldir]') }
+          it { is_expected.to contain_file('puppetboard private_key').that_requires('Group[www-data]') }
+          it { is_expected.to contain_file('puppetboard private_key').that_requires('User[www-data]') }
+          it { is_expected.to contain_file('puppetboard private_key').that_requires('File[puppetboard ssldir]') }
+        end
+
+        context "with certname => puppetboard.example.com and basedir => /srv/puppetboard" do
+          let(:params) { {
+            'certname' => 'puppetboard.example.com',
+            'basedir'  => '/srv/puppetboard'
+          } }
+
+          it { is_expected.to compile.with_all_deps }
+
+          it { is_expected.to contain_puppet_certificate('puppetboard.example.com').with(
+            'ensure'               => 'present',
+            'waitforcert'          => 60,
+            'renewal_grace_period' => 5,
+            'clean'                => true
+          ) }
+
+          it { is_expected.to contain_file('puppetboard basedir').with(
+            'path'   => '/srv/puppetboard'
+          ) }
+
+          it { is_expected.to contain_file('puppetboard ssldir').with(
+            'path'   => '/srv/puppetboard/ssl'
+          ) }
+
+          it { is_expected.to contain_file('puppetboard cacert').with(
+            'path'   => '/srv/puppetboard/ssl/ca.pem'
+          ) }
+
+          it { is_expected.to contain_file('puppetboard certificate').with(
+            'path'   => '/srv/puppetboard/ssl/public.pem',
+            'source' => 'file:///etc/puppetlabs/puppet/ssl/certs/puppetboard.example.com.pem'
+          ) }
+
+          it { is_expected.to contain_file('puppetboard private_key').with(
+            'path'   => '/srv/puppetboard/ssl/private.pem',
+            'source' => 'file:///etc/puppetlabs/puppet/ssl/private_keys/puppetboard.example.com.pem'
+          ) }
+
+          it { is_expected.to contain_file('puppetboard certificate').that_requires('Puppet_certificate[puppetboard.example.com]') }
+          it { is_expected.to contain_file('puppetboard private_key').that_requires('Puppet_certificate[puppetboard.example.com]') }
+        end
+      end
+
+      context "without parameters" do
+        let(:params) { {} }
+
+        it { expect { catalogue }.to raise_error(Puppet::ParseError, /expects a value for parameter 'certname'/) }
+      end
+    end
+  end
+end
