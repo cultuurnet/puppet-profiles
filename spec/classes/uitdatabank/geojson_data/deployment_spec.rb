6bbb18588cbd2eecd37709dba59262a0c4400cbc
Kristof Willaert <kristof@publiq.be>
2024-06-13 14:44:38 +0200

    Remove data_migration toggle from uitdatabank::geojson_data::deployment profile

--- spec/classes/uitdatabank/geojson_data/deployment_spec.rb
+++ spec/classes/uitdatabank/geojson_data/deployment_spec.rb
@@ -13,7 +13,6 @@ describe 'profiles::uitdatabank::geojson_data::deployment' do
         it { is_expected.to contain_class('profiles::uitdatabank::geojson_data::deployment').with(
           'version'        => 'latest',
           'repository'     => 'uitdatabank-geojson-data',
-          'data_migration' => false,
           'puppetdb_url'   => nil
         ) }
 
@@ -22,8 +21,6 @@ describe 'profiles::uitdatabank::geojson_data::deployment' do
         it { is_expected.to contain_package('uitdatabank-geojson-data').with( 'ensure' => 'latest') }
         it { is_expected.to contain_package('uitdatabank-geojson-data').that_notifies('Profiles::Deployment::Versions[profiles::uitdatabank::geojson_data::deployment]') }
         it { is_expected.to contain_package('uitdatabank-geojson-data').that_requires('Apt::Source[uitdatabank-geojson-data]') }
-
-        it { is_expected.not_to contain_package('uitdatabank-geojson-data').that_notifies('Class[profiles::uitdatabank::search_api::data_migration]') }
 
         context "without hieradata" do
           let(:hiera_config) { 'spec/support/hiera/empty.yaml' }
@@ -42,18 +39,16 @@ describe 'profiles::uitdatabank::geojson_data::deployment' do
         end
       end
 
-      context "with version => 1.2.3, repository => foo, data_migration => true and puppetdb_url => http://example.com:8000" do
+      context "with version => 1.2.3, repository => foo and puppetdb_url => http://example.com:8000" do
         let(:params) { {
           'version'        => '1.2.3',
           'repository'     => 'foo',
-          'data_migration' => true,
           'puppetdb_url'   => 'http://example.com:8000'
         } }
 
         context "with repository foo and class profiles::uitdatank::search_api::data_migration defined" do
           let(:pre_condition) { [
             '@apt::source { "foo": location => "http://localhost", release => "focal", repos => "main" }',
-            'class { "profiles::uitdatabank::search_api::data_migration": }'
           ] }
 
           it { is_expected.to contain_apt__source('foo') }
@@ -67,7 +62,6 @@ describe 'profiles::uitdatabank::geojson_data::deployment' do
           ) }
 
           it { is_expected.to contain_package('uitdatabank-geojson-data').that_requires('Apt::Source[foo]') }
-          it { is_expected.to contain_package('uitdatabank-geojson-data').that_notifies('Class[profiles::uitdatabank::search_api::data_migration]') }
         end
       end
     end
