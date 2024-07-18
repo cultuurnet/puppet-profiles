a0dccf3e04b5411af03f4951a0276d46b771e53a
Kristof Willaert <kristof@publiq.be>
2024-07-18 14:57:00 +0200

    No need for systemd::reload class, the same functionality is available in the systemd module

--- manifests/systemd/reload.pp
+++
@@ -1,9 +0,0 @@
-class profiles::systemd::reload inherits ::profiles {
-
-  exec { 'systemd daemon reload':
-    command     => '/usr/bin/systemctl daemon-reload',
-    cwd         => '/',
-    logoutput   => true,
-    refreshonly => true
-  }
-}
