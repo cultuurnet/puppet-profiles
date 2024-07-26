c6153bb10796b14af16e8615f15793d33eee081f
Kristof Willaert <kristof@publiq.be>
2024-07-26 15:13:45 +0200

    Add service symlink for glassfish domain instead of service alias overrides

--- spec/defines/glassfish/domain/service_alias_spec.rb
+++ spec/defines/glassfish/domain/service_alias_spec.rb
@@ -1,52 +1,46 @@
 describe 'profiles::glassfish::domain::service_alias' do
-  on_supported_os.each do |os, facts|
-    context "on #{os}" do
-      let(:facts) { facts }
+  context "with title => my_service" do
+    let(:title) { 'my_service' }
 
-      context "title foobar" do
-        let(:title) { 'foobar' }
+    include_examples 'operating system support'
+
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) { facts }
 
         it { is_expected.to compile.with_all_deps }
 
-        it { is_expected.to contain_systemd__dropin_file('glassfish domain service alias foobar').with(
-          'unit'           => 'glassfish-foobar.service',
-          'filename'       => 'foobar.conf',
-          'notify_service' => false,
-          'daemon_reload'  => false,
+        it { is_expected.to contain_file('my_service glassfish domain service alias link').with(
+          'ensure'  => 'link',
+          'path'    => '/etc/systemd/system/my_service.service',
+          'target'  => '/lib/systemd/system/glassfish-my_service.service'
         ) }
 
-        it { is_expected.to contain_systemd__dropin_file('glassfish domain service alias foobar').with_content(/^\[Install\]\nAlias=foobar.service$/) }
+        it { is_expected.to contain_systemd__daemon_reload('my_service') }
 
-        it { is_expected.to contain_exec('re-enable glassfish domain (foobar)').with(
-          'command'     => "systemctl reenable glassfish-foobar",
-          'path'        => ['/usr/sbin', '/usr/bin'],
-          'refreshonly' => true,
-          'logoutput'   => 'on_failure'
-        ) }
-
-        it { is_expected.to contain_systemd__dropin_file('glassfish domain service alias foobar').that_requires('Class[profiles::glassfish]') }
-        it { is_expected.to contain_exec('re-enable glassfish domain (foobar)').that_subscribes_to('Systemd::Dropin_file[glassfish domain service alias foobar]') }
+        it { is_expected.to contain_file('my_service glassfish domain service alias link').that_requires('Class[profiles::glassfish]') }
+        it { is_expected.to contain_file('my_service glassfish domain service alias link').that_notifies('Systemd::Daemon_reload[my_service]') }
       end
+    end
+  end
 
-      context "title baz" do
-        let(:title) { 'baz' }
+  context "with title => foo_service" do
+    let(:title) { 'foo_service' }
 
-        it { is_expected.to contain_systemd__dropin_file('glassfish domain service alias baz').with(
-          'unit'           => 'glassfish-baz.service',
-          'filename'       => 'baz.conf',
-          'notify_service' => false,
-          'daemon_reload'  => false,
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) { facts }
+
+        it { is_expected.to contain_file('foo_service glassfish domain service alias link').with(
+          'ensure'  => 'link',
+          'path'    => '/etc/systemd/system/foo_service.service',
+          'target'  => '/lib/systemd/system/glassfish-foo_service.service'
         ) }
 
-        it { is_expected.to contain_systemd__dropin_file('glassfish domain service alias baz').with_content(/^\[Install\]\nAlias=baz.service$/) }
+        it { is_expected.to contain_systemd__daemon_reload('foo_service') }
 
-        it { is_expected.to contain_exec('re-enable glassfish domain (baz)').with(
-          'command'     => "systemctl reenable glassfish-baz",
-          'path'        => ['/usr/sbin', '/usr/bin'],
-          'refreshonly' => true,
-          'logoutput'   => 'on_failure'
-        ) }
-
+        it { is_expected.to contain_file('foo_service glassfish domain service alias link').that_requires('Class[profiles::glassfish]') }
+        it { is_expected.to contain_file('foo_service glassfish domain service alias link').that_notifies('Systemd::Daemon_reload[foo_service]') }
       end
     end
   end
