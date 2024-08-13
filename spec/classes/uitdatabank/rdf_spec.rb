e3aa4aaa7ba68c9276a31b21c36485ab997b21ce
Kristof Willaert <kristof@publiq.be>
2024-08-13 16:15:35 +0200

    Add rewrite rule for /id/(event|place|organizer)/udb/<uuid> to UiTdatabank RDF vhost

--- spec/classes/uitdatabank/rdf_spec.rb
+++ spec/classes/uitdatabank/rdf_spec.rb
@@ -46,6 +46,13 @@ describe 'profiles::uitdatabank::rdf' do
                                                        '%{REQUEST_URI} "^/(events|places|organizers)/[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{16}$"'
                                                      ],
                                    'rewrite_rule' => '^/(events|places|organizers)/(.*)$ https://foo.example.com/$1/$2 [P]'
+                                 }, {
+                                   'comment'      => 'Reverse proxy /id/(event|place|organizer)/udb/<uuid> to backend',
+                                   'rewrite_cond' => [
+                                                       '%{REQUEST_URI} "^/id/(event|place|organizer)/udb/[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}$" [OR]',
+                                                       '%{REQUEST_URI} "^/id/(event|place|organizer)/udb/[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{16}$"'
+                                                     ],
+                                   'rewrite_rule' => '^/id/(event|place|organizer)/udb/(.*)$ https://foo.example.com/$1s/$2 [P]'
                                  } ]
         ) }
 
@@ -89,6 +96,13 @@ describe 'profiles::uitdatabank::rdf' do
                                                      '%{REQUEST_URI} "^/(events|places|organizers)/[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{16}$"'
                                                    ],
                                    'rewrite_rule' => '^/(events|places|organizers)/(.*)$ http://bar.example.com/$1/$2 [P]'
+                                 }, {
+                                   'comment'      => 'Reverse proxy /id/(event|place|organizer)/udb/<uuid> to backend',
+                                   'rewrite_cond' => [
+                                                     '%{REQUEST_URI} "^/id/(event|place|organizer)/udb/[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}$" [OR]',
+                                                     '%{REQUEST_URI} "^/id/(event|place|organizer)/udb/[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{16}$"'
+                                                   ],
+                                   'rewrite_rule' => '^/id/(event|place|organizer)/udb/(.*)$ http://bar.example.com/$1s/$2 [P]'
                                  } ]
         ) }
       end
