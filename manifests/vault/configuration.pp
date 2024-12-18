e25a49d98764ffb11b961d702e5c25cab358307b
Kristof Willaert <kristof@publiq.be>
2024-12-18 11:48:54 +0100

    Add vault GPG key class to configuration

--- manifests/vault/configuration.pp
+++ manifests/vault/configuration.pp
@@ -1,6 +1,16 @@
 class profiles::vault::configuration (
+  Boolean $auto_unseal     = false,
   String  $service_address = '127.0.0.1',
   Integer $service_port    = 8200
 ) inherits ::profiles {
 
+  $full_name     = 'Vault'
+  $email_address = 'vault@publiq.be'
+  $key_shares    = 5
+  $key_threshold = 1
+
+  class { 'profiles::vault::gpg_key':
+    full_name     => $full_name,
+    email_address => $email_address
+  }
 }
--- spec/classes/vault/configuration_spec.rb
+++ spec/classes/vault/configuration_spec.rb
@@ -11,13 +11,20 @@ describe 'profiles::vault::configuration' do
         it { is_expected.to compile.with_all_deps }
 
         it { is_expected.to contain_class('profiles::vault::configuration').with(
+          'auto_unseal'     => false,
           'service_address' => '127.0.0.1',
           'service_port'    => 8200
         ) }
+
+        it { is_expected.to contain_class('profiles::vault::gpg_key').with(
+          'full_name'     => 'Vault',
+          'email_address' => 'vault@publiq.be'
+        ) }
       end
 
-      context 'with service_address => 0.0.0.0 and service_port 18200' do
+      context 'with auto_unseal => true, service_address => 0.0.0.0 and service_port 18200' do
         let(:params) { {
+          'auto_unseal'     => true,
           'service_address' => '0.0.0.0',
           'service_port'    => 18200
         } }
