ef7bce8c60e05d519e013b6cd7cd2edf5ad5e371
Kristof Willaert <kristof@publiq.be>
2024-12-22 15:13:16 +0100

    Only create Vault GPG key when auto_unseal is set to true

--- manifests/vault/init.pp
+++ manifests/vault/init.pp
@@ -1,25 +1,47 @@
 class profiles::vault::init (
-  Variant[String, Array[String]] $gpg_keys = []
+  Boolean                        $auto_unseal = false,
+  Variant[String, Array[String]] $gpg_keys    = []
 ) inherits ::profiles {
 
-  $full_name     = 'Vault'
-  $email_address = 'vault@publiq.be'
-  $key_shares    = length([$email_address].flatten + [$gpg_keys].flatten)
+  if (!$auto_unseal and empty($gpg_keys)) {
+    fail('without auto_unseal, at least one GPG key has to be provided')
+  }
+
   $key_threshold = 1
+
+  if $auto_unseal {
+    $full_name       = 'Vault'
+    $email_address   = 'vault@publiq.be'
+    $key_shares      = length([$email_address].flatten + [$gpg_keys].flatten)
+    $init_key_string = '/etc/vault.d/gpg_keys/vault.asc'
+
+    class { 'profiles::vault::gpg_key':
+      full_name     => $full_name,
+      email_address => $email_address,
+      before        => Exec['vault_init']
+    }
+  } else {
+    $key_shares = length([$gpg_keys].flatten)
+  }
 
   realize Group['vault']
   realize User['vault']
+  realize Package['jq']
 
-  class { 'profiles::vault::gpg_key':
-    full_name     => $full_name,
-    email_address => $email_address
+  file { 'vault_process_init':
+    ensure  => 'file',
+    path    => '/usr/local/bin/vault-process-init',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    source  => 'puppet:///modules/profiles/vault/vault-process-init'
   }
 
   exec { 'vault_init':
-    command   => "/usr/bin/vault operator init -key-shares=${key_shares} -key-threshold=${key_threshold} -pgp-keys=\"/etc/vault.d/gpg_keys/vault.asc\" -tls-skip-verify -format=json | /usr/local/bin/vault-process-init",
+    command   => "/usr/bin/vault operator init -key-shares=${key_shares} -key-threshold=${key_threshold} -pgp-keys=\"${init_key_string}\" -tls-skip-verify -format=json | /usr/local/bin/vault-process-init",
     user      => 'vault',
     logoutput => 'on_failure',
-    require   => [User['vault'], Class['profiles::vault::gpg_key']]
+    require   => [User['vault'], File['vault_process_init'], Package['jq']]
   }
 
   file { 'vault_initialized_external_fact':
--- spec/classes/vault/init_spec.rb
+++ spec/classes/vault/init_spec.rb
@@ -5,21 +5,34 @@ describe 'profiles::vault::init' do
     context "on #{os}" do
       let(:facts) { facts }
 
-      context 'without parameters' do
-        let(:params) { {} }
+      context 'with auto_unseal => true' do
+        let(:params) { {
+          'auto_unseal' => true
+        } }
 
         it { is_expected.to compile.with_all_deps }
 
         it { is_expected.to contain_class('profiles::vault::init').with(
-          'gpg_keys' => []
+          'auto_unseal' => true,
+          'gpg_keys'    => []
         ) }
 
         it { is_expected.to contain_group('vault') }
         it { is_expected.to contain_user('vault') }
+        it { is_expected.to contain_package('jq') }
 
         it { is_expected.to contain_class('profiles::vault::gpg_key').with(
           'full_name'     => 'Vault',
           'email_address' => 'vault@publiq.be'
+        ) }
+
+        it { is_expected.to contain_file('vault_process_init').with(
+          'ensure'  => 'file',
+          'path'    => '/usr/local/bin/vault-process-init',
+          'owner'   => 'root',
+          'group'   => 'root',
+          'mode'    => '0755',
+          'source'  => 'puppet:///modules/profiles/vault/vault-process-init'
         ) }
 
         it { is_expected.to contain_exec('vault_init').with(
@@ -36,12 +49,34 @@ describe 'profiles::vault::init' do
           'mode'    => '0644',
           'content' => 'vault_initialized=true'
         ) }
+
+        it { is_expected.to contain_exec('vault_init').that_requires('User[vault]') }
+        it { is_expected.to contain_exec('vault_init').that_requires('Class[profiles::vault::gpg_key]') }
+        it { is_expected.to contain_exec('vault_init').that_requires('File[vault_process_init]') }
+        it { is_expected.to contain_exec('vault_init').that_requires('Package[jq]') }
+        it { is_expected.to contain_file('vault_initialized_external_fact').that_requires('Exec[vault_init]') }
       end
 
-      context 'with gpg_keys => xxxxx' do
+      context 'with auto_unseal => true and gpg_keys => xxxxx' do
         let(:params) { {
-          'gpg_keys' => xxxxx
+          'auto_unseal' => true,
+          'gpg_keys'    => xxxxx
         } }
+      end
+
+      context 'with auto_unseal => false and gpg_keys => xxxx' do
+        let(:params) { {
+          'auto_unseal' => false,
+          'gpg_keys'    => xxxxx
+        } }
+      end
+
+      context 'with auto_unseal => false' do
+        let(:params) { {
+          'auto_unseal' => false
+        } }
+
+        it { expect { catalogue }.to raise_error(Puppet::ParseError, /without auto_unseal, at least one GPG key has to be provided/) }
       end
     end
   end
