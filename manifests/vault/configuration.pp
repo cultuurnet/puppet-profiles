4e5d8ec0d428107ad716ce81b1348a92bb690e4d
Kristof Willaert <kristof@publiq.be>
2025-01-14 13:12:16 +0100

    Set correct service address and cert in configuration

--- manifests/vault/configuration.pp
+++ manifests/vault/configuration.pp
@@ -3,14 +3,12 @@ class profiles::vault::configuration (
   String           $service_address = '127.0.0.1'
 ) inherits ::profiles {
 
-  shellvar { 'VAULT_ADDR environment variable':
-    ensure   => 'present',
-    target   => '/etc/environment',
-    variable => 'VAULT_ADDR',
-    value    => 'https://127.0.0.1:8200'
-  }
+  realize Group['vault']
+  realize User['vault']
 
   if $certname {
+    $cert = $certname
+
     shellvar { 'VAULT_CACERT environment variable':
       ensure   => 'present',
       target   => '/etc/environment',
@@ -21,5 +19,23 @@ class profiles::vault::configuration (
     class { 'profiles::vault::certificate':
       certname => $certname
     }
+  } else {
+    $cert = 'tls'
+  }
+
+  shellvar { 'VAULT_ADDR environment variable':
+    ensure   => 'present',
+    target   => '/etc/environment',
+    variable => 'VAULT_ADDR',
+    value    => 'https://127.0.0.1:8200'
+  }
+
+  file { 'vault configuration':
+    ensure  => 'file',
+    path    => '/etc/vault.d/vault.hcl',
+    owner   => 'vault',
+    group   => 'vault',
+    content => template('profiles/vault/vault.hcl.erb'),
+    require => [Group['vault'], User['vault']]
   }
 }
--- spec/classes/vault/configuration_spec.rb
+++ spec/classes/vault/configuration_spec.rb
@@ -10,10 +10,24 @@ describe 'profiles::vault::configuration' do
 
         it { is_expected.to compile.with_all_deps }
 
+        it { is_expected.to contain_group('vault') }
+        it { is_expected.to contain_user('vault') }
+
         it { is_expected.to contain_class('profiles::vault::configuration').with(
           'service_address' => '127.0.0.1',
           'certname'        => nil
         ) }
+
+        it { is_expected.to contain_file('vault configuration').with(
+          'ensure' => 'file',
+          'path'   => '/etc/vault.d/vault.hcl',
+          'owner'  => 'vault',
+          'group'  => 'vault'
+        ) }
+
+        it { is_expected.to contain_file('vault configuration').with_content(/^\s*address\s+=\s+"127\.0\.0\.1:8200"$/) }
+        it { is_expected.to contain_file('vault configuration').with_content(/^\s*tls_cert_file\s+=\s+"\/opt\/vault\/tls\/tls\.crt"$/) }
+        it { is_expected.to contain_file('vault configuration').with_content(/^\s*tls_key_file\s+=\s+"\/opt\/vault\/tls\/tls\.key"$/) }
 
         it { is_expected.to contain_shellvar('VAULT_ADDR environment variable').with(
           'ensure'   => 'present',
@@ -24,6 +38,9 @@ describe 'profiles::vault::configuration' do
 
         it { is_expected.not_to contain_shellvar('VAULT_CACERT environment variable') }
         it { is_expected.not_to contain_class('profiles::vault::certificate') }
+
+        it { is_expected.to contain_file('vault configuration').that_requires('Group[vault]') }
+        it { is_expected.to contain_file('vault configuration').that_requires('User[vault]') }
       end
 
       context 'with service_address => 0.0.0.0 and certname => vault.example.com' do
@@ -42,6 +59,10 @@ describe 'profiles::vault::configuration' do
           'target'   => '/etc/environment',
           'value'    => '/etc/puppetlabs/puppet/ssl/certs/ca.pem',
         ) }
+
+        it { is_expected.to contain_file('vault configuration').with_content(/^\s*address\s+=\s+"0\.0\.0\.0:8200"$/) }
+        it { is_expected.to contain_file('vault configuration').with_content(/^\s*tls_cert_file\s+=\s+"\/opt\/vault\/tls\/vault\.example\.com\.crt"$/) }
+        it { is_expected.to contain_file('vault configuration').with_content(/^\s*tls_key_file\s+=\s+"\/opt\/vault\/tls\/vault\.example\.com\.key"$/) }
       end
     end
   end
