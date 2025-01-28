b47f0ceb50beb2a9fb7025e50497533dc2b24190
Paul Herbosch <paul.herbosch@publiq.be>
2025-01-28 16:11:39 +0100

    Add update/create logic for ES indices

--- manifests/uitpas/website/elasticsearch.pp
+++ manifests/uitpas/website/elasticsearch.pp
@@ -18,12 +18,20 @@ class profiles::uitpas::website::elasticsearch (
     require => File[$basedir]
   }
 
-  exec { 'elasticsearch balies mapping':
-    command     => "/usr/bin/curl -XPUT 'localhost:9200/balies?pretty -H 'Content-Type: application/json' -d @${basedir}/mapping_balies.json",
+  exec { 'create elasticsearch balies mapping':
+    command     => "/usr/bin/curl -XPUT 'localhost:9200/balies?pretty' -H 'Content-Type: application/json' -d @${basedir}/mapping_balies.json",
     logoutput   => true,
     refreshonly => true,
     subscribe   => File['uitpas-elasticsearch-balies-mapping'],
-    require     => File['uitpas-elasticsearch-balies-mapping']
+    unless      => '/usr/bin/curl -s localhost:9200/balies/_mappings | /usr/bin/jq -e .balies'
+  }
+
+  exec { 'update elasticsearch balies mapping':
+    command     => "/usr/bin/curl -XPATCH 'localhost:9200/balies?pretty' -H 'Content-Type: application/json' -d @${basedir}/mapping_balies.json",
+    logoutput   => true,
+    refreshonly => true,
+    subscribe   => File['uitpas-elasticsearch-balies-mapping'],
+    onlyif      => '/usr/bin/curl -s localhost:9200/balies/_mappings | /usr/bin/jq -e .balies'
   }
 
   file { 'uitpas-elasticsearch-promotions-mapping':
@@ -33,11 +41,19 @@ class profiles::uitpas::website::elasticsearch (
     require => File[$basedir]
   }
 
-  exec { 'elasticsearch promotions mapping':
-    command     => "/usr/bin/curl -XPUT 'localhost:9200/promotions?pretty -H 'Content-Type: application/json' -d @${basedir}/mapping_promotions.json",
+  exec { 'create elasticsearch promotions mapping':
+    command     => "/usr/bin/curl -XPUT 'localhost:9200/promotions?pretty' -H 'Content-Type: application/json' -d @${basedir}/mapping_promotions.json",
     logoutput   => true,
     refreshonly => true,
     subscribe   => File['uitpas-elasticsearch-promotions-mapping'],
-    require     => File['uitpas-elasticsearch-promotions-mapping']
+    unless      => '/usr/bin/curl -s localhost:9200/promotions/_mappings | /usr/bin/jq -e .promotions'
+  }
+
+  exec { 'update elasticsearch promotions mapping':
+    command     => "/usr/bin/curl -XPATCH 'localhost:9200/promotions?pretty' -H 'Content-Type: application/json' -d @${basedir}/mapping_promotions.json",
+    logoutput   => true,
+    refreshonly => true,
+    subscribe   => File['uitpas-elasticsearch-promotions-mapping'],
+    onlyif      => '/usr/bin/curl -s localhost:9200/promotions/_mappings | /usr/bin/jq -e .promotions'
   }
 }
