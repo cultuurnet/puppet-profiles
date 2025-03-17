eb737408ac7c84a32926d3cae1a133e5b6a40c18
Vincent Verbist <vincent.verbist@publiq.be>
2025-03-17 13:36:57 +0100

    add aws_cli profile

---
+++ manifests/aws_cli.pp
@@ -0,0 +1,12 @@
+class profiles::aws_cli (
+  String $version           = 'latest',
+  String $terrafile_version = 'latest'
+) inherits ::profiles {
+
+  realize Apt::Source['publiq-tools']
+
+  package { 'aws-cli':
+    ensure  => $version,
+    require => Apt::Source['publiq-tools']
+  }
+}
