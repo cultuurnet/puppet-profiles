9f0684a50e121453ba8fb6334fc94b1014a30f70
Paul Herbosch <paul.herbosch@publiq.be>
2025-03-27 11:45:52 +0100

    add php and newrelic agent to testserver

--- manifests/testproject/testcomponent.pp
+++ manifests/testproject/testcomponent.pp
@@ -9,4 +9,12 @@ class profiles::testproject::testcomponent (
     path    => '/tmp/testproject.json',
     content => template($config_source)
   }
+
+  include profiles::php
+  include profiles::newrelic::php
+
+  class profiles::apache::vhost::php_fpm { 'testproject':
+    'basedir'              => '/var/www/',
+    'public_web_directory' => 'html'
+  }
 }
