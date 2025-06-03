8e4711ec327b79a14bef7b7723d385ddf6886546
Kristof Willaert <kristof@publiq.be>
2025-06-03 15:08:27 +0200

    Add MySQL server, database and application user for platform in the relevant profile

--- manifests/platform.pp
+++ manifests/platform.pp
@@ -1,22 +1,38 @@
 class profiles::platform (
   String                        $servername,
-  Variant[String,Array[String]] $serveraliases = [],
-  Boolean                       $deployment    = true
+  String                        $database_password,
+  Variant[String,Array[String]] $serveraliases     = [],
+  Boolean                       $deployment        = true
 ) inherits ::profiles {
 
-  $basedir = '/var/www/platform-api'
+  $basedir       = '/var/www/platform-api'
+  $database_name = 'platform'
+  $database_user = 'platform'
 
   realize Group['www-data']
   realize User['www-data']
 
   include ::profiles::apache
   include ::profiles::php
+  include ::profiles::mysql::server
 
   file { $basedir:
     ensure  => 'directory',
     owner   => 'www-data',
     group   => 'www-data',
     require => [Group['www-data'], User['www-data']]
+  }
+
+  mysql_database { $database_name:
+    charset => 'utf8mb4',
+    collate => 'utf8mb4_unicode_ci',
+    require => Class['profiles::mysql::server']
+  }
+
+  profiles::mysql::app_user { "${database_user}@${database_name}":
+    password => $database_password,
+    remote   => true,
+    require  => Mysql_database[$database_name]
   }
 
   profiles::apache::vhost::php_fpm { "http://${servername}":
@@ -27,6 +43,7 @@ class profiles::platform (
 
   if $deployment {
     class { 'profiles::platform::deployment':
+      require   => Profiles::Mysql::App_user["${database_user}@${database_name}"],
       subscribe => Class['profiles::php']
     }
   }
