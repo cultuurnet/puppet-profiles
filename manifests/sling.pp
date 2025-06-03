acfa7258ca6266b471018193e6f3327f4ca5cc47
Vincent Verbist <vincent.verbist@publiq.be>
2025-06-03 09:28:13 +0200

    Adds optional Sling deployment
    Introduces a boolean parameter to control Sling package installation, allowing users to skip the deployment if needed.

--- manifests/sling.pp
+++ manifests/sling.pp
@@ -1,11 +1,15 @@
 class profiles::sling (
   String $version           = 'latest',
+  Optional[Boolean] $deploy               = false,
+
 ) inherits ::profiles {
 
   realize Apt::Source['publiq-tools']
 
-  package { 'sling':
-    ensure  => $version,
-    require => Apt::Source['publiq-tools']
+  if $deploy {
+    package { 'sling':
+      ensure  => $version,
+      require => Apt::Source['publiq-tools']
+    }
   }
 }
