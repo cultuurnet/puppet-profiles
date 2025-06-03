d15477adce4cc6f4cc5b73c2f37d0f3deea6ecec
Vincent Verbist <vincent.verbist@publiq.be>
2025-06-03 10:08:05 +0200

    Adds Sling deployment functionality
    Adds the ability to deploy Sling with configurable version and database integration.
    
    Includes a random password generation feature for application users, enhancing security when deploying with a database.

--- manifests/sling.pp
+++ manifests/sling.pp
@@ -1,15 +1,27 @@
 class profiles::sling (
-  String $version           = 'latest',
-  Optional[Boolean] $deploy = false,
-
+  String $version                 = 'latest',
+  Optional[Boolean] $deploy       = false,
+  Optional[String] $database_name = undef,
 ) inherits ::profiles {
 
   realize Apt::Source['publiq-tools']
 
+  # Generate a random password if deploying
+  if $deploy and $database_name {
+    $app_user_password = fqdn_rand_string(20, "${database_name}_sling_password")
+  }
+
   if $deploy {
     package { 'sling':
       ensure  => $version,
-      require => Apt::Source['publiq-tools']
+      require => Apt::Source['publiq-tools'],
+    }
+
+    profiles::mysql::app_user { "sling@${database_name}":
+      password => $app_user_password,
+      readonly => true,
+      remote   => true,
+      require  => Mysql_database[$database_name],
     }
   }
 }
