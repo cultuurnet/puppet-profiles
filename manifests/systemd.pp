cd61d87f54ecc85933760068311df7edc4fd980b
Vincent Verbist <vincent.verbist@publiq.be>
2025-06-06 09:47:46 +0200

    Refactor profile for systemd

--- manifests/systemd.pp
+++ manifests/systemd.pp
@@ -1,20 +1,9 @@
 class profiles::systemd (
   String $system_max_use   = '500M'
-) inherits ::profiles {
-  include systemd
-
-  systemd::journald { 'SystemMaxUse':
-    settings => {
-      'Journal' => {
-        'SystemMaxUse' => $system_max_use,
-      },
-    },
-    notify => Exec['systemd-journald-reload'],
-  }
-
-  exec { 'systemd-journald-reload':
-    command     => '/bin/systemctl reload systemd-journald',
-    refreshonly => true,
-    path        => ['/bin', '/usr/bin'],
+) inherits profiles {
+  class {
+    'systemd': journald_settings => {
+      SystemMaxUse => $system_max_use,
+    }
   }
 }
