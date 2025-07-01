2aa92a68b41f1a6c64d7eda58301835c0fffe449
Vincent Verbist <vincent.verbist@publiq.be>
2025-07-01 10:59:42 +0200

    add RDS backup cronjobs

---
+++ manifests/backup/rds.pp
@@ -0,0 +1,27 @@
+class profiles::backup::rds (
+  String $backupdir                   = '/data/rdsbackups',
+) inherits ::profiles {
+
+file { $backupdir:
+      ensure  => 'directory',
+      owner   => 'ubuntu',
+      group   => 'ubuntu',
+}
+
+$config = lookup('rds_hiera_configs', { merge => 'hash' })
+
+$config.each |$db_key, $db| {
+  $user     = $db['terraform::rds::user']
+  $password = $db['terraform::rds::password']
+  $host     = $db['terraform::rds::host']
+  $mysqld_version = $db['terraform::rds::mysqld_version']
+
+  cron { "rds-backup-${db_key}":
+    ensure  => present,
+    user    => 'ubuntu',
+    minute  => '0',
+    hour    => '2',
+    weekday => '0',
+    command => "/usr/bin/mysqldump --host=${host} --user=${user} --password='${password}' --databases $(/usr/bin/mysql --host=${host} --user=${user} --password='${password}' -N -e \"SHOW DATABASES;\" | grep -Ev '^(information_schema|performance_schema|mysql|sys)$') | gzip > ${backupdir}/${db_key}-$(date +\\%F).sql.gz",
+  }
+}
