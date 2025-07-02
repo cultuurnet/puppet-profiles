292bb7b4d69bb22da015db24958bd7e8861bad4b
Vincent Verbist <vincent.verbist@publiq.be>
2025-07-02 13:18:56 +0200

    Dump RDS via shell script

--- manifests/backup/rds.pp
+++ manifests/backup/rds.pp
@@ -9,24 +9,22 @@ class profiles::backup::rds (
 
   $config = lookup('rds_hiera_configs', { merge => 'hash' })
 
-  $config.each |$db_key, $db| {
-    $user     = $db['terraform::rds::user']
-    $password = $db['terraform::rds::password']
-    $host     = $db['terraform::rds::host']
-    $mysqld_version = $db['terraform::rds::mysqld_version']
+  file { '/usr/local/bin/dump_rds.sh':
+    ensure  => file,
+    owner   => 'ubuntu',
+    group   => 'ubuntu',
+    mode    => '0750',
+    content => template('profiles/backupserver/dump_rds.sh.erb'),
+  }
 
-    cron { "rds-backup-${db_key}":
-      ensure      => present,
-      environment => ['TZ=Europe/Brussels', 'MAILTO=infra+cron@publiq.be'],
-      user        => 'ubuntu',
-      minute      => '0',
-      hour        => '2',
-      weekday     => '0',
-      command     => "/usr/bin/mysqldump --host=${host} --user=${user} --password='${password}' " +
-      "--databases $(/usr/bin/mysql --host=${host} --user=${user} --password='${password}' " +
-      "-N -e \"SHOW DATABASES;\" | grep -Ev '^(information_schema|performance_schema|mysql|sys)$') " +
-      "| gzip > ${backupdir}/${db_key}-$(date +\\%F).sql.gz",
-    }
+  cron { 'rds-backup-all':
+    ensure      => present,
+    environment => ['TZ=Europe/Brussels', 'MAILTO=infra+cron@publiq.be'],
+    user        => 'ubuntu',
+    minute      => '0',
+    hour        => '2',
+    weekday     => '0',
+    command     => '/usr/local/bin/dump_rds.sh',
   }
   cron { 'rds-backup-cleanup':
     ensure      => present,
