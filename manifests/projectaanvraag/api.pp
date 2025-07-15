de7508b5a50107b468f3441bd1784506ce81bd5f
Kristof Willaert <kristof@publiq.be>
2025-07-15 16:41:49 +0200

    Add projectaanvraag API profile

---
+++ manifests/projectaanvraag/api.pp
@@ -0,0 +1,68 @@
+class profiles::projectaanvraag::api (
+  String                         $database_password,
+  String                         $servername,
+  Variant[String, Array[String]] $serveraliases     = [],
+  String                         $database_host     = '127.0.0.1',
+  Boolean                        $deployment        = true
+) inherits ::profiles {
+
+  $basedir       = '/var/www/projectaanvraag-api'
+  $database_name = 'projectaanvraag'
+  $database_user = 'projectaanvraag'
+
+  include profiles::redis
+  include profiles::mongodb
+  include profiles::apache
+  include profiles::php
+
+  if $database_host == '127.0.0.1' {
+    $database_host_remote    = false
+    $database_host_available = true
+
+    include profiles::mysql::server
+
+    Class['profiles::mysql::server'] -> Mysql_database[$database_name]
+  } else {
+    $database_host_remote = true
+
+    class { 'profiles::mysql::remote_server':
+      host => $database_host
+    }
+
+    if $facts['mysqld_version'] {
+      $database_host_available = true
+    } else {
+      $database_host_available = false
+    }
+  }
+
+  if $database_host_available {
+    mysql_database { $database_name:
+      charset => 'utf8mb4',
+      collate => 'utf8mb4_0900_ai_ci'
+    }
+
+    profiles::mysql::app_user { "${database_user}@${database_name}":
+      password => $database_password,
+      remote   => $database_host_remote,
+      require  => Mysql_database[$database_name]
+    }
+
+    if $deployment {
+      class { 'profiles::projectaanvraag::api::deployment':
+        database_name => $database_name
+      }
+
+      Profiles::Mysql::App_user["${database_user}@${database_name}"] -> Class['profiles::projectaanvraag::api::deployment']
+      Class['profiles::redis'] -> Class['profiles::projectaanvraag::api::deployment']
+      Class['profiles::mongodb'] -> Class['profiles::projectaanvraag::api::deployment']
+      Class['profiles::php'] ~> Class['profiles::projectaanvraag::api::deployment']
+    }
+  }
+
+  profiles::apache::vhost::php_fpm { "http://${servername}":
+    basedir               => $basedir,
+    public_web_directory  => 'web',
+    aliases               => $serveraliases
+  }
+}
---
+++ spec/classes/projectaanvraag/api_spec.rb
@@ -0,0 +1,118 @@
+describe 'profiles::projectaanvraag::api' do
+  include_examples 'operating system support'
+
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) { facts }
+
+      context 'with hieradata' do
+        let(:hiera_config) { 'spec/support/hiera/common.yaml' }
+
+        context 'with database_password => mypassword and servername => uitdatabank.example.com' do
+          let(:params) { {
+            'database_password' => 'mypassword',
+            'servername'        => 'projectaanvraag-api.example.com'
+          } }
+
+          context "with class profiles::mysql::server present" do
+            let(:pre_condition) { 'include profiles::mysql::server' }
+
+            it { is_expected.to compile.with_all_deps }
+
+            it { is_expected.to contain_class('profiles::projectaanvraag::api').with(
+              'database_password' => 'mypassword',
+              'database_host'     => '127.0.0.1',
+              'servername'        => 'projectaanvraag-api.example.com',
+              'serveraliases'     => [],
+              'deployment'        => true
+            ) }
+
+            it { is_expected.to contain_class('profiles::mysql::server') }
+            it { is_expected.to contain_class('profiles::redis') }
+            it { is_expected.to contain_class('profiles::mongodb') }
+            it { is_expected.to contain_class('profiles::apache') }
+            it { is_expected.to contain_class('profiles::php') }
+            it { is_expected.to contain_class('profiles::projectaanvraag::api::deployment').with(
+              'database_name' => 'projectaanvraag'
+            ) }
+
+            it { is_expected.to contain_profiles__apache__vhost__php_fpm('http://projectaanvraag-api.example.com').with(
+              'basedir'              => '/var/www/projectaanvraag-api',
+              'public_web_directory' => 'web',
+              'aliases'              => []
+            ) }
+
+            it { is_expected.to contain_mysql_database('projectaanvraag').with(
+              'charset' => 'utf8mb4',
+              'collate' => 'utf8mb4_0900_ai_ci'
+            ) }
+
+            it { is_expected.to contain_profiles__mysql__app_user('projectaanvraag@projectaanvraag').with(
+              'password' => 'mypassword',
+              'remote'   => false
+            ) }
+
+            it { is_expected.to contain_mysql_database('projectaanvraag').that_comes_before('Profiles::Mysql::App_user[projectaanvraag@projectaanvraag]') }
+            it { is_expected.to contain_mysql_database('projectaanvraag').that_requires('Class[profiles::mysql::server]') }
+            it { is_expected.to contain_profiles__mysql__app_user('projectaanvraag@projectaanvraag').that_comes_before('Class[profiles::projectaanvraag::api::deployment]') }
+            it { is_expected.to contain_class('profiles::redis').that_comes_before('Class[profiles::projectaanvraag::api::deployment]') }
+            it { is_expected.to contain_class('profiles::mongodb').that_comes_before('Class[profiles::projectaanvraag::api::deployment]') }
+            it { is_expected.to contain_class('profiles::php').that_notifies('Class[profiles::projectaanvraag::api::deployment]') }
+          end
+        end
+
+        context 'with database_password => secret, database_host => foo.example.com, servername => bar.example.com, serveraliases => baz.example.com and deployment => false' do
+          let(:params) { {
+            'database_password' => 'secret',
+            'database_host'     => 'foo.example.com',
+            'servername'        => 'bar.example.com',
+            'serveraliases'     => 'baz.example.com',
+            'deployment'        => false
+          } }
+
+          it { is_expected.to compile.with_all_deps }
+
+          it { is_expected.to contain_class('profiles::mysql::remote_server').with(
+            'host' => 'foo.example.com'
+          ) }
+
+          it { is_expected.not_to contain_class('profiles::projectaanvraag::api::deployment') }
+
+          it { is_expected.to contain_profiles__apache__vhost__php_fpm('http://bar.example.com').with(
+            'basedir'              => '/var/www/projectaanvraag-api',
+            'public_web_directory' => 'web',
+            'aliases'              => 'baz.example.com'
+          ) }
+
+          context "with fact mysqld_version => 8.0.33" do
+            let(:facts) { facts.merge( { 'mysqld_version' => '8.0.33' } ) }
+
+            it { is_expected.to contain_mysql_database('projectaanvraag').with(
+              'charset' => 'utf8mb4',
+              'collate' => 'utf8mb4_0900_ai_ci'
+            ) }
+
+            it { is_expected.to contain_profiles__mysql__app_user('projectaanvraag@projectaanvraag').with(
+              'password' => 'secret',
+              'remote'   => true
+            ) }
+
+            it { is_expected.to contain_mysql_database('projectaanvraag').that_comes_before('Profiles::Mysql::App_user[projectaanvraag@projectaanvraag]') }
+          end
+
+          context "without extra facts" do
+            it { is_expected.not_to contain_mysql_database('projectaanvraag') }
+            it { is_expected.not_to contain_profiles__mysql__app_user('projectaanvraag@projectaanvraag') }
+          end
+        end
+      end
+
+      context 'without parameters' do
+        let(:params) { {} }
+
+        it { expect { catalogue }.to raise_error(Puppet::ParseError, /expects a value for parameter 'database_password'/) }
+        it { expect { catalogue }.to raise_error(Puppet::ParseError, /expects a value for parameter 'servername'/) }
+      end
+    end
+  end
+end
