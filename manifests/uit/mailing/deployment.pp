558a79eb9edad515d4590539bf19969bae661ce4
Vincent Verbist <vincent.verbist@publiq.be>
2025-07-29 15:40:21 +0200

    Add manifests for uitid-mailing

---
+++ manifests/uit/mailing/deployment.pp
@@ -0,0 +1,30 @@
+class profiles::uit::mailing::deployment (
+
+  String           $version           = 'latest',
+  String           $repository        = 'uitid-mailing',
+  String           $config_source ,
+  Integer          $portbase          = 4800,
+) inherits profiles {
+
+  $database_name = 'uit_maling'
+  $database_user = 'uit_maling'
+
+  realize Apt::Source[$repository]
+  realize User['glassfish']
+
+  package { 'uitid-mailing':
+    ensure  => $version,
+    require => Apt::Source[$repository],
+    notify  => [App['uitid-mailing']],
+  }
+  app { 'uitid-mailing':
+    ensure        => 'present',
+    portbase      => String($portbase),
+    user          => 'glassfish',
+    passwordfile  => '/home/glassfish/asadmin.pass',
+    contextroot   => 'mailing',
+    precompilejsp => false,
+    source        => '/opt/uitid-mailing/uitid-mailing.war',
+    require       => [User['glassfish']],
+  }
+}
