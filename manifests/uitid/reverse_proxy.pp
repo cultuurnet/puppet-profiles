b6a76e6c476c7a0f5141977e27b1387761c98859
Vincent Verbist <vincent.verbist@publiq.be>
2025-08-20 14:35:05 +0200

    Update proxy

--- manifests/uitid/reverse_proxy.pp
+++ manifests/uitid/reverse_proxy.pp
@@ -1,9 +1,15 @@
 class profiles::uitid::reverse_proxy (
   String $servername,
+  Optional[String]              $certificate  = 'wildcard.uitid.be',
   Variant[String,Array[String]] $serveraliases = [],
+
   Hash $settings                               = {}
 
 ) inherits profiles {
-  include profiles::apache 
-
+  include nginx
+  unless ($certificate) {
+    fail('No valid certificate provided')
+  }
+  realize Profiles::Certificate[$certificate]
+  realize Firewall['300 accept HTTPS traffic']
 }
