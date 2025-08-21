328151bf9e47c4a63b6e243e2c21c6c50bfa2e3b
Kristof Willaert <kristof@publiq.be>
2025-08-21 16:40:44 +0200

    Proxy /upload and avoid caching index.html and config.json

--- manifests/widgetbeheer/frontend.pp
+++ manifests/widgetbeheer/frontend.pp
@@ -1,10 +1,12 @@
 class profiles::widgetbeheer::frontend (
   String                         $servername,
+  Stdlib::Httpurl                $api_url,
   Variant[String, Array[String]] $serveraliases = [],
   Boolean                        $deployment    = true
 ) inherits ::profiles {
 
-  $basedir = '/var/www/widgetbeheer-frontend'
+  $basedir       = '/var/www/widgetbeheer-frontend'
+  $api_transport = split($api_url, ':')[0]
 
   realize Group['www-data']
   realize User['www-data']
@@ -23,33 +25,40 @@ class profiles::widgetbeheer::frontend (
   }
 
   profiles::apache::vhost::basic { "http://${servername}":
-    documentroot  => $basedir,
-    serveraliases => $serveraliases,
-    rewrites      => {
-                       comment      => 'Send all requests through index.html',
-                       rewrite_cond => [
-                                         "${basedir}%{REQUEST_FILENAME} !-f",
-                                         "${basedir}%{REQUEST_FILENAME} !-d"
-                                       ],
-                       rewrite_rule => '. /index.html [L]'
-                     },
-    directories   => [{
-                       path     => 'index.html',
-                       provider => 'files',
-                       headers  => [
-                                     'set Cache-Control "max-age=0, no-cache, no-store, must-revalidate"',
-                                     'set Pragma "no-cache"',
-                                     'set Expires "Wed, 1 Jan 1970 00:00:00 GMT"'
-                                   ]
-                     }, {
-                       path     => 'config.json',
-                       provider => 'files',
-                       headers  => [
-                                     'set Cache-Control "max-age=0, no-cache, no-store, must-revalidate"',
-                                     'set Pragma "no-cache"',
-                                     'set Expires "Wed, 1 Jan 1970 00:00:00 GMT"'
-                                   ]
-                     }]
+    documentroot    => $basedir,
+    serveraliases   => $serveraliases,
+    rewrites        => [{
+                         comment      => 'Proxy /upload endpoint to API',
+                         rewrite_rule => "^/upload$ ${api_url}/upload [P]"
+                       }, {
+                         comment      => 'Send all requests through index.html',
+                         rewrite_cond => [
+                                           "${basedir}%{REQUEST_FILENAME} !-f",
+                                           "${basedir}%{REQUEST_FILENAME} !-d"
+                                         ],
+                         rewrite_rule => '. /index.html [L]'
+                       }],
+    directories     => [{
+                         path     => 'index.html',
+                         provider => 'files',
+                         headers  => [
+                                       'set Cache-Control "max-age=0, no-cache, no-store, must-revalidate"',
+                                       'set Pragma "no-cache"',
+                                       'set Expires "Wed, 1 Jan 1970 00:00:00 GMT"'
+                                     ]
+                       }, {
+                         path     => 'config.json',
+                         provider => 'files',
+                         headers  => [
+                                       'set Cache-Control "max-age=0, no-cache, no-store, must-revalidate"',
+                                       'set Pragma "no-cache"',
+                                       'set Expires "Wed, 1 Jan 1970 00:00:00 GMT"'
+                                     ]
+                       }],
+    ssl_proxyengine => $api_transport ? {
+                         'https' => true,
+                         'http'  => false
+                       }
   }
 
   # include ::profiles::widgetbeheer::frontend::monitoring
