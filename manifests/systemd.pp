b28b5e392575600ee318425bd15a687b44afbad3
Kristof Willaert <kristof@publiq.be>
2025-08-29 09:05:40 +0200

    Harmonize spacing

--- manifests/systemd.pp
+++ manifests/systemd.pp
@@ -1,9 +1,10 @@
 class profiles::systemd (
-  String $system_max_use   = '500M'
-) inherits profiles {
-  class {
-    'systemd': journald_settings => {
-      'SystemMaxUse' => $system_max_use,
-    }
+  String $journald_system_max_use = '500M'
+) inherits ::profiles {
+
+  class { 'systemd':
+    journald_settings => {
+                           'SystemMaxUse' => $journald_system_max_use
+                         }
   }
 }
