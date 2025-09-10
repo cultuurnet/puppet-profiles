a6dd99fc58739f6e881dc8bde5f784fc3aa163ab
Kristof Willaert <kristof@publiq.be>
2025-09-10 11:50:34 +0200

    Add UiT frontend service watchdog that triggers on 90% heap memory used

--- spec/classes/uit/frontend/deployment_spec.rb
+++ spec/classes/uit/frontend/deployment_spec.rb
@@ -20,6 +20,7 @@ describe 'profiles::uit::frontend::deployment' do
           'service_status'    => 'running',
           'service_address'   => '127.0.0.1',
           'service_port'      => 3000,
+          'service_watchdog'  => false,
           'puppetdb_url'      => nil
         ) }
 
@@ -58,6 +59,11 @@ describe 'profiles::uit::frontend::deployment' do
           'hasstatus' => true
         ) }
 
+        it { is_expected.to contain_profiles__systemd__service_watchdog('uit-frontend').with(
+          'ensure'      => 'absent',
+          'healthcheck' => "current_memory_mb=\$(( \$(/usr/bin/systemctl show -p MemoryCurrent --value uit-frontend) / ( 1024 * 1024) ))\nmaximum_memory_mb=\$(( 512 * 9 / 10 ))\n\n/usr/bin/test \${current_memory_mb} -lt \${maximum_memory_mb}\n"
+        ) }
+
         it { is_expected.to contain_package('uit-frontend').that_notifies('Service[uit-frontend]') }
         it { is_expected.to contain_file('uit-frontend-config').that_requires('Group[www-data]') }
         it { is_expected.to contain_file('uit-frontend-config').that_requires('User[www-data]') }
@@ -83,7 +89,7 @@ describe 'profiles::uit::frontend::deployment' do
     end
   end
 
-  context "with config_source => /bar, maximum_heap_size => 1024, service_address => 0.0.0.0, service_port => 3456, version => 1.2.3, repository => uit-frontend-exotic, service_status => stopped and puppetdb_url => http://example.com:8000" do
+  context "with config_source => /bar, maximum_heap_size => 1024, service_address => 0.0.0.0, service_port => 3456, version => 1.2.3, repository => uit-frontend-exotic, service_status => stopped, service_watchdog => true and puppetdb_url => http://example.com:8000" do
     let(:params) { {
       'config_source'     => '/bar',
       'version'           => '1.2.3',
@@ -92,6 +98,7 @@ describe 'profiles::uit::frontend::deployment' do
       'service_status'    => 'stopped',
       'service_address'   => '0.0.0.0',
       'service_port'      => 3456,
+      'service_watchdog'  => true,
       'puppetdb_url'      => 'http://example.com:8000'
     } }
 
@@ -117,6 +124,11 @@ describe 'profiles::uit::frontend::deployment' do
           it { is_expected.to contain_service('uit-frontend').with(
             'ensure'    => 'stopped',
             'enable'    => false
+          ) }
+
+          it { is_expected.to contain_profiles__systemd__service_watchdog('uit-frontend').with(
+            'ensure'      => 'present',
+            'healthcheck' => "current_memory_mb=\$(( \$(/usr/bin/systemctl show -p MemoryCurrent --value uit-frontend) / ( 1024 * 1024) ))\nmaximum_memory_mb=\$(( 1024 * 9 / 10 ))\n\n/usr/bin/test \${current_memory_mb} -lt \${maximum_memory_mb}\n"
           ) }
 
           it { is_expected.to contain_profiles__deployment__versions('profiles::uit::frontend::deployment').with(
