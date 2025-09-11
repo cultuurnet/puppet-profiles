7b3eb9fba6de4a0ad635e987cf4cdb2825927f22
Kristof Willaert <kristof@publiq.be>
2025-09-11 15:01:59 +0200

    Adapt existing platform::sling profile to use new sling::connection defined type

--- manifests/platform/sling.pp
+++ manifests/platform/sling.pp
@@ -1,69 +1,65 @@
 class profiles::platform::sling (
-  Optional[String] $database_name  = undef,
-  Optional[String] $project_id     = undef,
-  Optional[String] $bucket_name    = undef,
-  Boolean $dump_empty_tables       = true,
-  Integer $cron_hour               = 2,
-  String           $local_timezone = 'UTC'
+  String  $database_name,
+  String  $project_id,
+  String  $bucket_name,
+  Boolean $dump_empty_tables = true,
+  Integer $cron_hour         = 2,
+  String  $local_timezone    = 'UTC'
 
 ) inherits profiles {
 
   include profiles::sling
 
+  $database_user     = 'sling'
+  $database_password = fqdn_rand_string(20, "${database_name}_sling_password")
+  $secrets           = lookup('vault:platform/etl')
+
   realize Apt::Source['publiq-tools']
 
-
-  # Generate a random password if deploying
-  if $database_name {
-    $app_user_password = fqdn_rand_string(20, "${database_name}_sling_password")
-  }
-
-  profiles::mysql::app_user { "sling@${database_name}":
+  profiles::mysql::app_user { "${database_user}@${database_name}":
     password => $app_user_password,
     readonly => true,
-    remote   => false,
+    remote   => false
   }
 
-  if $project_id {
-    $secrets = lookup('vault:platform/etl')
+  profiles::sling::connection { $database_name:
+    type          => 'mysql',
+    configuration => {
+                        user     => $database_user,
+                        password => $database_password,
+                        database => $database_name
+                     },
+    require       => Profiles::Mysql::App_user["${database_user}@${database_name}"]
+  }
 
-    profiles::google::gcloud { 'root':
-      credentials => {
-        project_id     => $project_id,
-        private_key_id => $secrets['gcloud_private_key_id'],
-        private_key    => $secrets['gcloud_private_key'],
-        client_id      => $secrets['gcloud_client_id'],
-        client_email   => $secrets['gcloud_client_email'],
-      },
+  profiles::google::gcloud { 'root':
+    credentials => {
+      project_id     => $project_id,
+      private_key_id => $secrets['gcloud_private_key_id'],
+      private_key    => $secrets['gcloud_private_key'],
+      client_id      => $secrets['gcloud_client_id'],
+      client_email   => $secrets['gcloud_client_email']
     }
   }
-
 
   file { '/data/parquetdumps':
-    ensure => 'directory',
+    ensure => 'directory'
   }
 
-  file { '/root/.sling/env.yaml':
+  file { 'parquetdump_to_gcs':
     ensure  => 'file',
-    content => template('profiles/sling/sling.env.erb'),
+    path    => '/usr/local/bin/parquetdump_to_gcs',
+    mode    => '0755',
+    content => template('profiles/sling/parquetdump_to_gcs.sh.erb'),
+    require => Profiles::Google::Gcloud['root']
   }
 
-  if $bucket_name {
-    file { 'parquetdump_to_gcs':
-      ensure  => 'file',
-      path    => '/usr/local/bin/parquetdump_to_gcs',
-      mode    => '0755',
-      content => template('profiles/sling/parquetdump_to_gcs.sh.erb'),
-      require => Profiles::Google::Gcloud['root'],
-    }
-
-    cron { 'parquetdump_to_gcs':
-      ensure  => 'present',
-      command => '/usr/local/bin/parquetdump_to_gcs',
-      environment => ['SHELL=/bin/bash', "TZ=${local_timezone}", 'MAILTO=infra+cron@publiq.be'],
-      hour    => $cron_hour,
-      minute  => 0,
-      require => File['parquetdump_to_gcs'],
-    }
+  cron { 'parquetdump_to_gcs':
+    ensure      => 'present',
+    command     => '/usr/local/bin/parquetdump_to_gcs',
+    environment => ['SHELL=/bin/bash', "TZ=${local_timezone}", 'MAILTO=infra+cron@publiq.be'],
+    hour        => $cron_hour,
+    minute      => 0,
+    require     => File['parquetdump_to_gcs']
   }
 }
