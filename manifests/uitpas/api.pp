b640de5379124a464c0342b36b24234e862aca7f
Vincent Verbist <vincent.verbist@publiq.be>
2025-09-18 16:21:04 +0200

    Update file names

--- manifests/uitpas/api.pp
+++ manifests/uitpas/api.pp
@@ -14,6 +14,9 @@ class profiles::uitpas::api (
   String                         $magda_soap_path,
   String                         $magda_soap_keystore,
   String                         $magda_soap_truststore,
+  String                         $magda_soap_cert_password,
+  String                         $magda_soap_key_password,
+  String                         $magda_soap_alias,
   Optional[String]               $newrelic_license_key = lookup('data::newrelic::license_key', Optional[String], 'first', undef),
   Integer                        $portbase             = 4800,
   Enum['running', 'stopped']     $service_status       = 'running',
@@ -40,45 +43,60 @@ class profiles::uitpas::api (
   $magda_soap_keystorepath = "${magda_soap_path}/${magda_soap_keystore}"
   $magda_soap_truststorepath  = "${magda_soap_path}/${magda_soap_truststore}"
 
-  file { "${magda_soap_path}":
+  file { $magda_soap_path:
     ensure => 'directory',
     owner  => 'glassfish',
     group  => 'glassfish',
     mode   => '0755',
   }
-  file { "${magda_sftp_path}":
+  file { $magda_sftp_path:
     ensure => 'directory',
     owner  => 'glassfish',
     group  => 'glassfish',
     mode   => '0755',
   }
-  file { "${magda_sftp_certpath}":
+  file { $magda_sftp_certpath:
     ensure  => 'file',
     content => $secrets["magda-sftp-crt"],
     owner   => 'glassfish',
     group   => 'glassfish',
     mode    => '0644',
+    require => File[$magda_sftp_path],
   }
-  file { "${magda_sftp_keypath}":
+  file { $magda_sftp_keypath:
     ensure  => 'file',
     content => $secrets["magda-sftp-key"],
     owner   => 'glassfish',
     group   => 'glassfish',
     mode    => '0600',
+    require => File[$magda_sftp_path],
   }
-  file { '/tmp/magda-soap-cert.crt':
+  file { "${magda_soap_path}/magda-soap-cert.crt":
     ensure  => 'file',
     content => $secrets["magda-soap-crt"],
     owner   => 'glassfish',
     group   => 'glassfish',
     mode    => '0644',
+    require => File[$magda_soap_path],
   }
-  file { '/tmp/magda-soap-key.pem':
+  file { "${magda_soap_path}/magda-soap-key.pem":
     ensure  => 'file',
     content => $secrets["magda-soap-key"],
     owner   => 'glassfish',
     group   => 'glassfish',
     mode    => '0600',
+    require => File[$magda_soap_path],
+  }
+  openssl::export::pkcs12 { $magda_soap_alias:
+    ensure    => 'present',
+    basedir   => $magda_soap_path,
+    pkey      => '/tmp/magda-soap-key.pem',
+    cert      => '/tmp/magda-soap-cert.crt',
+    in_pass   => $magda_soap_key_password,
+    out_pass  => $magda_soap_cert_password,
+    dynamic   => true,
+    resources => File["${magda_soap_path}/magda-soap-cert.crt","${magda_soap_path}/magda-soap-key.pem"],
+    require   => [File["${magda_soap_path}/magda-soap-cert.crt"], File["${magda_soap_path}/magda-soap-key.pem"]],
   }
 
   profiles::apache::vhost::reverse_proxy { "http://${servername}":
