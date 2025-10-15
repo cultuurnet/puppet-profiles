e01da1bfdc97f0465ce2f4c530462d48494e6743
Kristof Willaert <kristof@publiq.be>
2025-10-15 16:39:37 +0200

    Notify the UiTPAS API service when JDBC connectionpool or resource change

--- spec/classes/uitpas/api_spec.rb
+++ spec/classes/uitpas/api_spec.rb
@@ -195,7 +195,9 @@ describe 'profiles::uitpas::api' do
             it { is_expected.to contain_profiles__mysql__app_user('2ndline_ro@uitpas_api').that_requires('Mysql_database[uitpas_api]') }
             it { is_expected.to contain_jdbcconnectionpool('mysql_uitpas_api_j2eePool').that_requires('Profiles::Glassfish::Domain[uitpas]') }
             it { is_expected.to contain_jdbcconnectionpool('mysql_uitpas_api_j2eePool').that_requires('Profiles::Mysql::App_user[uitpas_api@uitpas_api]') }
+            it { is_expected.to contain_jdbcconnectionpool('mysql_uitpas_api_j2eePool').that_notifies('Service[uitpas]') }
             it { is_expected.to contain_jdbcresource('jdbc/cultuurnet_uitpas').that_requires('Jdbcconnectionpool[mysql_uitpas_api_j2eePool]') }
+            it { is_expected.to contain_jdbcresource('jdbc/cultuurnet_uitpas').that_notifies('Service[uitpas]') }
             it { is_expected.to contain_set('server.network-config.protocols.protocol.http-listener-1.http.scheme-mapping').that_requires('Profiles::Glassfish::Domain[uitpas]') }
             it { is_expected.to contain_set('server.network-config.protocols.protocol.http-listener-1.http.scheme-mapping').that_notifies('Service[uitpas]') }
             it { is_expected.to contain_set('server.thread-pools.thread-pool.http-thread-pool.max-thread-pool-size').that_requires('Profiles::Glassfish::Domain[uitpas]') }
