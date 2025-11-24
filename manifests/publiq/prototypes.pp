97015a21abcbaf32890df39d164cff3fd6241a84
Kristof Willaert <kristof@publiq.be>
2025-11-24 11:29:26 +0100

    Refactor publiq::prototypes to use apache::vhost::basic profile

--- manifests/publiq/prototypes.pp
+++ manifests/publiq/prototypes.pp
@@ -1,56 +1,15 @@
 class profiles::publiq::prototypes (
-  Stdlib::Httpurl  $url,
-  Optional[String] $certificate = undef,
-  Boolean          $deployment  = true
+  Stdlib::Httpurl $url,
+  Boolean         $deployment = true
 ) inherits ::profiles {
 
-  include ::profiles::apache
-  include ::profiles::firewall::rules
-  include ::profiles::certificates
-
   $basedir    = '/var/www/prototypes'
-  $transport  = split($url, ':')[0]
   $servername = split($url, '/')[-1]
 
-  if $transport == 'https' {
-    unless $certificate {
-      fail("Class Profiles::Publiq::Prototypes expects a value for parameter 'certificate' when using HTTPS")
-    }
-
-    $https        = true
-    $port         = 443
-    $ssl_cert     = "/etc/ssl/certs/${certificate}.bundle.crt"
-    $ssl_key      = "/etc/ssl/private/${certificate}.key"
-
-    realize Profiles::Certificate[$certificate]
-    realize Firewall['300 accept HTTPS traffic']
-
-    Profiles::Certificate[$certificate] -> Apache::Vhost["${servername}_${port}"]
-    Profiles::Certificate[$certificate] ~> Class['apache::service']
-  } else {
-    $https        = false
-    $port         = 80
-    $ssl_cert     = undef
-    $ssl_key      = undef
-
-    realize Firewall['300 accept HTTP traffic']
-  }
-
-  apache::vhost { "${servername}_${port}":
-    docroot           => $basedir,
-    servername        => $servername,
-    serveraliases     => ["*.${servername}"],
-    virtual_docroot   => "${basedir}/%1",
-    docroot_owner     => 'www-data',
-    docroot_group     => 'www-data',
-    request_headers   => $profiles::apache::defaults::request_headers,
-    access_log_format => 'extended_json',
-    setenvif          => $profiles::apache::defaults::setenvif,
-    ssl               => $https,
-    port              => $port,
-    ssl_cert          => "/etc/ssl/certs/${certificate}.bundle.crt",
-    ssl_key           => "/etc/ssl/private/${certificate}.key",
-    require           => Class['profiles::apache']
+  profiles::apache::vhost::basic { $url:
+    documentroot         => $basedir,
+    serveraliases        => ["*.${servername}"],
+    virtual_documentroot => "${basedir}/%1"
   }
 
   if $deployment {
--- spec/classes/publiq/prototypes_spec.rb
+++ spec/classes/publiq/prototypes_spec.rb
@@ -14,113 +14,33 @@ describe 'profiles::publiq::prototypes' do
 
         it { is_expected.to compile.with_all_deps }
 
-        it { is_expected.to contain_class('profiles::apache') }
-
         it { is_expected.to contain_class('profiles::publiq::prototypes').with(
-          'url' => 'http://prototypes.local'
+          'url'        => 'http://prototypes.local',
+          'deployment' => true
         ) }
 
         it { is_expected.to contain_class('profiles::publiq::prototypes::deployment') }
 
-        it { is_expected.to contain_apache__vhost('prototypes.local_80').with(
-          'docroot'           => '/var/www/prototypes',
-          'servername'        => 'prototypes.local',
-          'serveraliases'     => ['*.prototypes.local'],
-          'virtual_docroot'   => '/var/www/prototypes/%1',
-          'docroot_owner'     => 'www-data',
-          'docroot_group'     => 'www-data',
-          'request_headers'   => [
-                                   'unset Proxy early',
-                                   'set X-Unique-Id %{UNIQUE_ID}e'
-                                 ],
-          'port'              => 80,
-          'access_log_format' => 'extended_json',
-          'setenvif'          => [
-                                   'X-Forwarded-Proto "https" HTTPS=on',
-                                   'X-Forwarded-For "^([^,]*),?.*" CLIENT_IP=$1'
-                                 ],
-          'ssl'               => false
+        it { is_expected.to contain_profiles__apache__vhost__basic('http://prototypes.local').with(
+          'documentroot'         => '/var/www/prototypes',
+          'serveraliases'        => ['*.prototypes.local'],
+          'virtual_documentroot' => '/var/www/prototypes/%1'
         ) }
-
-        it { is_expected.to contain_firewall('300 accept HTTP traffic') }
-
-        it {is_expected.to contain_class('profiles::apache').that_comes_before('Apache::Vhost[prototypes.local_80]') }
       end
 
       context "with url => http://prototypes.publiq.dev and deployment => false" do
         let(:params) { {
-          'url'          => 'http://prototypes.publiq.dev',
-          'deployment'   => false
+          'url'        => 'http://prototypes.publiq.dev',
+          'deployment' => false
         } }
 
         it { is_expected.to_not contain_class('profiles::publiq::prototypes::deployment') }
 
-        it { is_expected.to contain_apache__vhost('prototypes.publiq.dev_80').with(
-          'docroot'           => '/var/www/prototypes',
-          'servername'        => 'prototypes.publiq.dev',
-          'serveraliases'     => ['*.prototypes.publiq.dev'],
-          'virtual_docroot'   => '/var/www/prototypes/%1',
-          'docroot_owner'     => 'www-data',
-          'docroot_group'     => 'www-data',
-          'request_headers'   => [
-                                   'unset Proxy early',
-                                   'set X-Unique-Id %{UNIQUE_ID}e'
-                                 ],
-          'port'              => 80,
-          'access_log_format' => 'extended_json',
-          'setenvif'          => [
-                                   'X-Forwarded-Proto "https" HTTPS=on',
-                                   'X-Forwarded-For "^([^,]*),?.*" CLIENT_IP=$1'
-                                 ],
-          'ssl'               => false
+        it { is_expected.to contain_profiles__apache__vhost__basic('http://prototypes.publiq.dev').with(
+          'documentroot'         => '/var/www/prototypes',
+          'serveraliases'        => ['*.prototypes.publiq.dev'],
+          'virtual_documentroot' => '/var/www/prototypes/%1'
         ) }
-
-        it {is_expected.to contain_class('profiles::apache').that_comes_before('Apache::Vhost[prototypes.publiq.dev_80]') }
-      end
-
-      context "with url => https://foobar.example.com" do
-        let(:params) { {
-          'url' => 'https://foobar.example.com',
-        } }
-
-        context "without extra parameters" do
-          it { expect { catalogue }.to raise_error(Puppet::ParseError, /expects a value for parameter 'certificate' when using HTTPS/) }
-        end
-
-        context "with certificate => 'foobar.example.com'" do
-          let(:params) { super().merge( {
-            'certificate' => 'foobar.example.com'
-          } ) }
-
-          it { is_expected.to contain_profiles__certificate('foobar.example.com') }
-
-          it { is_expected.to contain_apache__vhost('foobar.example.com_443').with(
-            'docroot'           => '/var/www/prototypes',
-            'servername'        => 'foobar.example.com',
-            'serveraliases'     => ['*.foobar.example.com'],
-            'virtual_docroot'   => '/var/www/prototypes/%1',
-            'docroot_owner'     => 'www-data',
-            'docroot_group'     => 'www-data',
-            'request_headers'   => [
-                                     'unset Proxy early',
-                                     'set X-Unique-Id %{UNIQUE_ID}e'
-                                   ],
-            'access_log_format' => 'extended_json',
-            'setenvif'          => [
-                                     'X-Forwarded-Proto "https" HTTPS=on',
-                                     'X-Forwarded-For "^([^,]*),?.*" CLIENT_IP=$1'
-                                   ],
-            'port'              => 443,
-            'ssl'               => true,
-            'ssl_cert'          => '/etc/ssl/certs/foobar.example.com.bundle.crt',
-            'ssl_key'           => '/etc/ssl/private/foobar.example.com.key'
-          ) }
-
-          it { is_expected.to contain_firewall('300 accept HTTPS traffic') }
-
-          it { is_expected.to contain_profiles__certificate('foobar.example.com').that_comes_before('Apache::Vhost[foobar.example.com_443]') }
-          it { is_expected.to contain_profiles__certificate('foobar.example.com').that_notifies('Class[apache::service]') }
-        end
       end
 
       context "without parameters" do
