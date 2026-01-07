ff2cc9a4178b0da42f68456314d118e4d1aa36fb
caterina <caterina.mennito@gmail.com>
2026-01-07 16:33:16 +0100

    POC

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -3,7 +3,8 @@ class profiles::docker (
   Boolean          $schedule_prune = false,
   Boolean          $lvm            = false,
   Optional[String] $volume_group   = undef,
-  Optional[String] $volume_size    = undef
+  Optional[String] $volume_size    = undef,
+  Hash[String, Hash] $jenkins_agent_images = {}
 ) inherits ::profiles {
 
   $data_dir = '/var/lib/docker'
@@ -67,5 +68,51 @@ class profiles::docker (
     minute      => '30',
     weekday     => '0',
     require     => Class['docker']
+  }
+
+  # Pull Jenkins agent Docker images if configured
+  if !empty($jenkins_agent_images) {
+    $jenkins_agent_images.each |$image_name, $image_config| {
+      $image_config['versions'].each |$version, $version_config| {
+        $full_tag = "${image_name}:${version}"
+        
+        # Copy Dockerfile from module files
+        $dockerfile_basename = basename($image_name)
+        $dockerfile_path = "/opt/jenkins-docker/${dockerfile_basename}-${version}-Dockerfile"
+
+        file { $dockerfile_path:
+          ensure => 'file',
+          source => $version_config['dockerfile_source'],
+          owner  => 'root',
+          group  => 'root',
+          mode   => '0644',
+        }
+        
+          # Pull Jenkins agent images
+          $jenkins_agent_images.each |$image_name| {
+            exec { "docker_pull_${image_name}":
+              command => "docker pull ${image_name}",
+              unless  => "docker image inspect ${image_name} > /dev/null 2>&1",
+              require => Class['docker'],
+            }
+          }
+
+        # Build the image
+        # exec { "docker_build_${image_name}_${version}":
+        #   command => sprintf(
+        #     'docker build -t %s %s -f %s /opt/jenkins-docker',
+        #     $full_tag,
+        #     $version_config['build_args'].map |$k, $v| { "--build-arg ${k}=${v}" }.join(' '),
+        #     $dockerfile_path
+        #   ),
+        #   unless  => "docker image inspect ${full_tag} > /dev/null 2>&1",
+        #   require => [
+        #     File[$dockerfile_path],
+        #     Class['docker'],
+        #   ],
+        #   logoutput => 'on_failure',
+        # }
+      }
+    }
   }
 }
