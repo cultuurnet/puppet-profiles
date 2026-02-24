49178b93d3cda808f76bc233f36c72125d2a0f84
Kristof Willaert <kristof@publiq.be>
2026-02-24 10:48:10 +0100

    Split Jenkins buildtools profile in two parts: bootstrap and homebuilt

---
+++ spec/classes/jenkins/buildtools/homebuilt_spec.rb
@@ -0,0 +1,18 @@
+describe 'profiles::jenkins::buildtools::homebuilt' do
+  include_examples 'operating system support'
+
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) { facts }
+
+      it { is_expected.to compile.with_all_deps }
+
+      it { is_expected.to contain_apt__source('publiq-tools') }
+
+      it { is_expected.to contain_package('golang').with({ 'ensure' => 'present' }) }
+      it { is_expected.to contain_package('kubectl').with({ 'ensure' => 'present' }) }
+      it { is_expected.to contain_package('argocd').with({ 'ensure' => 'present' }) }
+      it { is_expected.to contain_package('maven').with({ 'ensure' => 'present' }) }
+    end
+  end
+end
