d74847eb58d2623b19fd9f89e4be4906fa6fe913
Kristof Willaert <kristof@publiq.be>
2026-03-18 16:24:58 +0100

    Add retention_days parameter and log rotation to rsyslog::tag_filter defined type

--- manifests/rsyslog/tag_filter.pp
+++ manifests/rsyslog/tag_filter.pp
@@ -1,10 +1,12 @@
 define profiles::rsyslog::tag_filter (
   String  $syslogtag,
   String  $destination,
-  Integer $priority    = 50
+  Integer $priority       = 50,
+  Integer $retention_days = 7
 ) {
 
   include profiles::rsyslog
+  include profiles::logrotate
 
   rsyslog::component::expression_filter { $title:
     priority     => 0,
@@ -25,4 +27,13 @@ define profiles::rsyslog::tag_filter (
                       }
                     }
   }
+
+  logrotate::rule { $title:
+      path         => $destination,
+      rotate       => $retention_days - 1,
+      create_owner => 'root',
+      create_group => 'adm',
+      postrotate   => '/usr/lib/rsyslog/rsyslog-rotate',
+      *            => $profiles::logrotate::default_rule_attributes
+    }
 }
--- spec/defines/rsyslog/tag_filter_spec.rb
+++ spec/defines/rsyslog/tag_filter_spec.rb
@@ -15,12 +15,14 @@ describe 'profiles::rsyslog::tag_filter' do
           it { is_expected.to compile.with_all_deps }
 
           it { is_expected.to contain_profiles__rsyslog__tag_filter('foo').with(
-            'syslogtag'   => 'bar',
-            'priority'    => 50,
-            'destination' => '/var/log/bar.log'
+            'syslogtag'      => 'bar',
+            'priority'       => 50,
+            'destination'    => '/var/log/bar.log',
+            'retention_days' => 7
           ) }
 
           it { is_expected.to contain_class('profiles::rsyslog') }
+          it { is_expected.to contain_class('profiles::logrotate') }
 
           it { is_expected.to contain_rsyslog__component__expression_filter('foo').with(
             'priority'     => 0,
@@ -40,7 +42,21 @@ describe 'profiles::rsyslog::tag_filter' do
                                                   ]
                                 }
                               }
-          )}
+          ) }
+
+          it { is_expected.to contain_logrotate__rule('foo').with(
+            'path'          => '/var/log/bar.log',
+            'rotate'        => 6,
+            'rotate_every'  => 'day',
+            'create'        => true,
+            'create_mode'   => '0640',
+            'create_owner'  => 'root',
+            'create_group'  => 'adm',
+            'compress'      => true,
+            'delaycompress' => true,
+            'sharedscripts' => true,
+            'postrotate'    => '/usr/lib/rsyslog/rsyslog-rotate'
+          ) }
         end
       end
     end
@@ -53,11 +69,12 @@ describe 'profiles::rsyslog::tag_filter' do
       context "on #{os}" do
         let(:facts) { facts }
 
-        context 'with syslogtag => mytag, priority => 20 and destination => /var/log/mytaggedlog.log' do
+        context 'with syslogtag => mytag, priority => 20, destination => /var/log/mytaggedlog.log and retention_days => 21' do
           let(:params) { {
-            'syslogtag'   => 'mytag',
-            'priority'    => 20,
-            'destination' => '/var/log/mytaggedlog.log'
+            'syslogtag'      => 'mytag',
+            'priority'       => 20,
+            'destination'    => '/var/log/mytaggedlog.log',
+            'retention_days' => 21
           } }
 
           it { is_expected.to compile.with_all_deps }
@@ -80,7 +97,21 @@ describe 'profiles::rsyslog::tag_filter' do
                                                   ]
                                 }
                               }
-          )}
+          ) }
+
+          it { is_expected.to contain_logrotate__rule('baz').with(
+            'path'          => '/var/log/mytaggedlog.log',
+            'rotate'        => 20,
+            'rotate_every'  => 'day',
+            'create'        => true,
+            'create_mode'   => '0640',
+            'create_owner'  => 'root',
+            'create_group'  => 'adm',
+            'compress'      => true,
+            'delaycompress' => true,
+            'sharedscripts' => true,
+            'postrotate'    => '/usr/lib/rsyslog/rsyslog-rotate'
+          ) }
         end
       end
     end
