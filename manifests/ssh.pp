b0c04a07932b6272b83a5ed5f34e29ca939f26db
Kristof Willaert <kristof@publiq.be>
2026-04-27 22:21:32 +0200

    Remove sshkey type parameter as it is passed through the resource title

--- manifests/ssh.pp
+++ manifests/ssh.pp
@@ -36,7 +36,6 @@ class profiles::ssh(
 
   if $settings::storeconfigs {
     @@sshkey { "${facts['networking']['hostname']}@ssh-rsa":
-      type         => 'ssh-rsa',
       key          => $facts['ssh']['rsa']['key'],
       host_aliases => [$facts['networking']['ip'], $facts['networking']['fqdn']],
       provider     => 'parsed'
