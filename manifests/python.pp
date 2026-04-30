448f08e6224af46ad2a2bc5f0484c36f6c86aed1
Kristof Willaert <kristof@publiq.be>
2026-04-30 17:26:40 +0200

    Bring your own facts without the python module

--- manifests/python.pp
+++ manifests/python.pp
@@ -2,6 +2,14 @@ class profiles::python (
   Boolean $with_dev = false
 ) inherits ::profiles {
 
+  if $facts['python_version'] {
+    $python_release = join(split($facts['python_version'], /\./)[0,2], '.')
+
+    @profiles::jenkins::node_labels { 'python':
+      content => "python${python_release}"
+    }
+  }
+
   if $with_dev {
     package { 'python3-pip':
       ensure => 'installed'
@@ -10,9 +18,5 @@ class profiles::python (
 
   package { 'python3':
     ensure => 'installed'
-  }
-
-  @profiles::jenkins::node_labels { 'python':
-    content => "python${facts['python3_release']}"
   }
 }
