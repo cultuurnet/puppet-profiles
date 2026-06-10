634b1a848fa306ee84aeda2ce9930a07f9b7256a
Kristof Willaert <kristof@publiq.be>
2026-06-10 19:33:00 +0200

    Add Jenkins keypair and public key fact from provided private key

---
+++ lib/facter/jenkins_pubkey.rb
@@ -0,0 +1,14 @@
+Facter.add(:jenkins_pubkey) do
+  key_file = '/var/lib/jenkins/.ssh/id_jenkins.pub'
+
+  confine File.exists?(key_file)
+
+  setcode do
+    data = File.read(key_file)
+
+    key_type    = data.split(' ')[0]
+    key_content = data.split(' ')[1]
+
+    { 'type' => key_type, 'key' => key_content }
+  end
+end
---
+++ spec/classes/jenkins/controller/configuration/private_key_spec.rb
@@ -0,0 +1,88 @@
+describe 'profiles::jenkins::controller::configuration::private_key' do
+  include_examples 'operating system support'
+
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) { facts }
+
+      context 'without parameters' do
+        let(:params) { {} }
+
+        it { is_expected.to compile.with_all_deps }
+
+        it { is_expected.to contain_group('jenkins') }
+        it { is_expected.to contain_user('jenkins') }
+
+        it { is_expected.to contain_file('Jenkins .ssh config directory').with(
+          'ensure' => 'directory',
+          'path'   => '/var/lib/jenkins/.ssh',
+          'owner'  => 'jenkins',
+          'group'  => 'jenkins',
+          'mode'   => '0500'
+        ) }
+
+        it { is_expected.to contain_file('Jenkins private key').with(
+          'ensure' => 'absent',
+          'path'   => '/var/lib/jenkins/.ssh/id_jenkins'
+        ) }
+
+        it { is_expected.not_to contain_exec('Jenkins public key') }
+      end
+
+      context 'with key => abc123' do
+        let(:params) { {
+          'key' => 'abc123'
+        } }
+
+        it { is_expected.to contain_group('jenkins') }
+        it { is_expected.to contain_user('jenkins') }
+
+        it { is_expected.to contain_file('Jenkins .ssh config directory').with(
+          'ensure' => 'directory',
+          'path'   => '/var/lib/jenkins/.ssh',
+          'owner'  => 'jenkins',
+          'group'  => 'jenkins',
+          'mode'   => '0500'
+        ) }
+
+        it { is_expected.to contain_file('Jenkins private key').with(
+          'ensure'  => 'file',
+          'path'    => '/var/lib/jenkins/.ssh/id_jenkins',
+          'content' => 'abc123',
+          'owner'   => 'jenkins',
+          'group'   => 'jenkins',
+          'mode'    => '0400'
+        ) }
+
+        it { is_expected.to contain_exec('Jenkins public key').with(
+          'command'     => '/usr/bin/ssh-keygen -y -f /var/lib/jenkins/.ssh/id_jenkins > /var/lib/jenkins/.ssh/id_jenkins.pub',
+          'user'        => 'jenkins',
+          'refreshonly' => true
+        ) }
+
+        it { expect(exported_resources).not_to contain_ssh_authorized_key('Jenkins public key') }
+
+        it { is_expected.to contain_exec('Jenkins public key').that_requires('Group[jenkins]') }
+        it { is_expected.to contain_exec('Jenkins public key').that_requires('User[jenkins]') }
+        it { is_expected.to contain_exec('Jenkins public key').that_requires('File[Jenkins .ssh config directory]') }
+        it { is_expected.to contain_exec('Jenkins public key').that_subscribes_to('File[Jenkins private key]') }
+      end
+
+      context 'with fact jenkins_pubkey => { type => ssh-ed25519, key => abcd1234 } on EC2' do
+        let(:facts) {
+          super().merge({
+            'jenkins_pubkey' => { 'type' => 'ssh-ed25519', 'key' => 'abcd1234' },
+            'ec2_metadata'   => true
+          })
+        }
+
+        it { expect(exported_resources).to contain_ssh_authorized_key('Jenkins public key').with(
+          'user' => 'ubuntu',
+          'type' => 'ssh-ed25519',
+          'key'  => 'abcd1234',
+          'tag'  => ['jenkins']
+        ) }
+      end
+    end
+  end
+end
