484a725293d40042eab8df8f1833e3dbd2edee79
caterina <caterina.mennito@gmail.com>
2026-06-26 14:01:35 +0200

    [OPS-1550_1]: use to_yaml instead of reduce

--- manifests/docker/ecr_repos.pp
+++ manifests/docker/ecr_repos.pp
@@ -4,17 +4,13 @@ class profiles::docker::ecr_repos (
 
   realize File['/etc/puppetlabs/facter/facts.d']
 
-  $repos_yaml = $repos.reduce("docker_ecr_repos:\n") |$memo, $pair| {
-    "${memo}  ${pair[0]}: ${pair[1]}\n"
-  }
-
   file { 'Docker ECR external facts':
     ensure  => 'file',
     path    => '/etc/puppetlabs/facter/facts.d/docker_ecr_repos.yaml',
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => $repos_yaml,
+    content => stdlib::to_yaml({ 'docker_ecr_repos' => $repos }),
     require => File['/etc/puppetlabs/facter/facts.d'],
   }
 }
