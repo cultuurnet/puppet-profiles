1bb6bd88a605bcaf78f3de20608922ca80f93937
Kristof Willaert <kristof@publiq.be>
2026-06-30 13:37:51 +0200

    Add arch specific playwright dependencies on Ubuntu > 20.04

--- manifests/jenkins/buildtools/playwright.pp
+++ manifests/jenkins/buildtools/playwright.pp
@@ -1,10 +1,14 @@
 class profiles::jenkins::buildtools::playwright inherits ::profiles {
 
   $dependencies = ['xvfb', 'libnss3', 'libxrandr2', 'libgbm1', 'libxkbcommon0',
-                   'libxdamage1', 'libxcomposite1', 'libatspi2.0-0', 'libcups2',
-                   'libatk-bridge2.0-0']
+                   'libxdamage1', 'libxcomposite1']
 
-  package { $dependencies:
+  $arch_dependencies = $facts['os']['release']['major'] ? {
+                        '20.04' => ['libatspi2.0-0', 'libcups2', 'libatk-bridge2.0-0'],
+                        default => ['libatspi2.0-0t64', 'libcups2t64', 'libatk-bridge2.0-0t64'],
+  }
+
+  package { $dependencies + $arch_dependencies:
     ensure => 'present'
   }
 }
