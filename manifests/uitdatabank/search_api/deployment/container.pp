921c558faff7f8a72a806120ea19f2503d680782
caterina <caterina.mennito@gmail.com>
2026-07-01 16:37:47 +0200

    [OPS-1550]: move deployment docker logic into new profile deployment::container

--- manifests/uitdatabank/search_api/deployment/container.pp
+++ manifests/uitdatabank/search_api/deployment/container.pp
@@ -1,4 +1,54 @@
 class profiles::uitdatabank::search_api::deployment::container (
+  String           $image,
+  String           $env_image_tag = 'acc',
+  String           $aws_region    = 'eu-west-1',
+  Optional[String] $image_tag     = undef,
+  Optional[String] $puppetdb_url  = lookup('data::puppet::puppetdb::url', Optional[String], 'first', undef)
 ) inherits ::profiles {
+  $config_dir         = '/etc/uitdatabank-search-api'
+  $ecr_repository     = regsubst($image, '^[^/]+/', '')
+  $resolved_image_tag = pick($image_tag, $facts.dig('docker_image_tag', $ecr_repository), 'latest')
 
+  class { 'profiles::docker::ecr_repos':
+    repos => {
+      $ecr_repository => {
+        'region'    => $aws_region,
+        'image_tag' => $env_image_tag,
+      },
+    },
+  }
+
+  file { 'uitdatabank-search-api-docker-compose':
+    ensure  => 'file',
+    path    => "${config_dir}/docker-compose.yml",
+    content => template('profiles/uitdatabank/search_api/deployment/container/docker-compose.yml.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => File[$config_dir],
+    notify  => Docker::Compose['uitdatabank-search-api'],
+  }
+
+  docker::compose { 'uitdatabank-search-api':
+    ensure        => 'present',
+    compose_files => ["${config_dir}/docker-compose.yml"],
+    pull_on_start => true,
+    require       => [
+      File['uitdatabank-search-api-config'],
+      File['uitdatabank-search-api-pubkey-keycloak'],
+      File['uitdatabank-search-api-docker-compose'],
+    ],
+  }
+
+  cron { 'uitdatabank-search-api-reindex-permanent':
+    command     => "docker compose -f ${config_dir}/docker-compose.yml exec -T search-api php bin/app.php udb3-core:reindex-permanent",
+    environment => ['MAILTO=infra+cron@publiq.be'],
+    hour        => '0',
+    minute      => '0',
+    require     => Docker::Compose['uitdatabank-search-api'],
+  }
+
+  profiles::deployment::versions { $title:
+    puppetdb_url => $puppetdb_url,
+  }
 }
