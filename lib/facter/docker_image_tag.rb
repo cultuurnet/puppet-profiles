cd3638630d6079da6c9f5cd6f831990200ccddb9
caterina <caterina.mennito@gmail.com>
2026-07-01 10:27:11 +0200

    [OPS-1550_3]: support environment specific tag in facts and profile

--- lib/facter/docker_image_tag.rb
+++ lib/facter/docker_image_tag.rb
@@ -3,14 +3,16 @@ Facter.add('docker_image_tag') do
     repos = Facter.value('docker_ecr_repos')
     next nil unless repos.is_a?(Hash) && !repos.empty?
 
-    repos.each_with_object({}) do |(name, region), result|
-      region ||= 'eu-west-1'
+    repos.each_with_object({}) do |(name, config), result|
+      region    = config['region']    || 'eu-west-1'
+      image_tag = config['image_tag'] || 'latest'
+
       output = Facter::Core::Execution.execute(
         "aws ecr describe-images \
           --repository-name #{name} \
-          --image-ids imageTag=latest \
+          --image-ids imageTag=#{image_tag} \
           --region #{region} \
-          --query \"imageDetails[0].imageTags[?!= 'latest'] | [0]\" \
+          --query \"imageDetails[0].imageTags[?!= '#{image_tag}'] | [0]\" \
           --output text",
         on_fail: nil
       )
