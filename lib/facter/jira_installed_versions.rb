4b01615f2dc89752dd724eb5c463cc44dde8bd8a
caterina <caterina.mennito@gmail.com>
2026-07-22 14:10:45 +0200

    [OPS-1571]: use software versions instead of mtime to sort

--- lib/facter/jira_installed_versions.rb
+++ lib/facter/jira_installed_versions.rb
@@ -5,7 +5,8 @@ Facter.add('jira_installed_versions') do
 
     Dir.glob('/opt/jira/atlassian-jira-software-[0-9]*-standalone')
        .select { |dir| File.directory?(dir) }
-       .sort_by { |dir| -File.mtime(dir).to_i }
+       .sort_by { |dir| Gem::Version.new(dir[/-(\d+\.\d+\.\d+)-standalone$/, 1]) }
+       .reverse
        .map { |dir| File.basename(dir) }
   end
 end
