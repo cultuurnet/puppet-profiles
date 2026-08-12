b6ede45ada7bccd90f28aaddd7830eeed05fc4de
caterina <caterina.mennito@gmail.com>
2026-08-12 15:30:25 +0200

    [OPS-1582]: apache logging: decrese retention days and add maxsize

--- manifests/apache/logging.pp
+++ manifests/apache/logging.pp
@@ -1,5 +1,6 @@
 class profiles::apache::logging (
-  Integer $retention_days = 21
+  Integer $retention_days = 16,
+  Logrotate::Size $access_maxsize = '500M'
 ) inherits ::profiles {
 
   include profiles::logrotate
@@ -7,6 +8,7 @@ class profiles::apache::logging (
   logrotate::rule { 'apache2':
     path         => '/var/log/apache2/*.log',
     rotate       => $retention_days - 1,
+    maxsize      => $access_maxsize,
     create_owner => 'root',
     create_group => 'adm',
     postrotate   => 'systemctl status apache2 > /dev/null 2>&1 && systemctl reload apache2 > /dev/null 2>&1',
