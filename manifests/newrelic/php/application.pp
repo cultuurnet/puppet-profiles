ef4b0cc65c1802959772160e3d482545cde4dbd4
Vincent Verbist <vincent.verbist@publiq.be>
2026-09-10 09:53:39 +0200

    Refactor PHP New Relic application configuration

--- manifests/newrelic/php/application.pp
+++ manifests/newrelic/php/application.pp
@@ -1,21 +1,31 @@
 define profiles::newrelic::php::application (
-  String         $docroot,
-  Boolean        $enable                                 = false,
-  Optional[Hash] $optional_config                        = {},
-  Boolean        $application_logging_forwarding_enabled = false,
-  Boolean        $transaction_tracer_enabled             = false,
-  Boolean        $distributed_tracing_enabled            = false
+  String           $docroot,
+  Boolean          $enable                                 = false,
+  Optional[String] $app_name                               = undef,
+  Optional[Hash]   $optional_config                        = {},
+  Boolean          $application_logging_forwarding_enabled = false,
+  Boolean          $transaction_tracer_enabled             = false,
+  Boolean          $distributed_tracing_enabled            = false
 ) {
 
   include ::profiles
 
-  $appname = "${title}_${environment}"
-
   if $enable {
+    unless $app_name {
+      fail("Defined resource type Profiles::Newrelic::Php::Application[${title}] expects a value for parameter 'app_name' when enabled")
+    }
+
+    $appname     = $app_name
+    $file_content = template('profiles/newrelic/php/user.ini.erb')
+
     include ::profiles::newrelic::php
+    $requirements = [Group['www-data'], User['www-data'], Class['profiles::newrelic::php']]
+  } else {
+    $file_content = undef
+    $requirements = [Group['www-data'], User['www-data']]
   }
 
-  file { "${appname} newrelic php config":
+  file { "${title} newrelic php config":
     ensure  => $enable ? {
                  true  => 'file',
                  false => 'absent'
@@ -23,8 +33,8 @@ define profiles::newrelic::php::application (
     path    => "${docroot}/.user.ini",
     owner   => 'www-data',
     group   => 'www-data',
-    content => template('profiles/newrelic/php/user.ini.erb'),
-    require => [Group['www-data'], User['www-data']]
+    content => $file_content,
+    require => $requirements
   }
 
 }
---
+++ spec/defines/newrelic/php/application_spec.rb
@@ -0,0 +1,88 @@
+describe 'profiles::newrelic::php::application' do
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) { facts }
+      let(:title) { 'example-api' }
+      let(:hiera_config) { 'spec/support/hiera/common.yaml' }
+      let(:pre_condition) do
+        <<~PUPPET
+          realize Group['www-data']
+          realize User['www-data']
+        PUPPET
+      end
+      let(:params) do
+        {
+          'app_name' => 'example-api.example.com_production',
+          'docroot'  => '/var/www/example-api/public'
+        }
+      end
+
+      context 'without New Relic enabled' do
+        it { is_expected.to compile.with_all_deps }
+
+        it { is_expected.to contain_profiles__newrelic__php__application('example-api').with(
+          'app_name' => 'example-api.example.com_production',
+          'docroot'  => '/var/www/example-api/public',
+          'enable'   => false
+        ) }
+
+        it { is_expected.not_to contain_class('profiles::newrelic::php') }
+
+        it { is_expected.to contain_file('example-api newrelic php config').with(
+          'ensure' => 'absent',
+          'path'   => '/var/www/example-api/public/.user.ini',
+          'owner'  => 'www-data',
+          'group'  => 'www-data'
+        ) }
+      end
+
+      context 'with New Relic enabled' do
+        let(:params) do
+          super().merge(
+            'enable'                                 => true,
+            'application_logging_forwarding_enabled' => true,
+            'transaction_tracer_enabled'             => true,
+            'distributed_tracing_enabled'            => true,
+            'optional_config'                        => { 'newrelic.daemon.address' => '/run/newrelic.sock' }
+          )
+        end
+
+        it { is_expected.to compile.with_all_deps }
+
+        it { is_expected.to contain_class('profiles::newrelic::php') }
+
+        it { is_expected.to contain_file('example-api newrelic php config').with(
+          'ensure' => 'file',
+          'path'   => '/var/www/example-api/public/.user.ini',
+          'owner'  => 'www-data',
+          'group'  => 'www-data'
+        ) }
+
+        it { is_expected.to contain_file('example-api newrelic php config').with_content(/^newrelic\.appname = "example-api\.example\.com_production"$/) }
+        it { is_expected.to contain_file('example-api newrelic php config').with_content(/^newrelic\.application_logging\.forwarding\.enabled = true$/) }
+        it { is_expected.to contain_file('example-api newrelic php config').with_content(/^newrelic\.transaction_tracer\.enabled = true$/) }
+        it { is_expected.to contain_file('example-api newrelic php config').with_content(/^newrelic\.distributed_tracing_enabled = true$/) }
+        it { is_expected.to contain_file('example-api newrelic php config').with_content(/^newrelic\.daemon\.address="\/run\/newrelic\.sock"$/) }
+        it { is_expected.to contain_file('example-api newrelic php config').that_requires('Class[profiles::newrelic::php]') }
+      end
+
+      context 'with two enabled PHP applications' do
+        let(:params) { super().merge('enable' => true) }
+        let(:post_condition) do
+          <<~PUPPET
+            profiles::newrelic::php::application { 'other-api':
+              app_name => 'other-api.example.com_production',
+              docroot  => '/var/www/other-api/public',
+              enable   => true,
+            }
+          PUPPET
+        end
+
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to have_profiles__newrelic__php__application_resource_count(2) }
+        it { is_expected.to contain_file('example-api newrelic php config').with_path('/var/www/example-api/public/.user.ini') }
+        it { is_expected.to contain_file('other-api newrelic php config').with_path('/var/www/other-api/public/.user.ini') }
+      end
+    end
+  end
+end
