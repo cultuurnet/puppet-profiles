240776355a69606e469da5fb1218513484583399
caterina <caterina.mennito@gmail.com>
2026-09-17 10:36:54 +0200

    [fix-playwright-missing-deps]: add missing dependencies for new playwright version

--- manifests/jenkins/buildtools/playwright.pp
+++ manifests/jenkins/buildtools/playwright.pp
@@ -1,12 +1,13 @@
 class profiles::jenkins::buildtools::playwright inherits ::profiles {
 
-  $dependencies = ['xvfb', 'libnss3', 'libxrandr2', 'libgbm1', 'libxkbcommon0',
-                   'libxdamage1', 'libxcomposite1']
+$dependencies = ['xvfb', 'libnss3', 'libxrandr2', 'libgbm1', 'libxkbcommon0',
+                 'libxdamage1', 'libxcomposite1', 'libxfixes3', 'libcairo2',
+                 'libpango-1.0-0']
 
-  $arch_dependencies = $facts['os']['release']['major'] ? {
-                        '20.04' => ['libatspi2.0-0', 'libcups2', 'libatk-bridge2.0-0'],
-                        default => ['libatspi2.0-0t64', 'libcups2t64', 'libatk-bridge2.0-0t64'],
-  }
+$arch_dependencies = $facts['os']['release']['major'] ? {
+                      '20.04' => ['libatspi2.0-0', 'libcups2', 'libatk-bridge2.0-0', 'libasound2'],
+                      default => ['libatspi2.0-0t64', 'libcups2t64', 'libatk-bridge2.0-0t64', 'libasound2t64'],
+}
 
   package { $dependencies + $arch_dependencies:
     ensure => 'present'
